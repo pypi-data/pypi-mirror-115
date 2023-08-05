6910208b4a80783d18acd7e34b0357717c6bcced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:52 +0000

    Add package distributions-spw/0.2/distributions_spw-0.2.tar.gz

---
+++ packages/distributions-spw/distributions_spw-0.2.tar.gz/distributions_spw-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_spw',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_spw'],
+      zip_safe=False)
