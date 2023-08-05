7074563680e878095a9d3728f808b6b7cba932a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:58 +0000

    Add package distributionsml/0.1/distributionsml-0.1.tar.gz

---
+++ packages/distributionsml/distributionsml-0.1.tar.gz/distributionsml-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsml',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsml'],
+      zip_safe=False)
