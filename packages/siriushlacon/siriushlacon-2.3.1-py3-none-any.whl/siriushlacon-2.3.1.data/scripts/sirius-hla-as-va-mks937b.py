7a84624c7abf53579a2ccb072790a78594185007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:41 +0000

    Add package siriushlacon/2.3.1/siriushlacon-2.3.1-py3-none-any.whl

---
+++ packages/siriushlacon/siriushlacon-2.3.1-py3-none-any.whl/siriushlacon-2.3.1.data/scripts/sirius-hla-as-va-mks937b.py
@@ -0,0 +1,14 @@
+#!python
+import argparse
+import os
+
+from siriushlacon.mks937b.consts import MKS_MAIN
+from siriushlacon.utils.launch import LogLevel, launch_pydm
+
+os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("VAC Pumps Controller")
+    args = parser.parse_args()
+    launch_pydm(displayfile=MKS_MAIN, hide_nav_bar=True, log_level=LogLevel.INFO)
