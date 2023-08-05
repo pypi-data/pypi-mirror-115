1a1145b6dbca88e08811a237e6158d2f2d3f5899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:51 +0000

    Add package ikea-api/0.3.0/ikea_api-0.3.0.tar.gz

---
+++ packages/ikea-api/ikea_api-0.3.0.tar.gz/ikea_api-0.3.0/src/ikea_api/__init__.py
@@ -0,0 +1,4 @@
+from .core import IkeaApi
+
+__version__ = "0.3.0"
+__all__ = ["IkeaApi"]
---
+++ packages/ikea-api/ikea_api-0.3.0.tar.gz/ikea_api-0.3.0/src/ikea_api/endpoints/purchases/queries.py
@@ -0,0 +1,61 @@
+from .fragments import *
+
+history = """
+  query History($skip: Int!, $take: Int!) {
+    history(skip: $skip, take: $take) {
+      id
+      dateAndTime {
+        ...DateAndTime
+      }
+      status
+      storeName
+      totalCost {
+        code
+        value
+      }
+    }
+  }
+
+  %s
+""" % (
+    date_and_time
+)
+
+status_banner_order = f"""
+  query StatusBannerOrder($orderNumber: String!, $liteId: String) {{
+    order(orderNumber: $orderNumber, liteId: $liteId) {{
+      id
+      dateAndTime {{
+        ...DateAndTime
+      }}
+      status
+      services {{
+        ...ServiceInfo
+      }}
+      deliveryMethods {{
+        ...DeliveryInfo
+      }}
+    }}
+  }}
+
+  {date_and_time}
+  {delivery_date}
+  {service_info}
+  {delivery_info}
+"""
+
+
+costs_order = """
+  query CostsOrder($orderNumber: String!, $liteId: String) {
+    order(orderNumber: $orderNumber, liteId: $liteId) {
+      id
+      costs {
+        ...Costs
+      }
+    }
+  }
+
+  %s
+""" % (
+    costs
+)
