dc25cb4b24ea9231667bdc83b8b49ff4d126bce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:59 +0000

    Add package pygraphblas/5.1.5.1/pygraphblas-5.1.5.1.tar.gz

---
+++ packages/pygraphblas/pygraphblas-5.1.5.1.tar.gz/pygraphblas-5.1.5.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import os
+
+setup(
+    name='pygraphblas',
+    version='5.1.5.1',
+    description='GraphBLAS Python bindings.',
+    author='Michel Pelletier',
+    packages=['pygraphblas'],
+    setup_requires=["pytest-runner"],
+    install_requires=["suitesparse-graphblas", "numba", "scipy", "contextvars"],
+)
+
