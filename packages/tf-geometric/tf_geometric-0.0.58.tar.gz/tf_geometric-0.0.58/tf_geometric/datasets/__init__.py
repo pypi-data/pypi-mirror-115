3f0a58be6a1fc3ac76bd612627f4bd06c3dfd159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:39 +0000

    Add package tf-geometric/0.0.58/tf_geometric-0.0.58.tar.gz

---
+++ packages/tf-geometric/tf_geometric-0.0.58.tar.gz/tf_geometric-0.0.58/tf_geometric/datasets/__init__.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+from tf_geometric.datasets.ppi import PPIDataset
+from tf_geometric.datasets.tu import TUDataset
+from tf_geometric.datasets.planetoid import PlanetoidDataset, CoraDataset, CiteseerDataset, PubmedDataset, \
+    SupervisedCoraDataset, SupervisedCiteseerDataset, SupervisedPubmedDataset
+from tf_geometric.datasets.reddit import TransductiveRedditDataset, InductiveRedditDataset
+from tf_geometric.datasets.ogb import OGBNodePropPredDataset
