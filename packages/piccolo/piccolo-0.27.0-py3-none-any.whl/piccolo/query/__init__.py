141a5f6cadc958ef766d37854e7cca800b1d0e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:42 +0000

    Add package piccolo/0.27.0/piccolo-0.27.0-py3-none-any.whl

---
+++ packages/piccolo/piccolo-0.27.0-py3-none-any.whl/piccolo/query/__init__.py
@@ -0,0 +1,16 @@
+from .base import Query
+from .methods import (
+    Alter,
+    Count,
+    Create,
+    CreateIndex,
+    Delete,
+    DropIndex,
+    Exists,
+    Insert,
+    Objects,
+    Raw,
+    Select,
+    TableExists,
+    Update,
+)
