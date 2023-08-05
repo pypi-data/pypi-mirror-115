f345326fd60a1eb0a2341106347d010f18eb59e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:29 +0000

    Add package ExGrads/0.1.8/ExGrads-0.1.8.tar.gz

---
+++ packages/ExGrads/ExGrads-0.1.8.tar.gz/ExGrads-0.1.8/exgrads/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.1.8'
+
+import exgrads.grad1
+from exgrads.trH import trH
+
+__all__ = [
+	'__version__',
+	'grad1',
+	'trH',
+]
---
+++ packages/ExGrads/ExGrads-0.1.8.tar.gz/ExGrads-0.1.8/exgrads/trH.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# -------------------------------------------------------
+import torch
+import exgrads.hooks as hooks
+def trH(model, x):
+	model.eval()
+	hooks.register(model)
+	
+	results = torch.zeros(x.shape[0])		# (b)
+	results = results.to(x.device)
+
+	logit = model(x)								# := (b,K)
+
+	# for p*p^T part
+	Z		= torch.logsumexp(logit, dim=1)			# -> (b)
+	loss 	= Z.sum()								# -> (1)
+	
+	loss.backward(retain_graph=True)
+	model.zero_grad()
+	for grad1 in hooks.generate_grad1(model):
+		grad1 = grad1**2 							# := (b,*)
+		grad1 = grad1.flatten(1,-1) 				# -> (b,prod(*))
+		grad1 = torch.sum(grad1,dim=1) 				# -> (b,)
+		results -= grad1.detach()
+
+	# for diag(p) part
+	prob	= torch.softmax(logit,dim=1)				# -> (b,K)
+	prob	= prob.unsqueeze(dim=2)						# -> (b,K,1)
+	logit	= logit.sum(dim=0)							# -> (K)
+	for k, logit_k in enumerate(logit):
+		logit_k.backward(retain_graph=True)
+		model.zero_grad()
+		for grad1 in hooks.generate_grad1(model):
+			grad1 = grad1.flatten(1,-1)				# -> (b,prod(*))
+			grad1 = prob[:,k] * grad1				# -> (b,prod(*))
+			grad1 = grad1.flatten(1,-1)				# -> (b)
+			grad1 = torch.sum(grad1,dim=1)			# -> (b)
+			results += grad1.detach()
+		
+	hooks.deregister(model)
+	return results
+
+
+# -------------------------------------------------------
+
