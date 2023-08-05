862a48d6734f5093df1ee9df1689c8f8418fc069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:06 +0000

    Add package hdp-distributions/0.1/hdp_distributions-0.1.tar.gz

---
+++ packages/hdp-distributions/hdp_distributions-0.1.tar.gz/hdp_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hdp_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hdp_distributions'],
+      zip_safe=False)
