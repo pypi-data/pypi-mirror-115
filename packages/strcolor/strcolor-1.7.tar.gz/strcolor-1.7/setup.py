ba74407a17acc2a5436e8b65fa52b10451847951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:26 +0000

    Add package strcolor/1.7/strcolor-1.7.tar.gz

---
+++ packages/strcolor/strcolor-1.7.tar.gz/strcolor-1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='strcolor',
+    version='1.7',
+    author='dugudongfangshuo',
+    author_email='2310775260@qq.com',
+    url='https://github.com/dugudongfangshuo',
+    description=u'一个用来进行颜色打印的便捷工具',
+    packages=['colorstr'],
+    install_requires=[],
+)
