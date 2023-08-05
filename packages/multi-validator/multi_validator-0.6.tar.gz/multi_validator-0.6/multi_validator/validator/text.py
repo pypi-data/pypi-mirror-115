486fc1290323e0294d8b492572b705a0669b8350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:29 +0000

    Add package multi-validator/0.6/multi_validator-0.6.tar.gz

---
+++ packages/multi-validator/multi_validator-0.6.tar.gz/multi_validator-0.6/multi_validator/validator/text.py
@@ -0,0 +1,9 @@
+def short_phrase(text):
+    if not len(text) == 0:
+        return False
+    return True
+
+def long_phrase(text):
+    if not len(text) > 10:
+        return False
+    return True
