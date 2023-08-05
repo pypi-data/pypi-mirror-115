7f615549404cb5d2973486ba1167f8e366ed7ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:17 +0000

    Add package cst-geometry/0.0.10/cst_geometry-0.0.10.tar.gz

---
+++ packages/cst-geometry/cst_geometry-0.0.10.tar.gz/cst_geometry-0.0.10/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cst_geometry']
+
+package_data = \
+{'': ['*'], 'cst_geometry': ['basic_script/*']}
+
+setup_kwargs = {
+    'name': 'cst-geometry',
+    'version': '0.0.10',
+    'description': 'Package for modelling in CST Microwave studio using python',
+    'long_description': None,
+    'author': 'konstantgr',
+    'author_email': '60608301+konstantgr@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
