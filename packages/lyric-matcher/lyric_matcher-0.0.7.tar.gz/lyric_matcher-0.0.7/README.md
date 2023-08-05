9989ce6d7136b0366f0b4f5eab936ac641a4c1bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:30 +0000

    Add package lyric-matcher/0.0.7/lyric_matcher-0.0.7.tar.gz

---
+++ packages/lyric-matcher/lyric_matcher-0.0.7.tar.gz/lyric_matcher-0.0.7/README.md
@@ -0,0 +1,18 @@
+# Inputs
+
+## 1.song_path 
+## 2.songwithTime_path 
+
+
+# Outputs
+# timestamp list
+
+# Installation:
+## pip install lyric_matcher
+
+
+## from lyricsmatcher import lyrics_matcher
+## example
+## lyrics_section_path = ''
+## lyrics_timestamp_path = ''
+## t = lyrics_matcher.lyrics_matcher(lyrics_section_path, lyrics_timestamp_path )
---
+++ packages/lyric-matcher/lyric_matcher-0.0.7.tar.gz/lyric_matcher-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lyric_matcher",
+    version="0.0.7",
+    author="hanhaodi zhang, zhaoyang bu",
+    author_email="1004591463@qq.com",
+    description="README.md",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",              
+    ],
+)
+
