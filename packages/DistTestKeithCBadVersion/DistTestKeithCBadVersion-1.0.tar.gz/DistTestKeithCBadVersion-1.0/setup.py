3a5e2d6940f057365e0837a71549e3b3fa79a896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package DistTestKeithCBadVersion/1.0/DistTestKeithCBadVersion-1.0.tar.gz

---
+++ packages/DistTestKeithCBadVersion/DistTestKeithCBadVersion-1.0.tar.gz/DistTestKeithCBadVersion-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'DistTestKeithCBadVersion',
+    version = '1.0',
+    author = 'Keith Cressman',
+    author_email = "keith.cressman@duke.edu",
+    packages = find_packages(),
+    url= "https://github.com/KeithCressman/test"
+)
