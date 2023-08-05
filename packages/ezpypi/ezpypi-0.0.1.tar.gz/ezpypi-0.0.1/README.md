6e09141ebb950d13ed7bef23b7644200468ccf0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:09 +0000

    Add package ezpypi/0.0.1/ezpypi-0.0.1.tar.gz

---
+++ packages/ezpypi/ezpypi-0.0.1.tar.gz/ezpypi-0.0.1/README.md
@@ -0,0 +1,2 @@
+# EzPyPI
+A Python publication tool for painlessly uploading your package to GitHub, Test PyPI, and PyPI.
---
+++ packages/ezpypi/ezpypi-0.0.1.tar.gz/ezpypi-0.0.1/ezpypi/core.py
@@ -0,0 +1,6 @@
+class EzPyPI:
+    def __init__(self):
+        pass
+
+    def main(self):
+        return 'Hello World!'
