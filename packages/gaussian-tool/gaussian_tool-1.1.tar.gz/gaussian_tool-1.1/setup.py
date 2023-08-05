10d906300e2493051e3e0753d4c769ceb1b4af50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:11 +0000

    Add package gaussian-tool/1.1/gaussian_tool-1.1.tar.gz

---
+++ packages/gaussian-tool/gaussian_tool-1.1.tar.gz/gaussian_tool-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_tool',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gaussian_tool'],
+      author = "Sarthak Sharma",
+      author_email = "sarthakparashar1408@gmail.com",
+      zip_safe=False)
