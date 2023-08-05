84a22fd0e6fd940a6348eaba29aece0045e112f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:47 +0000

    Add package analitiqs/0.0.7/analitiqs-0.0.7.tar.gz

---
+++ packages/analitiqs/analitiqs-0.0.7.tar.gz/analitiqs-0.0.7/analitiqs/etl.py
@@ -0,0 +1,30 @@
+#etl
+#class to process columns
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql.window import *
+from __main__ import __builtin__
+
+class cols:
+  
+    def __init__(self, df):
+      self.df = df
+    
+    #lag and compare
+    def lagcompare(self):
+      return print('test')
+    
+    #lag chosen col x times
+    def lagsum(self, partition, order, col, times):
+      #insert try catch and coltype checks
+    
+      #assign partition
+      w = Window.partitionBy(partition).orderBy(order).rowsBetween(-__builtin__.abs(times - 1), 0)
+
+      self.df = self.df.withColumn(col + '_' + str(times),sum(col).over(w))
+    
+    #return df after applying methods
+    def returndf(self):
+      return self.df
+        
