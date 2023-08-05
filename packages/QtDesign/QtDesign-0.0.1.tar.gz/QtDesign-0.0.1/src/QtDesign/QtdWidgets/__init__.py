b104c09525d6d18af5f31c00536508f1ad1bb601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:34 +0000

    Add package QtDesign/0.0.1/QtDesign-0.0.1.tar.gz

---
+++ packages/QtDesign/QtDesign-0.0.1.tar.gz/QtDesign-0.0.1/src/QtDesign/QtdWidgets/__init__.py
@@ -0,0 +1,5 @@
+from .QCardWidget import QCardWidget
+from .QRichTabBar import QRichTabBar
+from .QRichTabWidget import QRichTabWidget
+
+__all__ = ["QCardWidget", "QRichTabBar", "QRichTabWidget"]
---
+++ packages/QtDesign/QtDesign-0.0.1.tar.gz/QtDesign-0.0.1/src/QtDesign/__init__.py
@@ -0,0 +1,7 @@
+"""
+Custom widgets and utilities for PySide2 for additional design flexibility
+"""
+
+__version__ = "0.0.1"
+
+from QtDesign import QtdUiTools, QtdWidgets
