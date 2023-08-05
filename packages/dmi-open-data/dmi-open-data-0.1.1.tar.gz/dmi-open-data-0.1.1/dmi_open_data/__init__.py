768e87d85a89893ade2b312490969940dd37a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:48 +0000

    Add package dmi-open-data/0.1.1/dmi-open-data-0.1.1.tar.gz

---
+++ packages/dmi-open-data/dmi-open-data-0.1.1.tar.gz/dmi-open-data-0.1.1/dmi_open_data/__init__.py
@@ -0,0 +1,15 @@
+"""Simple Python interface to the The Danish Meteorological Institute's (DMI) Open Data API."""
+
+__version__ = "0.1.1"
+
+from dmi_open_data.client import DMIOpenDataClient
+from dmi_open_data.enums import Parameter
+from dmi_open_data.utils import microseconds2date, date2microseconds
+
+
+__all__ = [
+    "DMIOpenDataClient",
+    "Parameter",
+    "microseconds2date",
+    "date2microseconds",
+]
