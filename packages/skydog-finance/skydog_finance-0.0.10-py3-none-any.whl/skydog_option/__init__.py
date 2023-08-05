6f968788f2c028e5981e155d38607280796551e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package skydog-finance/0.0.10/skydog_finance-0.0.10-py3-none-any.whl

---
+++ packages/skydog-finance/skydog_finance-0.0.10-py3-none-any.whl/skydog_option/__init__.py
@@ -0,0 +1,10 @@
+from skydog_option import hkex
+from skydog_option import cboe
+
+
+def option_price(underlying: str, strike: int, expiry: str, otype: str) -> dict:
+    result = {"underlying": underlying, "strike": strike, "otype": otype}
+    if underlying[-3:].lower() == ".hk":
+        return hkex.get_option(underlying, strike, expiry, otype)
+    else:
+        return cboe.get_option(underlying, strike, expiry, otype)
