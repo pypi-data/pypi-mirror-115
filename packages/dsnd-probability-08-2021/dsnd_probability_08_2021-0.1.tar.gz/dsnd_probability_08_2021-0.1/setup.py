71a559b520e734d11d7ecea778cb52a8989a703d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:27 +0000

    Add package dsnd-probability-08-2021/0.1/dsnd_probability_08_2021-0.1.tar.gz

---
+++ packages/dsnd-probability-08-2021/dsnd_probability_08_2021-0.1.tar.gz/dsnd_probability_08_2021-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_08_2021',
+      version='0.1',
+      description='Gaussian and Binomial distributions | Udacity Nanodegree Execise',
+      author= 'Andre Vargas',
+      packages=['dsnd_probability_08_2021'],
+      zip_safe=False)
