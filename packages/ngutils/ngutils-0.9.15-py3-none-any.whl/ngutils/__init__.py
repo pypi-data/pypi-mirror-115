40884d7a3c943b7b020bcd5fa536dc5f9f2643b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package ngutils/0.9.15/ngutils-0.9.15-py3-none-any.whl

---
+++ packages/ngutils/ngutils-0.9.15-py3-none-any.whl/ngutils/__init__.py
@@ -0,0 +1,15 @@
+from ngutils.functions import *
+
+__version__ = '0.9.15'
+
+__all__ = [
+    'accel_steps',
+    'flatten',
+    'hash_hd',
+    'read_files_contents',
+    'read_urls_contents',
+    'reduce_content',
+    'tune_steps',
+    'view_types',
+]
+
