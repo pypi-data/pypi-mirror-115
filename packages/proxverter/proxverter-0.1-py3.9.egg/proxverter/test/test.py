cd763c9a7595f6cf5dbd1ba4f56e85ea34e7faab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:40 +0000

    Add package proxverter/0.1/proxverter-0.1-py3.9.egg

---
+++ packages/proxverter/proxverter-0.1-py3.9.egg/proxverter/test/test.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import tempfile
+import proxverter
+import logging
+import multiprocessing
+
+if __name__ == "__main__":
+    multiprocessing.freeze_support()
+
+    p = proxverter.Proxverter(
+        "127.0.0.1",
+        8899,
+        is_https=True,
+        verbose=True
+    )
+
+    fl1 = os.path.join(tempfile.gettempdir(), 'priv.key')
+    fl2 = os.path.join(tempfile.gettempdir(), 'cert.crt')
+    fl3 = "certificate.pfx"
+
+    #p.gen_key(fl1)
+    #p.gen_cert(fl2)
+    #p.gen_pfx(fl3)
+
+    p.join(fl1, fl2)
