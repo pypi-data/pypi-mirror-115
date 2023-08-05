753a579ec57e6e13d3f46190ee95be7ce8e7e4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:08 +0000

    Add package osutil/0.0.9/osutil-0.0.9.tar.gz

---
+++ packages/osutil/osutil-0.0.9.tar.gz/osutil-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import osutil
+
+setuptools.setup(
+	name=osutil.__name__,
+	version=osutil.__version__,
+	py_modules=['osutil'],
+	url='https://github.com/PencilShavings/python-osutil',
+	license='MIT',
+	author=osutil.__author__,
+	author_email='eb.pencilshavings@protonmail.com',
+	description=osutil.__doc__,
+	long_description=osutil.cat('README.md'),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/PencilShavings/python-osutil/issues",
+    },
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3'
+	]
+)
