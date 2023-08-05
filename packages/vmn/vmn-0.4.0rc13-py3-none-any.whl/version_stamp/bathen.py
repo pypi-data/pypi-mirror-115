5e0f4a50d8a574957c463235494bbd67fdff808e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package vmn/0.4.0rc13/vmn-0.4.0rc13-py3-none-any.whl

---
+++ packages/vmn/vmn-0.4.0rc13-py3-none-any.whl/version_stamp/bathen.py
@@ -0,0 +1,14 @@
+buy = input("if you want to buy?")
+if buy == "no":
+    print("bye bye")
+
+if buy == "yes":
+    choice = input("1 or 2")
+
+    if choice == "1":
+        print("you buy 1")
+    if choice == "2":
+        print("you buy 2")
+
+
+pass
---
+++ packages/vmn/vmn-0.4.0rc13-py3-none-any.whl/version_stamp/version.py
@@ -0,0 +1,3 @@
+name = "vmn"
+version = "0.4.0-rc13"
+_version = "0.4.0-rc13"
