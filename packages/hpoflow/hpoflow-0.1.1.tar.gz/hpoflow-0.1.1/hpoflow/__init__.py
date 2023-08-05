68e6ccc2c4023d681a1ef4fc6cf55a3b8f740226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:43 +0000

    Add package hpoflow/0.1.1/hpoflow-0.1.1.tar.gz

---
+++ packages/hpoflow/hpoflow-0.1.1.tar.gz/hpoflow-0.1.1/hpoflow/__init__.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2021 Philip May
+# This software is distributed under the terms of the MIT license
+# which is available at https://opensource.org/licenses/MIT
+
+"""HPOflow main package."""
+
+import sys
+from typing import TYPE_CHECKING
+
+from lazy_imports import LazyImporter
+
+from hpoflow.version import __version__
+
+
+_import_structure = {
+    "mlflow": [
+        "normalize_mlflow_entry_name",
+        "normalize_mlflow_entry_names_in_dict",
+        "check_repo_is_dirty",
+    ],
+    "optuna": ["SignificanceRepeatedTrainingPruner"],
+    "optuna_mlflow": ["OptunaMLflow"],
+    "optuna_transformers": ["OptunaMLflowCallback"],
+    "utils": ["func_no_exception_caller"],
+}
+
+# Direct imports for type-checking
+if TYPE_CHECKING:
+    from hpoflow.mlflow import (  # noqa: F401
+        check_repo_is_dirty,
+        normalize_mlflow_entry_name,
+        normalize_mlflow_entry_names_in_dict,
+    )
+    from hpoflow.optuna import SignificanceRepeatedTrainingPruner  # noqa: F401
+    from hpoflow.optuna_mlflow import OptunaMLflow  # noqa: F401
+    from hpoflow.optuna_transformers import OptunaMLflowCallback  # noqa: F401
+    from hpoflow.utils import func_no_exception_caller  # noqa: F401
+else:
+    sys.modules[__name__] = LazyImporter(
+        __name__,
+        globals()["__file__"],
+        _import_structure,
+        extra_objects={"__version__": __version__},
+    )
