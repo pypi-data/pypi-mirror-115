72a10b9764621fb24c60638573bfa481dd5596e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:06 +0000

    Add package cartils/0.0.9/cartils-0.0.9.tar.gz

---
+++ packages/cartils/cartils-0.0.9.tar.gz/cartils-0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+with open("cartils/VERSION", "r") as f:
+    version = f.read()
+
+setuptools.setup(
+    name="cartils",
+    version=version,
+    author="John Carter",
+    author_email="jfcarter2358@gmail.com",
+    description="A collection of useful Python utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jfcarter2358/cartils",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        'colorama==0.4.4'
+    ]
+)
