b7da920df767ad71faff9569b4c7be2ed88d5126
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package vcard/0.14.1/vcard-0.14.1.tar.gz

---
+++ packages/vcard/vcard-0.14.1.tar.gz/vcard-0.14.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vcard']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil']
+
+setup_kwargs = {
+    'name': 'vcard',
+    'version': '0.14.1',
+    'description': 'vCard validator, class and utility functions',
+    'long_description': None,
+    'author': 'Victor Engmark',
+    'author_email': 'victor@engmark.name',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
