b15f5958e2fb828248e53fa99e0a69cc16e7b2ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:12 +0000

    Add package checkprime/0.0.1/checkprime-0.0.1.tar.gz

---
+++ packages/checkprime/checkprime-0.0.1.tar.gz/checkprime-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+    name="checkprime",                     
+    version="0.0.1",                        
+    author= "Bhupin Baral", 
+    url = 'https://github.com/bhupinbaral25/checkprime',      
+    #download_url ='https://github.com/bhupinbaral25/checkprime/releases/tag/0.0.1' ,            
+        
+    description="This nodules checks wheather your number is prime or not ",
+      
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                     
+    python_requires='>=3.6',               
+    py_modules=["checkprime"],           
+    
+      
+    install_requires=[]                    
+)
