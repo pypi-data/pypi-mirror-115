bd4876cbf1e7ba61b2212901c80ad6a8a8c24876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:12 +0000

    Add package migri/0.6.1/migri-0.6.1.tar.gz

---
+++ packages/migri/migri-0.6.1.tar.gz/migri-0.6.1/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.6.1"
+
+
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="migri",
+    version=VERSION,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    extras_requires={
+        "mysql": ["aiomysql"],
+        "postgresql": ["asyncpg"],
+        "sqlite": ["aiosqlite"],
+    },
+    install_requires=[
+        "asyncpg",  # TODO remove in 1.1.0
+        "click==7.*",
+        "sqlparse==0.4.*",
+    ],
+    entry_points={
+        "console_scripts": [
+            "migri = migri.main:main"
+        ]
+    },
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Database",
+    ]
+)
