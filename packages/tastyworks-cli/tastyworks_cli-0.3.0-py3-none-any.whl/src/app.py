dceeb8ff809c6eeae2472dcfe4b6c443024ab9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:17 +0000

    Add package tastyworks-cli/0.3.0/tastyworks_cli-0.3.0-py3-none-any.whl

---
+++ packages/tastyworks-cli/tastyworks_cli-0.3.0-py3-none-any.whl/src/app.py
@@ -0,0 +1,28 @@
+import asyncio
+import sys
+
+import asyncclick as click
+
+from .future.commands import future
+from .option.commands import option
+from .pairs.commands import pairs
+from .plot.commands import plot
+from .utils import VERSION
+
+
+@click.group()
+@click.version_option(VERSION)
+async def app():
+    pass
+
+
+def main():
+    if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    app.add_command(future)
+    app.add_command(option)
+    app.add_command(plot)
+    app.add_command(pairs)
+
+    app(_anyio_backend='asyncio')
