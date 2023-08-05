33eff30c24ae9d4c7e7e8165523207798ed7f25b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:26 +0000

    Add package ExGrads/0.1.10/ExGrads-0.1.10.tar.gz

---
+++ packages/ExGrads/ExGrads-0.1.10.tar.gz/ExGrads-0.1.10/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as fp:
+	long_description = fp.read()
+
+setup(
+	name='ExGrads',
+	packages=['exgrads'],
+	version='0.1.10',
+	license='MIT',
+	
+	install_requires=['torch'],
+
+	author='Takuo Hamaguchi',
+	author_email='nyahha@gmail.com',
+	
+	url='https://gitlab.com/takuo-h/examplewise-gradients',
+
+	description='calculate example-wise gradient',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	keywords='',
+
+	classifiers=[
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3.6',
+	],
+)
