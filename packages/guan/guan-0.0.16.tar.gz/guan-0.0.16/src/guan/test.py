6487be765feb1f42c06162a7d148fa3ea2ac2ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:15:32 +0000

    Add package guan/0.0.16/guan-0.0.16.tar.gz

---
+++ packages/guan/guan-0.0.16.tar.gz/guan-0.0.16/src/guan/test.py
@@ -0,0 +1,6 @@
+# GUAN is an open-source python package developed and maintained by https://www.guanjihuan.com. The primary location of this package is on website https://py.guanjihuan.com.
+
+# test
+
+def test():
+    print('\nSuccess in the installation of GUAN package!\n')
