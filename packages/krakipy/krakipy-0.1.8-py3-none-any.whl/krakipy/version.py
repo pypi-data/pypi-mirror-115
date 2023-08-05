05856b51e484a748ae3ffc050fbf45a84878d437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:15 +0000

    Add package krakipy/0.1.8/krakipy-0.1.8-py3-none-any.whl

---
+++ packages/krakipy/krakipy-0.1.8-py3-none-any.whl/krakipy/version.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.8"
+__author__ = "Hubertus Wilisch"
+__copyright__ = "Copyright 2020-2021 Hubertus Wilisch"
+__license__ = "GNU GPLv3+"
+__url__ = "https://krakipy.readthedocs.io/en/latest/"
