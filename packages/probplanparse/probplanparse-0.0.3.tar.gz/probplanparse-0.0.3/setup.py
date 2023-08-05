815530b6555c634b5f48fdd4a198f461b12b49bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:05 +0000

    Add package probplanparse/0.0.3/probplanparse-0.0.3.tar.gz

---
+++ packages/probplanparse/probplanparse-0.0.3.tar.gz/probplanparse-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='probplanparse',
+  version='0.0.3',
+  description='Parse report from prob plan in ext planner',
+  long_description='# PR Result \n A small library that parses through the report.txt file and converts to a `user-dict` made up of Hypothesis objects. This will use report.txt files from the `external-project`. \n Use `pip install pr-result` to install package.',
+  url='',
+  author='Tomo Bessho',
+  author_email='tomobessho018@gmail.com',
+  packages=find_packages(),
+  install_requires=['']
+)
