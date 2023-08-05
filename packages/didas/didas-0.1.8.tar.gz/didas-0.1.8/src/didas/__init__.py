655157e70f70f137f17483b04e470e56818836b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:29 +0000

    Add package didas/0.1.8/didas-0.1.8.tar.gz

---
+++ packages/didas/didas-0.1.8.tar.gz/didas-0.1.8/src/didas/__init__.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+import pandas as pd
+
+
+def dic(o):
+    pd.DataFrame()
+    r = defaultdict(set)
+    for d in dir(o):
+        a = getattr(o, d)
+        t = type(a)
+        r[t].add(d)
+    return dict(r)
+__version__ = "0.1.8"
