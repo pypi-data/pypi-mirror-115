a1d2596413fcad64321eb2b52892e8d3849ffe13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:32 +0000

    Add package yyx-tools/0.0.10/yyx_tools-0.0.10.tar.gz

---
+++ packages/yyx-tools/yyx_tools-0.0.10.tar.gz/yyx_tools-0.0.10/yyx_tools/list.py
@@ -0,0 +1,7 @@
+''' Author: yanyuxiangtoday '''
+
+def rm_elem(input_list, elem):
+    ''' rm elem from input_list if elem in input_list '''
+    if elem in input_list:
+        input_list.remove(elem)
+    return
