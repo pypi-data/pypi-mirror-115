0ab93a9c07ae2e4b59e1325796aba94a58080543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:34 +0000

    Add package pyright/0.0.7/pyright-0.0.7.tar.gz

---
+++ packages/pyright/pyright-0.0.7.tar.gz/pyright-0.0.7/pyright/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'pyright'
+__author__ = 'RobertCraigie'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Robert Craigie'
+__version__ = '0.0.7'
+
+
+import os
+from .cli import *
+
+
+if os.environ.get('PYRIGHT_PYTHON_DEBUG'):
+    import logging
+
+    logging.basicConfig(
+        format='%(asctime)-15s - %(levelname)s - %(name)s - %(message)s'
+    )
+    logging.getLogger('pyright').setLevel(logging.DEBUG)
