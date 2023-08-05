e0ebdffed6538223c529cdfa8488514b2b2abb93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:57 +0000

    Add package fro-AI/0.8.4/fro_AI-0.8.4-py3-none-any.whl

---
+++ packages/fro-AI/fro_AI-0.8.4-py3-none-any.whl/fro_AI/__init__.py
@@ -0,0 +1,7 @@
+from .face import FaceRecognizer
+from .face_detect import FaceDetector
+from .classifier import Classifier
+from .color import ColorTracker, ColorRecognizer
+from .Aruco import ArucoTracker
+
+__version__ = '0.8.4'
---
+++ packages/fro-AI/fro_AI-0.8.4-py3-none-any.whl/fro_AI/utils/__init__.py
@@ -0,0 +1,3 @@
+from .img_utils import RtpStream, bgr8_to_jpeg
+from .usb_camera import USBCamera
+from .webstream import WebStreamServer
