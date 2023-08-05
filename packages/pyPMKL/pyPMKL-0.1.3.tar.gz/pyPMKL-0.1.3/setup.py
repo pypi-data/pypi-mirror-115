306b178459fe948f3c1381c74e1c619c715a1996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:53 +0000

    Add package pyPMKL/0.1.3/pyPMKL-0.1.3.tar.gz

---
+++ packages/pyPMKL/pyPMKL-0.1.3.tar.gz/pyPMKL-0.1.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='pyPMKL',
+    version='0.1.3',
+    description='Tessellated Kernels SVM',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author='AT',
+    author_email='atalitck@asu.edu',
+    keywords=['Kernel learning', 'SVM', 'Multiple Kernel Learning'],
+    url='https://github.com/Talitsky/PMKL',
+    download_url='https://pypi.org/project/pyPMKL/'
+)
+
+install_requires = [
+    'numpy',
+    'scikit-learn',
+    'matplotlib',
+    'scipy',
+    'libsvm'
+]
+
+if __name__ == '__main__':
+    setup(**setup_args, install_requires=install_requires)
