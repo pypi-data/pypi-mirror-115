d911f180c1e8af76db2f51f0beba8e35f840fedf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:00 +0000

    Add package minos-microservice-common/0.1.8/minos_microservice_common-0.1.8.tar.gz

---
+++ packages/minos-microservice-common/minos_microservice_common-0.1.8.tar.gz/minos_microservice_common-0.1.8/minos/common/model/__init__.py
@@ -0,0 +1,52 @@
+"""
+Copyright (C) 2021 Clariteia SL
+
+This file is part of minos framework.
+
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+from .abc import (
+    Model,
+)
+from .declarative import (
+    Aggregate,
+    AggregateAction,
+    AggregateDiff,
+    AggregateRef,
+    Command,
+    CommandReply,
+    CommandStatus,
+    DeclarativeModel,
+    Event,
+    MinosModel,
+    ValueObject,
+)
+from .dynamic import (
+    BucketModel,
+    DataTransferObject,
+    DynamicModel,
+    FieldsDiff,
+)
+from .fields import (
+    Field,
+    ModelField,
+)
+from .serializers import (
+    AvroDataDecoder,
+    AvroDataEncoder,
+    AvroSchemaDecoder,
+    AvroSchemaEncoder,
+)
+from .types import (
+    Decimal,
+    Enum,
+    Fixed,
+    MissingSentinel,
+    ModelRef,
+    ModelRefExtractor,
+    ModelRefInjector,
+    ModelType,
+    NoneType,
+    TypeHintBuilder,
+    TypeHintComparator,
+)
