6e89f1d066ff95f5f5ec656a380936948867fe50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:13 +0000

    Add package cubicweb-registration/0.9.0/cubicweb_registration-0.9.0-py3-none-any.whl

---
+++ packages/cubicweb-registration/cubicweb_registration-0.9.0-py3-none-any.whl/cubicweb_registration/__pkginfo__.py
@@ -0,0 +1,28 @@
+# pylint: disable-msg=W0622
+"""cubicweb-registration application packaging information"""
+
+modname = "registration"
+distname = "cubicweb-registration"
+
+numversion = (0, 9, 0)
+version = ".".join(str(num) for num in numversion)
+
+license = "LGPL"
+description = "public registration component for the CubicWeb framework"
+author = "LOGILAB S.A. (Paris, FRANCE)"
+author_email = "contact@logilab.fr"
+web = "http://www.cubicweb.org/project/%s" % distname
+classifiers = [
+    "Environment :: Web Environment",
+    "Framework :: CubicWeb",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: JavaScript",
+]
+
+__depends__ = {
+    "cubicweb": ">= 3.27.0",
+    "cubicweb-forgotpwd": None,
+    "Pillow": None,
+    "pycrypto": None,
+}
