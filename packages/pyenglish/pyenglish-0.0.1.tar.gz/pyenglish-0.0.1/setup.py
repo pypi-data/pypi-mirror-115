5fd6eb3e9ec8f8ec0d31153187db17420bd7b11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:42 +0000

    Add package pyenglish/0.0.1/pyenglish-0.0.1.tar.gz

---
+++ packages/pyenglish/pyenglish-0.0.1.tar.gz/pyenglish-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="pyenglish",
+    version="0.0.1",
+    license='MIT',
+    author="Jung Ji-Hyo",
+    author_email="cord0318@gmail.com",
+    description="영어 공부를 도와주는 모듈.",
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
