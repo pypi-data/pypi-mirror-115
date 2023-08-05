05c9c198dc07720c226913c0e4a49cc6e4f6fc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package DistTest/1.0/DistTest-1.0.tar.gz

---
+++ packages/DistTest/DistTest-1.0.tar.gz/DistTest-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'DistTest',
+    version = '1.0',
+    author = 'Keith Cressman',
+    author_email = "keith.cressman@duke.edu",
+    packages = find_packages(),
+    url= "https://github.com/KeithCressman/test"
+)
