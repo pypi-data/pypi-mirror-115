83e194c40b78e909740053bf1fb3a775ef80d6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:48 +0000

    Add package feapder/1.6.2b1/feapder-1.6.2b1.tar.gz

---
+++ packages/feapder/feapder-1.6.2b1.tar.gz/feapder-1.6.2b1/tests/test.py
@@ -0,0 +1,65 @@
+# from feapder.utils import tools
+#
+# EMAIL_SENDER = "feapder@163.com"  # 发件人
+# EMAIL_PASSWORD = "YPVZHXFVVDPCJGTH"  # 授权码
+# EMAIL_RECEIVER = "564773807@qq.com"  # 收件人 支持列表，可指定多个
+# # 时间间隔
+# WARNING_INTERVAL = 3600  # 相同报警的报警时间间隔，防止刷屏
+# WARNING_LEVEL = "DEBUG"  # 报警级别， DEBUG / ERROR
+# EMAIL_SMTPSERVER="smtp.163.com"
+#
+#
+# tools.email_warning(
+#     message="test3",
+#     title="test",
+#     message_prefix=None,
+#     email_sender=EMAIL_SENDER,
+#     email_password=EMAIL_PASSWORD,
+#     email_receiver=EMAIL_RECEIVER,
+#     rate_limit=WARNING_INTERVAL,
+#     email_smtpserver=EMAIL_SMTPSERVER
+# )
+
+# from feapder.utils.tools import make_update_sql, make_insert_sql
+#
+#
+# if __name__ == "__main__":
+#     data = {
+#         "name": '''do"nt't''',
+#         "name2": "呵呵哒"
+#     }
+#     sql = make_insert_sql("test1", data)
+#     print(sql)
+#
+#     sql = make_update_sql("test1", data, condition="id=''1'")
+#     print(sql)
+
+import threading
+import time
+
+
+class Test(threading.Thread):
+    def run(self):
+        print(1)
+        time.sleep(3)
+        print(3)
+        self._started.clear()
+
+    def join(self, timeout=None):
+        if not self._started.is_set():
+            return
+
+        super().join()
+
+
+
+t1 = Test()
+t2 = Test()
+
+for i in range(2):
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+
+print("end")
