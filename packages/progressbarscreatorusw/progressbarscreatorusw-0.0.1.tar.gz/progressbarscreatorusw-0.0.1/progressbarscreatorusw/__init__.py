14a66851a0add42ab8784ea4bab456406ef75971
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:26 +0000

    Add package progressbarscreatorusw/0.0.1/progressbarscreatorusw-0.0.1.tar.gz

---
+++ packages/progressbarscreatorusw/progressbarscreatorusw-0.0.1.tar.gz/progressbarscreatorusw-0.0.1/progressbarscreatorusw/__init__.py
@@ -0,0 +1,7 @@
+from progressbarscreatorusw import Progressbar
+from progressbarscreatorusw import Marker
+from progressbarscreatorusw import Pgreen
+from progressbarscreatorusw import Porange
+from progressbarscreatorusw import Pred
+from progressbarscreatorusw import bcolors
+from progressbarscreatorusw import info
