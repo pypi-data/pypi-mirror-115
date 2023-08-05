c190b2731a1616a35ade571992cb5dc2e4a27085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package spacerocks/1.0.11/spacerocks-1.0.11.tar.gz

---
+++ packages/spacerocks/spacerocks-1.0.11.tar.gz/spacerocks-1.0.11/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, Extension
+
+import os
+import sys
+
+import sysconfig
+suffix = sysconfig.get_config_var('EXT_SUFFIX')
+if suffix is None:
+    suffix = ".so"
+
+extra_link_args=[]
+if sys.platform == 'darwin':
+    from distutils import sysconfig
+    vars = sysconfig.get_config_vars()
+    vars['LDSHARED'] = vars['LDSHARED'].replace('-bundle', '-shared')
+    extra_link_args=['-Wl,-install_name,@rpath/libspacerocks'+suffix]
+
+libspacerocksmodule = Extension('libspacerocks',
+                                sources=['src/speedy.c'],
+                                include_dirs=['src'],
+                                language='c',
+                                extra_compile_args=['-O3', '-fPIC', '-Wno-unknown-pragmas', '-std=c99'],
+                                extra_link_args=extra_link_args
+                                )
+
+
+setup(
+   name='spacerocks',
+   version='1.0.11',
+   description='A Python Package for Solar System Ephemerides and Dynamics.',
+   author='Kevin J. Napier',
+   author_email='kjnapier@umich.edu',
+   url="https://github.com/kjnapier/spacerocks",
+   packages=['spacerocks'],
+   # I'm not sure package_data is working.
+   package_data={'spacerocks': ['data/observatories.csv']},
+   install_requires=['healpy',
+                     'numpy',
+                     'skyfield',
+                     'astropy',
+                     'pandas',
+                     'rebound',
+                     'reboundx'],
+   include_package_data=True,
+   ext_modules=[libspacerocksmodule],
+   zip_safe=False
+)
