064d10082d96ee660cfa4ab819b7e44cdb3a9788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:24 +0000

    Add package Gaus-Bino-distributions-joyb-1/1.1/Gaus-Bino-distributions-joyb-1-1.1.tar.gz

---
+++ packages/Gaus-Bino-distributions-joyb-1/Gaus-Bino-distributions-joyb-1-1.1.tar.gz/Gaus-Bino-distributions-joyb-1-1.1/Gaus-Bino-distributions-joyb-1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Gaus-Bino-distributions-joyb-1',
+      version='1.1',
+      description='Gaussian distributions and Binomial Distribution ',
+      packages=['Gaus-Bino-distributions-joyb-1'],
+      author = 'Joy Bhowmick',
+      zip_safe=False)
