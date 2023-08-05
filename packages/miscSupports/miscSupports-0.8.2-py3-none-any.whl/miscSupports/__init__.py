c13ce069d1487d8f09ad11f2868b292f83785aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:09 +0000

    Add package miscSupports/0.8.2/miscSupports-0.8.2-py3-none-any.whl

---
+++ packages/miscSupports/miscSupports-0.8.2-py3-none-any.whl/miscSupports/__init__.py
@@ -0,0 +1,6 @@
+from miscSupports.alter_array import *
+from miscSupports.misc import *
+from miscSupports.file_support import *
+from miscSupports.shapely_support import *
+from miscSupports.dates_and_time import *
+from miscSupports.byte_supports import *
---
+++ packages/miscSupports/miscSupports-0.8.2-py3-none-any.whl/miscSupports/misc.py
@@ -0,0 +1,110 @@
+from contextlib import contextmanager
+from distutils.util import strtobool
+import sys
+import os
+
+
+def parse_as_numeric(value, return_type=int):
+    """
+    Try isolating converting to return type, if a ValueError Arises then return zero.
+    """
+    try:
+        return return_type(value)
+    except ValueError:
+        return 0
+
+
+def index_modifier(index, modifier, index_mod=0):
+    """
+    Modify and index based on the sub division, whilst also allow to modify the current index by an int
+    """
+    return (index + index_mod) + ((index + index_mod) * modifier)
+
+
+def index_range(index, modifier, inclusive=True):
+    """
+    Create the iterable range of values from the current index base on a modifier
+
+    Example
+    --------
+    index = 0
+    modifier = 3
+
+    0 + 0 * 3 = 0
+
+    The plus 1 is their because range is NOT inclusive yet we need it to be, can be turned off
+    (1 + (1 * 3)) + 1 = 5
+    """
+
+    if inclusive:
+        return range(index_modifier(index, modifier), index_modifier(index, modifier, 1) + 1)
+    else:
+        return range(index_modifier(index, modifier), index_modifier(index, modifier, 1))
+
+
+def string_to_bool(value):
+    """
+    This will convert a string to a bool if it is a str, return the bool if it was a bool, and raise a TypeError
+    otherwise
+    """
+
+    if isinstance(value, bool):
+        return value
+    elif isinstance(value, str):
+        return bool(strtobool(value))
+    else:
+        raise TypeError(f"string_to_bool expects a bool or str yet was passed {type(value)}")
+
+
+@contextmanager
+def suppress_stdout():
+    """
+    Suppress print statements
+
+    Note
+    ----
+    Sometimes external packages will prints every step which can end up cluttering .log's or terminal output. This will
+    silence it.
+
+    Source: https://stackoverflow.com/questions/2125702/how-to-suppress-console-output-in-python
+
+    Example
+    -------
+    >>> print("You can't silence me!")
+    >>> with suppress_stdout():
+    >>>     print("...or maybe not")
+
+
+
+    """
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
+def sep_num(number, space=True):
+    """
+    Creates a string representation of a number with separators each thousand. If space is True, then it uses spaces for
+    the separator otherwise it will use commas
+
+    Note
+    ----
+    Source: https://stackoverflow.com/questions/16670125/python-format-string-thousand-separator-with-spaces
+
+    :param number: A number
+    :type number: int | float
+
+    :param space: Separates numbers with spaces if True, else with commas
+    :type space: bool
+
+    :return: string representation with space separation
+    :rtype: str
+    """
+    if space:
+        return '{:,}'.format(number).replace(',', ' ')
+    else:
+        return '{:,}'.format(number)
