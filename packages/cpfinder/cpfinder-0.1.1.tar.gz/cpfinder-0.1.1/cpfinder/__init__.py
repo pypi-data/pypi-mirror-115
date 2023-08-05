21840fdd3988baacc0992c990fb0375a0d0f94f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:05:57 +0000

    Add package cpfinder/0.1.1/cpfinder-0.1.1.tar.gz

---
+++ packages/cpfinder/cpfinder-0.1.1.tar.gz/cpfinder-0.1.1/cpfinder/__init__.py
@@ -0,0 +1,69 @@
+from cpfinder.utils import *
+
+
+# Various feature engineering functions
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import animation
+
+
+class cpfinder:
+    def __init__(self, data: np.ndarray, annotations=None, method: str = "bocpd"):
+        """
+        Finds the changepoints with various methods.
+
+        Args:
+            data (np.ndarray): Time series that changepoint detector will work on
+            method (str): A method to find changepoint including; bocpd (as Bayesian Online Chanpoint Detection), rulsif (as online RuLSif)..
+        """
+        self.methodString = method
+        self.data = data
+        self.annotations = annotations
+
+    def saveAnimationVideo(self, fname):
+        writergif = animation.PillowWriter(fps=1)
+        self.animationVideo.save(fname, writer=writergif)
+
+    def fit(
+        self,
+        animationFlag: bool,
+        interval: int = None,
+        plotFlag: bool = False,
+        model_parameters: dict = {"auto": True},
+    ):
+        """Fit the given data on model
+
+        Args:
+            animationFlag (bool): Whether return results animation or not
+            interval (int): Animation variable to use to determine how much sample in each step
+            plotFlag (bool): Whether plot animation or not
+            model_parameters (dict): Model paramters for given method
+
+            Example for bocpd:
+
+            model_parameters = {
+                "hazard" : 1 / 100,
+                "mean0" = 0,
+                "var0" = 1,
+                "varx" = 2,
+                "ii" = 3 ,
+            }
+
+            * These are default parameters if you want to use this simply no need to define anything for `model_parameters` argument.
+
+            Example for rulsif:
+
+
+        """
+
+        # Given method to function
+        method = str_to_func(self.methodString)
+
+        detector = method(model_parameters)
+
+        results = detector.fit(self.data, interval, animationFlag, plotFlag)
+        if plotFlag:
+            plt.show()
+        self.animationVideo = results if animationFlag else None
+        self.changepoints = results if not animationFlag else None
---
+++ packages/cpfinder/cpfinder-0.1.1.tar.gz/cpfinder-0.1.1/cpfinder/methods/__init__.py
@@ -0,0 +1,112 @@
+from matplotlib import pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
+import roerich
+from . import online_changepoint_detection as oncd
+from . import online_rulsif as orul
+from cpfinder.feature_engineering import _get_cps_from_R
+from cpfinder.vis import plot_matplotlib
+
+
+class bocpd:
+    """
+    Creates animatation of the Bayesian Online Changepoint Detection by
+    matplotlib.animation package.
+
+    Org. Paper:
+        https://arxiv.org/abs/0710.3742
+
+    """
+
+    def __init__(self, args):
+        if args["auto"]:
+            self.hazard = 1 / 100
+            self.mean0 = 0
+            self.var0 = 1
+            self.varx = 2
+            self.ii = 3
+        else:
+            self.hazard = args["hazard"]
+            self.mean0 = args["mean0"]
+            self.var0 = args["var0"]
+            self.varx = args["varx"]
+            self.ii = args["ii"]
+
+    def fit(self, data, interval, animationFlag, plotFlag, annots=[]):
+
+        # Create model
+        model = oncd.GaussianUnknownMean(self.mean0, self.var0, self.varx)
+
+        if animationFlag == True:
+            # Plot
+            animation = oncd.online_changepoint_detection_animation(
+                data, model, self.hazard, interval, self.ii
+            )
+            if plotFlag:
+                plt.show()
+            return animation
+        else:
+            R, pmean, pvar = oncd.online_changepoint_detection(
+                data, model, self.hazard, self.ii
+            )
+            cps = _get_cps_from_R(R, self.ii)
+            if plotFlag:
+                plot_matplotlib(data, np.arange(0, len(data)), R, pmean, pvar, annots)
+            return cps
+
+
+class rulsif:
+    """
+    Creates animatation of the Generalization of Change-Point Detection
+    in Time Series Data Based on Direct Density Ratio Estimation by
+    matplotlib.animation package.
+
+    Org. Paper:
+        https://arxiv.org/abs/2001.06386
+
+    """
+
+    def __init__(self, args):
+        if args["auto"]:
+            self.model = roerich.OnlineNNRuLSIF(
+                net="default",
+                scaler="default",
+                metric="KL_sym",
+                periods=1,
+                window_size=10,
+                lag_size=500,
+                step=10,
+                n_epochs=10,
+                lr=0.1,
+                lam=0.0001,
+                optimizer="Adam",
+                alpha=0.001,
+            )
+        else:
+            self.model = roerich.OnlineNNRuLSIF(*args)
+
+    def fit(self, data, interval, animationFlag, plotFlag):
+        T = np.arange(len(data))
+
+        # Create
+        d = data.flatten()
+        try:
+            score, peaks = self.model.predict(d)
+        except ValueError or TypeError:
+            peaks = []
+
+        # if animationFlag:
+        #     fig = plt.figure(figsize=(12, 20))
+        #     ani = FuncAnimation(
+        #         fig,
+        #         orul.animate_rulsif,
+        #         np.arange(500, len(data), 500),
+        #         fargs=(data, [], self.model),
+        #     )
+
+        if plotFlag == True:
+            data = data.reshape(-1, 1)
+            roerich.display(data, T, [], score, T, peaks=peaks)
+            plt.show()
+
+        return peaks
