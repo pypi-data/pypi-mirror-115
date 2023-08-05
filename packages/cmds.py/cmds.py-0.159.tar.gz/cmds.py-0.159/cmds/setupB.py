c27a8bd3b37ecdd4618671ecd5e16c45d7513421
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:01 +0000

    Add package cmds.py/0.159/cmds.py-0.159.tar.gz

---
+++ packages/cmds.py/cmds.py-0.159.tar.gz/cmds.py-0.159/cmds/setupB.py
@@ -0,0 +1,10 @@
+# import setupB as F
+### exec(open( "/content/檔案名.py", encoding = 'utf-8').read(),{"site":site}) 
+### exec ####
+
+if ("builtins"==__name__):
+    print("@!!! 1:::",siteOP())
+    print("@!!! 2:::",siteOP())
+
+
+
