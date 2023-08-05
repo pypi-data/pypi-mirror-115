6661515e48acca5a65055650cf6ebcd5cf2fa2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:51 +0000

    Add package syn-reports/0.0.14/syn_reports-0.0.14-py2.py3-none-any.whl

---
+++ packages/syn-reports/syn_reports-0.0.14-py2.py3-none-any.whl/syn_reports/commands/benefactor_permissions_report/cli.py
@@ -0,0 +1,20 @@
+from .benefactor_permissions_report import BenefactorPermissionsReport
+
+
+def create(subparsers, parents):
+    parser = subparsers.add_parser('benefactor-permissions',
+                                   parents=parents,
+                                   help='Report the unique permissions on a Synapse entity and all its child entities.')
+    parser.add_argument('entities',
+                        nargs='+',
+                        help='The IDs and/or names of the entities to report on.')
+    parser.add_argument('-o', '--out-path', default=None,
+                        help='Path to export the report to. Specify a path that ends in ".csv" to export to a specific file otherwise a timestamped filename will be created in the out-path.')
+    parser.set_defaults(_execute=execute)
+
+
+def execute(args):
+    return BenefactorPermissionsReport(
+        args.entities,
+        out_path=args.out_path
+    ).execute()
