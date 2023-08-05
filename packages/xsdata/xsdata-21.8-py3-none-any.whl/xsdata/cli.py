3faa3af1b8ee52c5002c74622752a1ed65e3af17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:07 +0000

    Add package xsdata/21.8/xsdata-21.8-py3-none-any.whl

---
+++ packages/xsdata/xsdata-21.8-py3-none-any.whl/xsdata/cli.py
@@ -0,0 +1,211 @@
+import logging
+import sys
+from pathlib import Path
+from typing import Any
+from typing import Iterator
+
+import click
+import click_log
+from click_default_group import DefaultGroup
+
+from xsdata import __version__
+from xsdata.codegen.transformer import SchemaTransformer
+from xsdata.codegen.writer import CodeWriter
+from xsdata.logger import logger
+from xsdata.models.config import DocstringStyle
+from xsdata.models.config import GeneratorConfig
+from xsdata.models.config import OutputFormat
+from xsdata.models.config import StructureStyle
+from xsdata.utils.downloader import Downloader
+from xsdata.utils.hooks import load_entry_points
+
+load_entry_points("xsdata.plugins.cli")
+
+outputs = click.Choice(CodeWriter.generators.keys())
+docstring_styles = click.Choice([x.value for x in DocstringStyle])
+structure_styles = click.Choice([x.value for x in StructureStyle])
+click_log.basic_config(logger)
+
+
+@click.group(cls=DefaultGroup, default="generate", default_if_no_args=False)
+@click.version_option(__version__)
+@click_log.simple_verbosity_option(logger)
+def cli():
+    """xsdata command line interface."""
+
+
+@cli.command("init-config")
+@click.argument("output", type=click.Path(), default=".xsdata.xml")
+@click.option("-pp", "--print", is_flag=True, default=False, help="Print output")
+def init_config(**kwargs: Any):
+    """Create or update a configuration file."""
+
+    if kwargs["print"]:
+        logger.setLevel(logging.ERROR)
+
+    file_path = Path(kwargs["output"])
+    if file_path.exists():
+        config = GeneratorConfig.read(file_path)
+        logger.info("Updating configuration file %s", kwargs["output"])
+    else:
+        logger.info("Initializing configuration file %s", kwargs["output"])
+        config = GeneratorConfig.create()
+
+    if kwargs["print"]:
+        config.write(sys.stdout, config)
+    else:
+        with file_path.open("w") as fp:
+            config.write(fp, config)
+
+
+@cli.command("download")
+@click.argument("source", required=True)
+@click.option(
+    "-o",
+    "--output",
+    type=click.Path(resolve_path=True),
+    default="./",
+    help="Output directory, default cwd",
+)
+def download(source: str, output: str):
+    """Download a schema or a definition locally with all its dependencies."""
+    downloader = Downloader(output=Path(output).resolve())
+    downloader.wget(source)
+
+
+@cli.command("generate")
+@click.argument("source", required=True)
+@click.option(
+    "-c",
+    "--config",
+    default=".xsdata.xml",
+    help="Specify a configuration file with advanced options.",
+)
+@click.option(
+    "-p",
+    "--package",
+    required=False,
+    help=(
+        "Specify the target package to be created inside the current working directory "
+        "Default: generated"
+    ),
+    default="generated",
+)
+@click.option(
+    "-o",
+    "--output",
+    type=outputs,
+    help=(
+        "Specify the output format from the builtin code generator and any third "
+        "party installed plugins. Default: dataclasses"
+    ),
+    default="dataclasses",
+)
+@click.option(
+    "-ds",
+    "--docstring-style",
+    type=docstring_styles,
+    help=(
+        "Specify the docstring style for the default output format. "
+        "Default: reStructuredText"
+    ),
+    default="reStructuredText",
+)
+@click.option(
+    "-ss",
+    "--structure-style",
+    type=structure_styles,
+    help=(
+        "Specify a structure style to organize classes "
+        "Default: filenames"
+        "\n\n"
+        "filenames: groups classes by the schema location"
+        "\n\n"
+        "namespaces: group classes by the target namespace"
+        "\n\n"
+        "clusters: group by strong connected dependencies"
+        "\n\n"
+        "namespace-clusters: group by strong connected dependencies and namespaces"
+        "\n\n"
+        "single-package: group all classes together"
+    ),
+    default="filenames",
+)
+@click.option(
+    "-cf",
+    "--compound-fields",
+    is_flag=True,
+    default=False,
+    help=(
+        "Use compound fields for repeating choices in order to maintain elements "
+        "ordering between data binding operations."
+    ),
+)
+@click.option(
+    "-ri",
+    "--relative-imports",
+    is_flag=True,
+    default=False,
+    help="Enable relative imports",
+)
+@click.option(
+    "-pp",
+    "--print",
+    is_flag=True,
+    default=False,
+    help="Print to console instead of writing the generated output to files",
+)
+def generate(**kwargs: Any):
+    """
+    Generate code from xml schemas, webservice definitions and any xml or json
+    document.
+
+    The input source can be either a filepath, uri or a directory
+    containing xml, json, xsd and wsdl files.
+    """
+    if kwargs["print"]:
+        logger.setLevel(logging.ERROR)
+
+    config_file = Path(kwargs["config"])
+    if config_file.exists():
+        config = GeneratorConfig.read(config_file)
+        if kwargs["package"] != "generated":
+            config.output.package = kwargs["package"]
+    else:
+        config = GeneratorConfig()
+        config.output.format = OutputFormat(value=kwargs["output"])
+        config.output.package = kwargs["package"]
+        config.output.relative_imports = kwargs["relative_imports"]
+        config.output.compound_fields = kwargs["compound_fields"]
+        config.output.docstring_style = DocstringStyle(kwargs["docstring_style"])
+
+    if kwargs["structure_style"] != StructureStyle.FILENAMES.value:
+        config.output.structure = StructureStyle(kwargs["structure_style"])
+
+    if kwargs["output"] != "dataclasses":
+        config.output.format.value = kwargs["output"]
+
+    if kwargs["relative_imports"]:
+        config.output.relative_imports = True
+
+    uris = resolve_source(kwargs["source"])
+    transformer = SchemaTransformer(config=config, print=kwargs["print"])
+    transformer.process(list(uris))
+
+
+def resolve_source(source: str) -> Iterator[str]:
+    if source.find("://") > -1 and not source.startswith("file://"):
+        yield source
+    else:
+        path = Path(source).resolve()
+        if path.is_dir():
+            yield from (x.as_uri() for x in path.glob("*.wsdl"))
+            yield from (x.as_uri() for x in path.glob("*.xsd"))
+            yield from (x.as_uri() for x in path.glob("*.xml"))
+            yield from (x.as_uri() for x in path.glob("*.json"))
+        else:  # is file
+            yield path.as_uri()
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli()
---
+++ packages/xsdata/xsdata-21.8-py3-none-any.whl/xsdata/formats/dataclass/parsers/handlers/native.py
@@ -0,0 +1,219 @@
+import functools
+from typing import Any
+from typing import Dict
+from typing import Iterable
+from typing import Iterator
+from typing import Optional
+from typing import Tuple
+from typing import Type
+from urllib.parse import urljoin
+from xml import sax
+from xml.etree import ElementInclude as xinclude
+from xml.etree import ElementTree as etree
+
+from xsdata.exceptions import XmlHandlerError
+from xsdata.formats.dataclass.parsers.mixins import PushParser
+from xsdata.formats.dataclass.parsers.mixins import SaxHandler
+from xsdata.formats.dataclass.parsers.mixins import XmlHandler
+from xsdata.models.enums import EventType
+from xsdata.utils import namespaces
+from xsdata.utils.namespaces import build_qname
+
+EVENTS = (EventType.START, EventType.END, EventType.START_NS)
+
+
+class XmlEventHandler(XmlHandler):
+    """
+    Event handler based on :func:`xml.etree.ElementTree.iterparse` api.
+
+    :param parser: The parser instance to feed with events
+    :param clazz: The target binding model, auto located if omitted.
+    """
+
+    __slots__ = ()
+
+    def parse(self, source: Any) -> Any:
+        """
+        Parse an XML document from a system identifier or an InputSource or
+        directly from an xml Element or ElementTree.
+
+        When source is an Element or ElementTree the handler will walk
+        over the objects structure.
+
+        When source is a system identifier or an InputSource the parser
+        will ignore comments and recover from errors.
+
+        When config process_xinclude is enabled the handler will parse
+        the whole document and then walk down the element tree.
+        """
+
+        if isinstance(source, etree.ElementTree):
+            source = source.getroot()
+
+        if isinstance(source, etree.Element):
+            ctx = iterwalk(source, {})
+        elif self.parser.config.process_xinclude:
+            root = etree.parse(source).getroot()  # nosec
+            base_url = get_base_url(self.parser.config.base_url, source)
+            loader = functools.partial(xinclude_loader, base_url=base_url)
+
+            xinclude.include(root, loader=loader)
+            ctx = iterwalk(root, {})
+        else:
+            ctx = etree.iterparse(source, EVENTS)  # nosec
+
+        return self.process_context(ctx)
+
+    def process_context(self, context: Iterable) -> Any:
+        """Iterate context and push the events to main parser."""
+        ns_map: Dict = {}
+        for event, element in context:
+            if event == EventType.START:
+                self.parser.start(
+                    self.clazz,
+                    self.queue,
+                    self.objects,
+                    element.tag,
+                    element.attrib,
+                    self.merge_parent_namespaces(ns_map),
+                )
+                ns_map = {}
+            elif event == EventType.END:
+                self.parser.end(
+                    self.queue,
+                    self.objects,
+                    element.tag,
+                    element.text,
+                    element.tail,
+                )
+                element.clear()
+            elif event == EventType.START_NS:
+                prefix, uri = element
+                ns_map[prefix or None] = uri
+            else:
+                raise XmlHandlerError(f"Unhandled event: `{event}`.")
+
+        return self.objects[-1][1] if self.objects else None
+
+
+class XmlSaxHandler(SaxHandler, sax.handler.ContentHandler):
+    """
+    Sax content handler based on native python.
+
+    :param parser: The parser instance to feed with events
+    :param clazz: The target binding model, auto located if omitted.
+    """
+
+    __slots__ = ()
+
+    def __init__(self, parser: PushParser, clazz: Optional[Type]):
+        super().__init__(parser, clazz)
+        self.ns_map: Dict = {}
+
+    def parse(self, source: Any) -> Any:
+        """
+        Parse an XML document from a system identifier or an InputSource.
+
+        :raises XmlHandlerError: If process xinclude config is enabled.
+        """
+        if self.parser.config.process_xinclude:
+            raise XmlHandlerError(
+                f"{type(self).__name__} doesn't support xinclude elements."
+            )
+
+        parser = sax.make_parser()  # nosec
+        parser.setFeature(sax.handler.feature_namespaces, True)
+        parser.setContentHandler(self)
+        parser.parse(source)
+        return self.close()
+
+    def startElementNS(self, name: Tuple[Optional[str], str], qname: Any, attrs: Dict):
+        """
+        Start element notification receiver.
+
+        The receiver will flush any previous active element, append a
+        new data frame to collect data content for the next active
+        element and notify the main parser to prepare for next binding
+        instruction.
+
+        Converts name and attribute keys to fully qualified tags to
+        respect the main parser api, eg (foo, bar) -> {foo}bar
+
+        :param name: Namespace-name tuple
+        :param qname: Not used
+        :param attrs: Attribute key-value map
+        """
+        attrs = {build_qname(key[0], key[1]): value for key, value in attrs.items()}
+        self.start(build_qname(name[0], name[1]), attrs, self.ns_map)
+        self.ns_map = {}
+
+    def endElementNS(self, name: Tuple, qname: Any):
+        """
+        End element notification receiver.
+
+        The receiver will flush any previous active element and set
+        the next element to be flushed.
+
+        Converts name and attribute keys to fully qualified tags to
+        respect the ain parser api, eg (foo, bar) -> {foo}bar
+
+        :param name: Namespace-name tuple
+        :param qname: Not used
+        """
+        self.end(build_qname(name[0], name[1]))
+
+    def characters(self, content: str):
+        """
+        Proxy for the data notification receiver.
+
+        :param content: Text or tail content
+        """
+        self.data(content)
+
+    def startPrefixMapping(self, prefix: str, uri: Optional[str]):
+        """
+        Start element prefix-URI namespace mapping.
+
+        :param prefix: Namespace prefix
+        :param uri: Namespace uri
+        """
+        self.ns_map[prefix] = uri or ""
+
+
+def iterwalk(element: etree.Element, ns_map: Dict) -> Iterator[Tuple[str, Any]]:
+    """
+    Walk over the element tree structure and emit start-ns/start/end events.
+
+    The ElementTree doesn't preserve the original namespace prefixes, we
+    have to generate new ones.
+    """
+    uri = namespaces.target_uri(element.tag)
+    if uri is not None:
+        prefix = namespaces.load_prefix(uri, ns_map)
+        yield EventType.START_NS, (prefix, uri)
+
+    yield EventType.START, element
+
+    for child in element:
+        yield from iterwalk(child, ns_map)
+
+    yield EventType.END, element
+
+
+def get_base_url(base_url: Optional[str], source: Any) -> Optional[str]:
+
+    if base_url:
+        return base_url
+
+    return source if isinstance(source, str) else None
+
+
+def xinclude_loader(
+    href: str,
+    parse: str,
+    encoding: Optional[str] = None,
+    base_url: Optional[str] = None,
+) -> Any:
+    """Custom loader for xinclude to support base_url argument that doesn't
+    exist for python < 3.9."""
+    return xinclude.default_loader(urljoin(base_url or "", href), parse, encoding)
