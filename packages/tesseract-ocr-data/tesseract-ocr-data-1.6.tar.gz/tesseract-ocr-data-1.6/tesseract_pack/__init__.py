02f397e4c464bffeceb4779f8cc3150769ddcf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:36 +0000

    Add package tesseract-ocr-data/1.6/tesseract-ocr-data-1.6.tar.gz

---
+++ packages/tesseract-ocr-data/tesseract-ocr-data-1.6.tar.gz/tesseract-ocr-data-1.6/tesseract_pack/__init__.py
@@ -0,0 +1,7 @@
+import os
+"""
+Framework for the tesseract-ocr,(https://tesseract-ocr.github.io/)
+import this package.data_here and
+use the tesseract-ocr files.
+"""
+data_here = os.path.dirname(os.path.abspath(__file__))
