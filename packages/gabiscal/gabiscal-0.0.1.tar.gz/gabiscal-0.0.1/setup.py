7299f8ef4c612572d0797b327d0f4db36db76756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:39 +0000

    Add package gabiscal/0.0.1/gabiscal-0.0.1.tar.gz

---
+++ packages/gabiscal/gabiscal-0.0.1.tar.gz/gabiscal-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages, version
+import setuptools
+
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Intended Audience :: Developers',
+]
+
+setup(
+    name='gabiscal',
+    version='0.0.1',
+    description ='Very basic calculator',
+    long_description='',
+    url='',
+    author='Gabriel Renom',
+    author_email='',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requieres=[''])
