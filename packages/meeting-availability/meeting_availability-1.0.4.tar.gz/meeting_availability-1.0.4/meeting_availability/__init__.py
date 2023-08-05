040bc05bc4bf6426417b8f1f9eb8a22c239e4a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:20 +0000

    Add package meeting-availability/1.0.4/meeting_availability-1.0.4.tar.gz

---
+++ packages/meeting-availability/meeting_availability-1.0.4.tar.gz/meeting_availability-1.0.4/meeting_availability/__init__.py
@@ -0,0 +1,1 @@
+from .meeting_availability import *
---
+++ packages/meeting-availability/meeting_availability-1.0.4.tar.gz/meeting_availability-1.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setuptools.setup(
+    name="meeting_availability",
+    version="1.0.4",
+    description="A module that allows you to know available time blocks for two people to meet.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="Adrián Moreno",
+    author_email="adrianmn90@gmail.com",
+    license=license,
+    url="https://github.com/adriwicked/meeting-availability",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['meeting_availability'],
+)
