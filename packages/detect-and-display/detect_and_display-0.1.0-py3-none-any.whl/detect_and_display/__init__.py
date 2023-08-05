cf87dde4d4b41c641a9fef6546b2826d8205cf8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package detect-and-display/0.1.0/detect_and_display-0.1.0-py3-none-any.whl

---
+++ packages/detect-and-display/detect_and_display-0.1.0-py3-none-any.whl/detect_and_display/__init__.py
@@ -0,0 +1,11 @@
+__version__ = '0.1.0'
+
+
+""" Top level package for the detect_and_display """
+
+__author__ = """ Patricio Tula and Agustin Tedesco """
+__version__ = '0.1.0'
+
+from detect_and_display.detect_and_display import DetectAndDisplay
+
+__all__ = ['DetectAndDisplay']
