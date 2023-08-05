9fccca350eb79b5abe01628747e396a09b502754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:32 +0000

    Add package qaeval/0.1.0/qaeval-0.1.0.tar.gz

---
+++ packages/qaeval/qaeval-0.1.0.tar.gz/qaeval-0.1.0/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+# version.py defines the VERSION variable.
+# We use exec here so we don't import qaeval whilst setting up.
+VERSION = {}
+with open('qaeval/version.py', 'r') as version_file:
+    exec(version_file.read(), VERSION)
+
+setuptools.setup(
+    name='qaeval',
+    version=VERSION['VERSION'],
+    author='Daniel Deutsch',
+    description='A package for evaluating the content of summaries through question-answering',
+    url='https://github.com/danieldeutsch/qaeval',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'allennlp==1.1.0',
+        'click==7.1.2',
+        'edlib',
+        'spacy==2.2.4',
+        'torch==1.6.0',
+        'transformers==3.0.2',
+        'urllib3>=1.25.10'
+    ]
+)
