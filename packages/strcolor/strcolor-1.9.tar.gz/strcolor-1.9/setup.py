c9d03e0f5f15ddd4a49258477a28ee01fd2be4bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:02 +0000

    Add package strcolor/1.9/strcolor-1.9.tar.gz

---
+++ packages/strcolor/strcolor-1.9.tar.gz/strcolor-1.9/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='strcolor',
+    version='1.9',
+    author='dugudongfangshuo',
+    author_email='2310775260@qq.com',
+    url='https://github.com/dugudongfangshuo',
+    description=u'一个用来进行颜色打印的便捷工具',
+    license='MIT',
+    packages=['strcolor'],
+    install_requires=[],
+)
