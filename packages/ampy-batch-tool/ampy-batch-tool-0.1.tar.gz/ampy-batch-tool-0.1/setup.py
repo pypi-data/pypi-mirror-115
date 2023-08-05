c202ca3a2a104e3931bb8508cb017f2364c89cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:15 +0000

    Add package ampy-batch-tool/0.1/ampy-batch-tool-0.1.tar.gz

---
+++ packages/ampy-batch-tool/ampy-batch-tool-0.1.tar.gz/ampy-batch-tool-0.1/setup.py
@@ -0,0 +1,25 @@
+import ab
+from setuptools import setup
+
+
+setup(
+	name = 'ampy-batch-tool',
+	packages = ['ab'],
+	version = ab.__version__,
+	author = ab.__author__,
+	author_email = ab.__email__,
+	description = 'An adafruit-ampy batch tool',
+	url = 'https://gitee.com/walkline/a-batch-tool',
+	license = 'MIT',
+	provides = ['ab'],
+	entry_points = {
+		'console_scripts': [
+			'ab = ab.__main__:main'
+		],
+	},
+	install_requires = [
+		'pyserial',
+		'adafruit-ampy',
+		'pyminifier'
+	]
+)
