b29c2d0d1bf635d8c5f79af5c0a74103882379a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:27 +0000

    Add package indico-plugin-topmenuextender/3.0.1/indico-plugin-topmenuextender-3.0.1.tar.gz

---
+++ packages/indico-plugin-topmenuextender/indico-plugin-topmenuextender-3.0.1.tar.gz/indico-plugin-topmenuextender-3.0.1/setup.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2021  Björn Pedersen
+#
+# This Indico plugin is free software; you can redistribute
+# them and/or modify them under the terms of the MIT License; see
+# the LICENSE file for more details.
+
+from __future__ import unicode_literals
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='indico-plugin-topmenuextender',
+    license='MIT',
+    author='MLZ indico team',
+    author_email='bjoern.pedersen@frm2.tum.de',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    use_scm_version={"write_to":"indico_topmenu_extender/version.py",
+                     "local_scheme":"node-and-timestamp"},
+    setup_requires = ["setuptools>=39", "setuptools_scm[toml]>=3.4"],
+    install_requires=[
+        'indico>=3'
+    ],
+    entry_points={
+        'indico.plugins': {'topmenu_extender=indico_topmenu_extender.plugin:TopMenuExtenderPlugin',
+                          }
+    },
+    classifiers=[
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
