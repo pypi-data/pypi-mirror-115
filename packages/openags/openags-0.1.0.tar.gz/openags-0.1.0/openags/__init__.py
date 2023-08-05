0ff64990360c8981687ac069b830b07ca52fcbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:03 +0000

    Add package openags/0.1.0/openags-0.1.0.tar.gz

---
+++ packages/openags/openags-0.1.0.tar.gz/openags-0.1.0/openags/__init__.py
@@ -0,0 +1,5 @@
+from backend import ActivationAnalysis, ROI
+from baseClasses import Model, Peak, Background, StandardPeak, BoronPeak
+from evaluators import MassSensEval
+from models import LinearBackground, QuadraticBackground, ArctanBackground, GaussianPeak, KuboSakaiBoronPeak
+from parsers import SpectrumParser, StandardsFileParser, ExcelWriter, CSVWriter
