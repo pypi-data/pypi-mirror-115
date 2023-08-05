2bb4039a81c75ab878da2a85c32cedefd9d86c2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package pyagrum-nightly/0.21.0.9.dev202108041628009378/pyAgrum_nightly-0.21.0.9.dev202108041628009378-cp38-cp38-win32.whl

---
+++ packages/pyagrum-nightly/pyAgrum_nightly-0.21.0.9.dev202108041628009378-cp38-cp38-win32.whl/pyAgrum/lib/classifier.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+# (c) Copyright by Pierre-Henri Wuillemin, UPMC, 2017
+# (pierre-henri.wuillemin@lip6.fr)
+
+# Permission to use, copy, modify, and distribute this
+# software and its documentation for any purpose and
+# without fee or royalty is hereby granted, provided
+# that the above copyright notice appear in all copies
+# and that both that copyright notice and this permission
+# notice appear in supporting documentation or portions
+# thereof, including modifications, that you make.
+
+# THE AUTHOR P.H. WUILLEMIN  DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT
+# OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
+# RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
+# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+# ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+# OR PERFORMANCE OF THIS SOFTWARE!
+
+# @authors Clara CHARON and Pierre-henri WUILLEMIN
+import os
+import numpy as np
+import pandas as pd
+import logging
+
+import pyAgrum as gum
+import pyAgrum.skbn as skbn
+from pyAgrum.lib.bn2roc import _computepoints, _computeROC
+from sklearn.metrics import accuracy_score
+
+
+print("** pyAgrum.lib.classifier is deprecated in pyAgrum>0.18.1.")
+print("** Please use pyAgrum.skbn.BNClassifier instead.")
