63c455b1591e8deac05f0072d2ccb135a4b3caeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package ir2json/0.0.1/ir2json-0.0.1-py3-none-any.whl

---
+++ packages/ir2json/ir2json-0.0.1-py3-none-any.whl/ir2json/__main__.py
@@ -0,0 +1,23 @@
+"""Convert a UV File to JSON.
+
+Takes a Bruker OPUS dotZero (*.0) file and converts it into a JSON file containing the x (cm^-1) and y (transmittance)
+"""
+
+from argparse import ArgumentParser
+from ir2json import convert_to_json_ir
+
+
+def main():
+    parser = ArgumentParser(description="Convert Gilson CSV to JSON file")
+    parser.add_argument(
+        "filenames",
+        help="convert given filename(s) to json",
+        nargs="+",
+    )
+
+    args = parser.parse_args()
+    convert_to_json_ir.convert(args.filenames)
+
+
+if __name__ == "__main__":
+    main()
