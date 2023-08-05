279b91dd88c93f44abc9dce1d39cdecd94530bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:16 +0000

    Add package proto-parser-ng/1.0.1/proto_parser_ng-1.0.1-py3-none-any.whl

---
+++ packages/proto-parser-ng/proto_parser_ng-1.0.1-py3-none-any.whl/protoparser_ng/__init__.py
@@ -0,0 +1,5 @@
+from protoparser_ng.parser import parse, parse_from_file, serialize2json_from_file
+from protoparser_ng.parser import serialize2json
+
+__version__ = "1.0.1"
+__all__ = [parse, parse_from_file, serialize2json, serialize2json_from_file]
