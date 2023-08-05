b8fb34890a8d8dbbdd7dbaa001ca478231f47c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:01 +0000

    Add package pynomics/0.1.0/pynomics-0.1.0.tar.gz

---
+++ packages/pynomics/pynomics-0.1.0.tar.gz/pynomics-0.1.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pynomics']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click==7.1.2',
+ 'nomics-python>=3.2.0,<4.0.0',
+ 'numpy>=1.21.1,<2.0.0',
+ 'pandas>=1.3.1,<2.0.0',
+ 'pydantic>=1.8.2,<2.0.0',
+ 'tqdm>=4.62.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'pynomics',
+    'version': '0.1.0',
+    'description': 'Small library to interact with the [Nomics api](https://p.nomics.com/cryptocurrency-bitcoin-api).',
+    'long_description': None,
+    'author': 'Soumendra Prasad Dhanee',
+    'author_email': 'soumendra@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.6,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
