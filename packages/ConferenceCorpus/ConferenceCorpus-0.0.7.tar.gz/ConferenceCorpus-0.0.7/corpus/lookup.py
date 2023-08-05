94ff3e5d6a2bece81ba935e3453d9361d3491ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:45 +0000

    Add package ConferenceCorpus/0.0.7/ConferenceCorpus-0.0.7.tar.gz

---
+++ packages/ConferenceCorpus/ConferenceCorpus-0.0.7.tar.gz/ConferenceCorpus-0.0.7/corpus/lookup.py
@@ -0,0 +1,187 @@
+'''
+Created on 2021-07-39
+
+@author: wf
+'''
+from corpus.event import EventStorage
+from corpus.eventcorpus import EventCorpus, EventDataSource
+
+from datasources.dblp import DblpEventManager,DblpEventSeriesManager
+from datasources.wikidata import Wikidata,WikidataEventManager,WikidataEventSeriesManager
+from datasources.openresearch import OREventManager,OREventSeriesManager
+from datasources.wikicfp import WikiCfpEventManager,WikiCfpEventSeriesManager
+from datasources.crossref import CrossrefEventManager,CrossrefEventSeriesManager
+from lodstorage.uml import UML
+
+from datetime import datetime
+
+import os
+import sys
+
+from argparse import ArgumentParser
+from argparse import RawDescriptionHelpFormatter
+
+class CorpusLookup(object):
+    '''
+    search and lookup for different EventCorpora
+    '''
+    lookupIds=["dblp","crossref","wikidata","wikicfp","or","or-backup","orclone","orclone-backup"]
+
+    def __init__(self,lookupIds:list=None,
+                 configure:callable=None,debug=False):
+        '''
+        Constructor
+        
+        Args:
+            lookupIds(list): the list of lookupIds to addDataSources for
+            configure(callable): Callback to configure the corpus lookup
+        '''
+        self.debug=debug
+        self.configure=configure
+        self.eventCorpus=EventCorpus()
+        if lookupIds is None:
+            lookupIds=CorpusLookup.lookupIds
+        if "crossref" in lookupIds:
+            self.eventCorpus.addDataSource(CrossrefEventManager(),CrossrefEventSeriesManager(),lookupId="crossref",name="crossref.org",url="https://www.crossref.org/",title="CrossRef",tablePrefix="crossref")
+        if "dblp" in lookupIds:
+            self.eventCorpus.addDataSource(DblpEventManager(),DblpEventSeriesManager(),lookupId="dblp",name="dblp",url='https://dblp.org/',title='dblp computer science bibliography',tablePrefix="dblp")
+        if "wikidata" in lookupIds: 
+            self.eventCorpus.addDataSource(WikidataEventManager(),WikidataEventSeriesManager(),lookupId="wikidata",name="Wikidata",url='https://www.wikidata.org/wiki/Wikidata:Main_Page',title='Wikidata',tablePrefix="wikidata")
+        if "wikicfp" in lookupIds:    
+            self.eventCorpus.addDataSource(WikiCfpEventManager(),WikiCfpEventSeriesManager(),lookupId="wikicfp",name="WikiCFP",url='http://www.wikicfp.com',title='WikiCFP',tablePrefix="wikicfp")
+        if "or" in lookupIds:    
+            self.eventCorpus.addDataSource(OREventManager(),OREventSeriesManager(),lookupId="or",name="OR_Triples",url='https://www.openresearch.org/wiki/Main_Page',title='OPENRESEARCH-api',tablePrefix="orapi")
+        if "or-backup" in lookupIds:    
+            self.eventCorpus.addDataSource(OREventManager(),OREventSeriesManager(),lookupId="or-backup",name="OR_Markup",url='https://www.openresearch.org/wiki/Main_Page',title='OPENRESEARCH-wiki',tablePrefix="orwiki")
+        if "orclone" in lookupIds:    
+            self.eventCorpus.addDataSource(OREventManager(),OREventSeriesManager(),lookupId="orclone",name="OR_Clone_Triples",url='https://confident.dbis.rwth-aachen.de/or/index.php?title=Main_Page',title='OPENRESEARCH-clone-api',tablePrefix="orcapi")
+        if "orclone-backup" in lookupIds:    
+            self.eventCorpus.addDataSource(OREventManager(),OREventSeriesManager(),lookupId="orclone-backup",name="OR_Clone_Markup",url='https://confident.dbis.rwth-aachen.de/or/index.php?title=Main_Page',title='OPENRESEARCH-clone-wiki',tablePrefix="orcwiki")
+        
+    def getDataSource(self,lookupId:str)->EventDataSource:
+        '''
+        get the given data source
+        
+        Args:
+            lookupId(str): the lookupId of the data source to get
+            
+        Return:
+            EventDataSource: the data source
+
+        '''
+        eventDataSource=None
+        if lookupId in self.eventCorpus.eventDataSources:
+            eventDataSource=self.eventCorpus.eventDataSources[lookupId]
+        return eventDataSource
+        
+    def load(self,forceUpdate:bool=False):
+        '''
+        load the event corpora
+        Args:
+            forceUpdate(bool): True if the data should be fetched from the source instead of the cache
+        '''
+        if self.configure:
+            self.configure(self)
+        self.eventCorpus.loadAll(forceUpdate=forceUpdate)
+        
+    def asPlantUml(self,baseEntity='Event'):
+        '''
+        return me as a plantUml Diagram markup
+        '''
+        storageTableList=EventStorage.getTableList()
+        schemaManager=None
+        uml=UML()
+        now=datetime.now()
+        nowYMD=now.strftime("%Y-%m-%d")
+        tableList=[]
+        for table in storageTableList:
+            tableName=table['name']
+            if tableName.endswith(baseEntity):
+                if 'instances' in table:
+                    instanceNote=""
+                    instanceCount=table['instances']
+                    instanceNote=f"\n{instanceCount} instances "
+                    table['notes']=instanceNote
+                tableList.append(table)
+        title=f"""ConfIDent  {baseEntity}
+{nowYMD}
+[[https://projects.tib.eu/en/confident/ © 2019-2021 ConfIDent project]]
+see also [[http://ptp.bitplan.com/settings Proceedings Title Parser]]
+"""
+        plantUml=uml.mergeSchema(schemaManager,tableList,title=title,packageName='DataSources',generalizeTo=baseEntity)
+        return plantUml
+        
+        
+__version__ = "0.0.7"
+__date__ = '2020-06-22'
+__updated__ = '2021-08-02'    
+
+DEBUG = 1
+
+    
+def main(argv=None): # IGNORE:C0111
+    '''main program.'''
+
+    if argv is None:
+        argv = sys.argv
+    else:
+        sys.argv.extend(argv)    
+        
+    program_name = os.path.basename(sys.argv[0])
+    program_version = "v%s" % __version__
+    program_build_date = str(__updated__)
+    program_version_message = '%%(prog)s %s (%s)' % (program_version, program_build_date)
+    program_shortdesc = __import__('__main__').__doc__.split("\n")[1]
+    user_name="Wolfgang Fahl"
+    program_license = '''%s
+
+  Created by %s on %s.
+  Copyright 2020-2021 Wolfgang Fahl. All rights reserved.
+
+  Licensed under the Apache License 2.0
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Distributed on an "AS IS" basis without warranties
+  or conditions of any kind, either express or implied.
+
+USAGE
+''' % (program_shortdesc, user_name,str(__date__))
+
+    try:
+        # Setup argument parser
+        datasourcesDefault=",".join(CorpusLookup.lookupIds)
+        parser = ArgumentParser(description=program_license, formatter_class=RawDescriptionHelpFormatter)
+        parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="show debug info")
+        parser.add_argument('-e', '--endpoint', default=Wikidata.endpoint, help="SPARQL endpoint to use for wikidata queries")     
+        parser.add_argument('-v', '--version', action='version', version=program_version_message)
+        parser.add_argument("-u", "--uml", dest="uml", action="store_true", help="output plantuml diagram markup")
+        parser.add_argument("-f", "--force",dest="forceUpdate",action="store_true",help="force Update - may take quite a time")
+        parser.add_argument("--datasources",help=", delimited list of datasource lookup ids",default=datasourcesDefault)
+        
+        # Process arguments
+        args = parser.parse_args()   
+        Wikidata.endpoint=args.endpoint
+        lookupIds=args.datasources.split(",")
+        lookup=CorpusLookup(debug=args.debug,lookupIds=lookupIds)
+        lookup.load(forceUpdate=args.forceUpdate)
+        if args.uml:
+            for baseEntity in ["Event","EventSeries"]:
+                plantUml=lookup.asPlantUml(baseEntity)
+            print(plantUml)
+        
+        
+    except KeyboardInterrupt:
+        ### handle keyboard interrupt ###
+        return 1
+    except Exception as e:
+        if DEBUG:
+            raise(e)
+        indent = len(program_name) * " "
+        sys.stderr.write(program_name + ": " + repr(e) + "\n")
+        sys.stderr.write(indent + "  for help use --help")
+        return 2         
+        
+if __name__ == "__main__":
+    if DEBUG:
+        sys.argv.append("-d")
+    sys.exit(main())
---
+++ packages/ConferenceCorpus/ConferenceCorpus-0.0.7.tar.gz/ConferenceCorpus-0.0.7/datasources/dblp.py
@@ -0,0 +1,120 @@
+'''
+Created on 2021-07-28
+
+@author: th
+'''
+from corpus.event import EventSeriesManager,EventSeries, Event, EventManager
+from lodstorage.storageconfig import StorageConfig
+from datasources.dblpxml import Dblp
+
+class DblpEvent(Event):
+    '''
+    a Dblp Event
+    
+    Example event: https://dblp.org/db/conf/aaai/aaai2020.html
+    '''
+    def __init__(self):
+        '''constructor '''
+        super().__init__()
+        pass
+
+
+class DblpEventSeries(EventSeries):
+    '''
+    a Dblp Event Series
+    
+    Example event series: https://dblp.org/db/conf/aaai/index.html
+    '''
+    def __init__(self):
+        '''constructor '''
+        super().__init__()
+        pass
+
+class DblpEventManager(EventManager):
+    '''
+    dblp event access (in fact proceedings)
+    
+    Example event: https://dblp.org/db/conf/aaai/aaai2020.html
+    
+    '''
+    def __init__(self, config: StorageConfig = None):
+        '''
+        Constructor
+        '''
+        super(DblpEventManager, self).__init__(name="DblpEvents", clazz=DblpEventSeries,
+                                                         tableName="dblp_eventseries", config=config)
+    pass
+
+    def configure(self):
+        '''
+        configure me
+        '''
+        withProgress=False
+        if not hasattr(self, "dblp"): 
+            self.dblp=Dblp()
+            self.dblp.warnFullSize()
+            withProgress=True
+        self.sqlDb=self.dblp.getXmlSqlDB(showProgress=withProgress)
+        if not hasattr(self,"getListOfDicts"):
+            self.getListOfDicts=self.getLoDfromDblp
+
+    def getLoDfromDblp(self)->list:
+        '''
+        get the LoD for the event series
+            
+        Return:
+            list: the list of dict with my series data
+
+        '''
+        query = """select conf as series,title,year,url,series as publicationSeries
+        from proceedings 
+        order by series,year"""
+        listOfDicts = self.sqlDb.query(query)
+        self.setAllAttr(listOfDicts,"source","dblp")
+        return listOfDicts
+
+class DblpEventSeriesManager(EventSeriesManager):
+    '''
+    dblp event series access
+    Example event series: https://dblp.org/db/conf/aaai/index.html
+
+    dblp provides regular dblp xml dumps
+    '''
+
+    def __init__(self, config: StorageConfig = None):
+        '''
+        Constructor
+        '''
+        super(DblpEventSeriesManager, self).__init__(name="DblpEventSeries", clazz=DblpEventSeries,
+                                                         tableName="dblp_eventseries", config=config)
+        
+    def configure(self):
+        '''
+        configure me
+        '''
+        withProgress=False
+        if not hasattr(self, "dblp"): 
+            self.dblp=Dblp()
+            self.dblp.warnFullSize()
+            withProgress=True
+        self.sqlDb=self.dblp.getXmlSqlDB(showProgress=withProgress)
+        if not hasattr(self,"getListOfDicts"):
+            self.getListOfDicts=self.getLoDfromDblp
+
+    def getLoDfromDblp(self)->list:
+        '''
+
+        get the list of dicts for the event data
+            
+        Return:
+            list: the list of dict with my event data
+
+        '''
+        query = """select conf as acronym,count(*) as count,min(year) as minYear,max(year) as maxYear
+        from proceedings 
+        where acronym is not null
+        group by acronym
+        order by 2 desc"""
+        listOfDicts = self.sqlDb.query(query)
+        self.setAllAttr(listOfDicts,"source","dblp")
+        return listOfDicts
