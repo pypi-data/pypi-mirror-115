253529d07c08096d9d50c6352d603fc25ab3e28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:04 +0000

    Add package vcdilog/0.2.4/vcdilog-0.2.4-py3-none-any.whl

---
+++ packages/vcdilog/vcdilog-0.2.4-py3-none-any.whl/vcdilog/__init__.py
@@ -0,0 +1,3 @@
+from .obj import Logger, log, p  # noqas
+from .prefect import get_prefect_logger, set_prefect_extra_loggers, PrefectLogger  # noqas
+from .vcdilogintrospect import get_calling_module, get_nicest_module_name  # noqa
