1e1843d22e259fcd26975f965e9d650e88e55ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:58 +0000

    Add package boxie/1.0.2/boxie-1.0.2.tar.gz

---
+++ packages/boxie/boxie-1.0.2.tar.gz/boxie-1.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from boxie.version import __version__
+
+setup(
+    name="boxie",
+    version=__version__,
+    description="Like Figlet but puts the text in a box",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="UltiRequiem",
+    author_email="eliaz.bobadilladev@gmail.com",
+    url="https://github.com/UltiRequiem/boxie",
+    license="MIT",
+    scripts=["./bin/boxie"],
+    packages=["boxie"],
+)
