ce8b95f784537d1d0db7934f04407311d03d5af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:20 +0000

    Add package OsEasyControl/1.0/OsEasyControl-1.0.tar.gz

---
+++ packages/OsEasyControl/OsEasyControl-1.0.tar.gz/OsEasyControl-1.0/OsEasyControl/OsEasyControl.py
@@ -0,0 +1,85 @@
+import os
+
+def io():
+     import os
+
+def cmd(a):
+     os.system(a)
+def nt():
+     os.system('pause')
+     os.system('cls')
+
+def next_text():
+     os.system('pause')
+     os.system('cls')
+
+def rs():
+     os.system('color 0f')
+     os.system('cls')
+
+def reset_screen():
+     os.system('color 0f')
+     os.system('cls')
+
+def to():
+     os.system('shutdown -s')
+
+def turn_off():
+     os.system('shutdown -s')
+
+def rc():
+     os.system('shutdown -r')
+
+def restart_computer():
+     os.system('shutdown -r')
+
+def ss():
+     os.system('shutdown -a')
+
+def stop_shutdown():
+     os.system('shutdown -a')
+
+def rcit(a):
+     os.system('shutdown -r -t '+ str(a) +'')
+
+def restart_computer_in_time(a):
+     os.system('shutdown -r -t '+ str(a) +'')
+
+def toit(a):
+     os.system('shutdown -s -t '+ str(a) +'')
+
+def turn_off_in_time(a):
+     os.system('shutdown -s -t '+ str(a) +'')
+
+def cp(a):
+     os.system('ping '+ str(a) +'')
+
+def check_ping(a):
+     os.system('ping '+ str(a) +'')
+
+def cpc(a, b):
+     os.system('ping '+ str(a) +' -n '+ str(b) +'')
+
+def check_ping_count(a, b):
+     os.system('ping '+ str(a) +' -n '+ str(b) +'')
+def bs():
+     os.system('color 1f')
+
+def blue_screen():
+     os.system('color 1f')
+
+def nt():
+     os.system('start')
+
+def new_tab():
+     os.system('start')
+
+def ver():
+     os.system('ver')
+
+def gs(a):
+     os.system('start /max '+ str(a) +'')
+
+def go_site(a):
+     os.system('start /max '+ str(a) +'')
+
