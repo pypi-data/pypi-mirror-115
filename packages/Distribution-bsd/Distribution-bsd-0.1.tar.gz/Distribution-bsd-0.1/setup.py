42964cefef825fb8b41b55056c710b958f4652ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:06 +0000

    Add package Distribution-bsd/0.1/Distribution-bsd-0.1.tar.gz

---
+++ packages/Distribution-bsd/Distribution-bsd-0.1.tar.gz/Distribution-bsd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Distribution-bsd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['Distribution_bsd'],
+      zip_safe=False)
