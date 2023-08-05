59e84b88c32dccfc6c55613dd1aa3b75445d63fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package uv2json/1.0.0/uv2json-1.0.0-py3-none-any.whl

---
+++ packages/uv2json/uv2json-1.0.0-py3-none-any.whl/uv2json/__main__.py
@@ -0,0 +1,23 @@
+"""Convert a UV File to JSON.
+
+Takes a CSV file output by Gilson GX machines and converts it into a JSON file containing sample information alongwith intensities and time for three different UV wavelengths.
+"""
+
+from argparse import ArgumentParser
+from uv2json import convert_to_json_uv
+
+
+def main():
+    parser = ArgumentParser(description="Convert Gilson CSV to JSON file")
+    parser.add_argument(
+        "filenames",
+        help="convert given filename(s) to json",
+        nargs="+",
+    )
+
+    args = parser.parse_args()
+    convert_to_json_uv.convert(args.filenames)
+
+
+if __name__ == "__main__":
+    main()
