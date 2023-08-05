4393689bd4aa792491526ea87fd1114b5c3fb5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:38 +0000

    Add package progressbarsusw/0.0.1/progressbarsusw-0.0.1.tar.gz

---
+++ packages/progressbarsusw/progressbarsusw-0.0.1.tar.gz/progressbarsusw-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from distutils.core import setup
+setup(
+	name='progressbarsusw',
+	packages=["progressbarsmkrusw"],
+	version='0.0.1',
+	description='makes progressbars',
+	long_description=open('README.txt').read(),
+	author='Dominik Krenn',
+	author_email='i96774080@gmail.com',
+	license='MIT-license',
+	url='https://pypi.python.org/pypi/progressbarsmkr',
+	keywords=["Prozes", "Ladebalken", "Marker", "dominik12345"]
+)
