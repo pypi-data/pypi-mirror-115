70f9adecbae5b93a84e460ac184eee10c426b2cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package mmgen/0.3.0/mmgen-0.3.0.tar.gz

---
+++ packages/mmgen/mmgen-0.3.0.tar.gz/mmgen-0.3.0/mmgen/models/architectures/biggan/__init__.py
@@ -0,0 +1,13 @@
+from .generator_discriminator import BigGANDiscriminator, BigGANGenerator
+from .generator_discriminator_deep import (BigGANDeepDiscriminator,
+                                           BigGANDeepGenerator)
+from .modules import (BigGANConditionBN, BigGANDeepDiscResBlock,
+                      BigGANDeepGenResBlock, BigGANDiscResBlock,
+                      BigGANGenResBlock, SelfAttentionBlock, SNConvModule)
+
+__all__ = [
+    'BigGANGenerator', 'BigGANGenResBlock', 'BigGANConditionBN',
+    'BigGANDiscriminator', 'SelfAttentionBlock', 'BigGANDiscResBlock',
+    'BigGANDeepDiscriminator', 'BigGANDeepGenerator', 'BigGANDeepDiscResBlock',
+    'BigGANDeepGenResBlock', 'SNConvModule'
+]
