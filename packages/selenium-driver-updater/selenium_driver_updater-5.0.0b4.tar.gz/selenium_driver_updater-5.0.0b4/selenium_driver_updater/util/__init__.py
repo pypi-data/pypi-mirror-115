287014caa52f330e98e9aa0b6425f99c83cdec8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:30 +0000

    Add package selenium-driver-updater/5.0.0b4/selenium_driver_updater-5.0.0b4.tar.gz

---
+++ packages/selenium-driver-updater/selenium_driver_updater-5.0.0b4.tar.gz/selenium_driver_updater-5.0.0b4/selenium_driver_updater/util/__init__.py
@@ -0,0 +1,17 @@
+from selenium_driver_updater._chromeDriver import ChromeDriver
+from selenium_driver_updater._geckoDriver import GeckoDriver
+from selenium_driver_updater._operaDriver import OperaDriver
+from selenium_driver_updater._edgeDriver import EdgeDriver
+from selenium_driver_updater._chromiumChromeDriver import ChromiumChromeDriver
+from selenium_driver_updater._phantomJS import PhantomJS
+from selenium_driver_updater._safari_driver import SafariDriver
+
+ALL_DRIVERS= {
+    "chromedriver" : ChromeDriver,
+    "geckodriver" : GeckoDriver,
+    "operadriver" : OperaDriver,
+    "edgedriver" : EdgeDriver,
+    "chromium_chromedriver" : ChromiumChromeDriver,
+    "phantomjs" : PhantomJS,
+    "safaridriver" : SafariDriver,
+}
