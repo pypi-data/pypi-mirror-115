6d8e6096269de9e8f19d210d3fc71b3b6044a104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:14 +0000

    Add package jetson-adapter-pkg/0.8.2/jetson-adapter-pkg-0.8.2.tar.gz

---
+++ packages/jetson-adapter-pkg/jetson-adapter-pkg-0.8.2.tar.gz/jetson-adapter-pkg-0.8.2/frotech_adapter/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+from .heartbeat import Heartbeat
+from .motor import Motor
+from .servo import Servo
+from .pantilt import PanTilt
+from .serialLed import SerialLed
+from .ledMatrix import LedMatrix
+from .powerDetect import PowerDetect
+from .car import Car
+from .piece import Piece
+from .serialServo import SerialServo
+from .claw import Claw
+from .lift import Lift
+
+__version__ = '0.8.2'
