0f057841c9fd6edecf24b2b4e104ce7e74abc213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:08 +0000

    Add package scdiffeq/0.0.42/scdiffeq-0.0.42.tar.gz

---
+++ packages/scdiffeq/scdiffeq-0.0.42.tar.gz/scdiffeq-0.0.42/scdiffeq/__init__.py
@@ -0,0 +1,13 @@
+# scdiffeq __init__.py
+
+__author__ = ', '.join([
+    'Michael E. Vinyard'
+])
+__email__ = ', '.join([
+    'vinyard@g.harvard.edu',
+])
+
+from . import data
+from . import tools as tl
+from . import plotting  as pl
+from . import utilities as ut
