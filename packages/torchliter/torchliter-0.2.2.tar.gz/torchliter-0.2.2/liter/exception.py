38dd1a90c914d64cf57ee5bf58b100cb7ef2f08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:13 +0000

    Add package torchliter/0.2.2/torchliter-0.2.2.tar.gz

---
+++ packages/torchliter/torchliter-0.2.2.tar.gz/torchliter-0.2.2/liter/exception.py
@@ -0,0 +1,43 @@
+"""TorchLiter Exceptions."""
+
+
+class BreakIteration(Exception):
+    def __init__(self, shutdown_engine=False):
+        self.shutdown_engine = shutdown_engine
+
+
+class ContinueIteration(Exception):
+    pass
+
+
+class BadBatchError(ContinueIteration):
+    """
+    Use case:
+
+    current batch of data is corrupted, skip current batch and fetch a new batch
+    and then continue.
+    """
+
+
+class StopEngine(BreakIteration):
+    """
+    Use case:
+
+    when raised at batch iteration level, if `shutdown_engine=True` then reraise
+    BreakIteration to terminate engine.
+    """
+
+    def __init__(self):
+        super().__init__(True)
+
+
+class GradientExplosionError(StopEngine):
+    pass
+
+
+class FoundNanError(StopEngine):
+    pass
+
+
+class EarlyStopping(StopEngine):
+    pass
