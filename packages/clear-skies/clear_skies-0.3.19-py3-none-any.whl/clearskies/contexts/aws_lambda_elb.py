1cf3de96aea1eeefe40bf2d49d3a0c3c3ef41374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:37 +0000

    Add package clear-skies/0.3.19/clear_skies-0.3.19-py3-none-any.whl

---
+++ packages/clear-skies/clear_skies-0.3.19-py3-none-any.whl/clearskies/contexts/aws_lambda_elb.py
@@ -0,0 +1,40 @@
+from ..input_outputs import AWSLambdaELB as AWSLambdaELBInputOutput
+from ..di import StandardDependencies
+from .build_context import build_context
+
+
+class AWSLambdaELB:
+    _di = None
+    _handler = None
+
+    def __init__(self, di):
+        self._di = di
+
+    def configure(self, application):
+        self._handler = self._di.build(application.handler_class, cache=False)
+        self._handler.configure(application.handler_config)
+
+    def __call__(self, event, context):
+        if self._handler is None:
+            raise ValueError("Cannot execute AWSLambda context without first configuring it")
+
+        return self._handler(AWSLambdaELBInputOutput(event, context))
+
+    def bind(self, key, value):
+        self._di.bind(key, value)
+
+def aws_lambda_elb(
+    application,
+    di_class=StandardDependencies,
+    bindings=None,
+    binding_classes=None,
+    binding_modules=None
+):
+    return build_context(
+        AWSLambdaELB,
+        application,
+        di_class,
+        bindings=bindings,
+        binding_classes=binding_classes,
+        binding_modules=binding_modules
+    )
