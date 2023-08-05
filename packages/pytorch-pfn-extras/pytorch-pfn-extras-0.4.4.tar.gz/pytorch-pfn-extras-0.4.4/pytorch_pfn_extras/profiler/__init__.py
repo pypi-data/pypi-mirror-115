0f7219a2b054b49541afcd2a244b79a709b1bc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:50 +0000

    Add package pytorch-pfn-extras/0.4.4/pytorch-pfn-extras-0.4.4.tar.gz

---
+++ packages/pytorch-pfn-extras/pytorch-pfn-extras-0.4.4.tar.gz/pytorch-pfn-extras-0.4.4/pytorch_pfn_extras/profiler/__init__.py
@@ -0,0 +1,3 @@
+from pytorch_pfn_extras.profiler._record import record  # NOQA
+from pytorch_pfn_extras.profiler._time_summary import time_summary  # NOQA
+from pytorch_pfn_extras.profiler._time_summary import TimeSummary  # NOQA
