225651435365e3e772fd8cdb1315c55526e5eb6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:47 +0000

    Add package uvicore/0.1.18/uvicore-0.1.18.tar.gz

---
+++ packages/uvicore/uvicore-0.1.18.tar.gz/uvicore-0.1.18/uvicore/console/commands/stubs/command.py
@@ -0,0 +1,10 @@
+import uvicore
+from uvicore.support.dumper import dump, dd
+from uvicore.console import command, argument, option
+
+
+@command()
+async def cli():
+    """xx_AppName xx_name CLI command"""
+
+    print('Welcome to your new xx_AppName xx_name CLI command!')
