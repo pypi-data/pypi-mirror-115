b2d4e66372d5ac6f605cd29c349d002698536c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package ksonpy/0.0.3/ksonpy-0.0.3.tar.gz

---
+++ packages/ksonpy/ksonpy-0.0.3.tar.gz/ksonpy-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ksonpy",
+    version="0.0.3",
+    author="Jacob Brazeal",
+    author_email="jacob.brazeal@gmail.com",
+    description="KSON is JSON with embedded SQL and networking",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/jacob.brazeal/ksonpy",
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/jacob.brazeal/ksonpy/-/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+    install_requires=[
+        'requests',
+    ],
+
+)
