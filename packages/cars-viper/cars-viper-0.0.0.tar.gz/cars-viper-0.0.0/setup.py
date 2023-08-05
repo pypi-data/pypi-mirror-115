a2938df222e8e364727d898d7ba31d84650a9a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:32 +0000

    Add package cars-viper/0.0.0/cars-viper-0.0.0.tar.gz

---
+++ packages/cars-viper/cars-viper-0.0.0.tar.gz/cars-viper-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cars-viper',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='cars-viper',
+    author_email='datascience@cars.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
