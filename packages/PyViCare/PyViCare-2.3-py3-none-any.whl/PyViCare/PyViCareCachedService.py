df1b38f14109db2e0b0775fde009e7f32f659d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:05 +0000

    Add package PyViCare/2.3/PyViCare-2.3-py3-none-any.whl

---
+++ packages/PyViCare/PyViCare-2.3-py3-none-any.whl/PyViCare/PyViCareCachedService.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+import threading
+from PyViCare.PyViCareService import ViCareService, readFeature
+
+class ViCareCachedService(ViCareService):
+
+    def __init__(self, oauth_manager, accessor, cacheDuration):
+        ViCareService.__init__(self, oauth_manager, accessor)
+        self.cacheDuration = cacheDuration
+        self.cache = None
+        self.cacheTime = None
+        self.lock = threading.Lock()
+
+    def getProperty(self, property_name):
+        data = self.getOrUpdateCache()
+        entities = data["data"]
+        return readFeature(entities, property_name)
+
+    def setProperty(self, property_name, action, data):
+        response = super().setProperty(property_name, action, data)
+        self.clearCache()
+        return response
+
+    def getOrUpdateCache(self):
+        self.lock.acquire()
+        try:
+            if self.isCacheInvalid():
+                url = f'/equipment/installations/{self.accessor.id}/gateways/{self.accessor.serial}/devices/{self.accessor.device_id}/features/'
+                self.cache = self.oauth_manager.get(url)
+                self.cacheTime = datetime.now()
+            return self.cache
+        finally:
+            self.lock.release()
+
+    def isCacheInvalid(self):
+        return self.cache is None or self.cacheTime is None or (datetime.now() - self.cacheTime).seconds > self.cacheDuration
+
+    def clearCache(self):
+        self.lock.acquire()
+        try:
+            self.cache = None
+            self.cacheTime = None
+        finally:
+            self.lock.release()
