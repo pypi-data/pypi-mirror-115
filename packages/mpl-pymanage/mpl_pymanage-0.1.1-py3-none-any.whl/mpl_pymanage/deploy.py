939cf991f8b2d057fc734e2e66c16e1ad4fa52b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:16 +0000

    Add package mpl-pymanage/0.1.1/mpl_pymanage-0.1.1-py3-none-any.whl

---
+++ packages/mpl-pymanage/mpl_pymanage-0.1.1-py3-none-any.whl/mpl_pymanage/deploy.py
@@ -0,0 +1,42 @@
+from os.path import exists
+from pathlib import Path
+import shutil
+
+import click
+
+from mpl_pymanage.utils import shell, find_python
+
+
+def deploy_pypi():
+    """Deploy project in current directory to PyPi."""
+
+    cwd = Path.cwd()
+
+    if not exists(cwd / 'setup.py'):
+        raise click.UsageError(
+            'No setup.py found. Is this really a project root?')
+
+    if exists(cwd / 'build') or exists(cwd / 'dist'):
+        if click.confirm('build and/or dist folder found which will be deleted. Continue?'):
+            shutil.rmtree(cwd / 'build')
+            shutil.rmtree(cwd / 'dist')
+        else:
+            click.Abort()
+            return
+
+    shell(find_python() + ' -m pip install --upgrade wheel twine', silent=False)
+    shell(find_python() + ' setup.py sdist bdist_wheel', silent=False)
+
+    do_twine()
+
+
+def deploy_github(url, main):
+    shell('git remote add origin ' + url, silent=False)
+    shell('git branch -M ' + main, silent=False)
+    shell('git push -u origin ' + main, silent=False)
+
+
+def do_twine():
+    """Login and upload to PyPi."""
+    click.echo('Log in to PyPi...')
+    shell(find_python() + ' -m twine upload *', 'dist', silent=False)
