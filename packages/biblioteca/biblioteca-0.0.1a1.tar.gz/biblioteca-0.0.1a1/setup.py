7373b7fa9eb6314cff20a17ffbc98ddf965d584f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:14 +0000

    Add package biblioteca/0.0.1a1/biblioteca-0.0.1a1.tar.gz

---
+++ packages/biblioteca/biblioteca-0.0.1a1.tar.gz/biblioteca-0.0.1a1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='biblioteca',
+    version='0.0.1a1',
+    packages=['biblioteca', 'biblioteca.corpora'],
+    url='',
+    license='',
+    author='jarbasai',
+    author_email='',
+    description=''
+)
