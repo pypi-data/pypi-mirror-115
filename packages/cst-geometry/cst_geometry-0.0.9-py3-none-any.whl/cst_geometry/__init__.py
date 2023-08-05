1f6f43462dceb3d8dc5314fa4a068b626ae2db28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package cst-geometry/0.0.9/cst_geometry-0.0.9-py3-none-any.whl

---
+++ packages/cst-geometry/cst_geometry-0.0.9-py3-none-any.whl/cst_geometry/__init__.py
@@ -0,0 +1,7 @@
+from cst_geometry.geometry import Wire, Geometry
+from cst_geometry import simple_geometries
+from cst_geometry._script import Script
+
+
+__all__ = ["Wire", "Geometry", 'simple_geometries', 'Script']
+__version__ = 0.1
