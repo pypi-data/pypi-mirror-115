1eacaf98874492570e2b134060d4fc09a6b3d5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:44 +0000

    Add package apin/1.0.1/apin-1.0.1.tar.gz

---
+++ packages/apin/apin-1.0.1.tar.gz/apin-1.0.1/apin/core/generateCase.py
@@ -0,0 +1,71 @@
+# Author:柠檬班-木森
+# E-mail:musen_nmb@qq.com
+import unittest
+import json
+import os
+import yaml
+from apin.core.httptest import HttpCase
+from apin.core.basecase import GenerateTest
+
+
+class ParserDataToCase:
+    @staticmethod
+    def parser_json_create_cases(dir: str):
+        """解析json文件创建用例"""
+        suite = unittest.TestSuite()
+        if not os.path.isdir(dir): return suite
+        load = unittest.TestLoader()
+        files = os.listdir(dir)
+        for filename in files:
+            if filename.endswith('.json') and filename.startswith('test'):
+                with open(os.path.join(dir, filename), 'rb') as f:
+                    datas = json.load(f)
+                cls_name = filename.replace('.json', '').replace('test', '').split('_')
+                cls_name = 'Test' + ''.join([i.capitalize() for i in cls_name])
+                index = 1
+                for data in datas:
+                    if isinstance(data, dict):
+                        cls_name = cls_name + str(index)
+                        index += 1
+                        cls = GenerateTest(cls_name, (HttpCase,), data)
+                        suite.addTest(load.loadTestsFromTestCase(cls))
+        return suite
+
+    @staticmethod
+    def parser_yaml_create_cases(dir: str):
+        """解析yaml文件创建用例"""
+
+        suite = unittest.TestSuite()
+        load = unittest.TestLoader()
+        if not os.path.isdir(dir): return suite
+        files = os.listdir(dir)
+        for filename in files:
+            # 支持yaml
+            if filename.endswith('.yaml') and filename.startswith('test'):
+                with open(os.path.join(dir, filename), 'rb') as f:
+                    datas = yaml.load(f, Loader=yaml.FullLoader)
+                cls_name = filename.replace('.yaml', 'Yaml').replace('test', '').split('_')
+                cls_name = 'Test' + ''.join([i.capitalize() for i in cls_name])
+                index = 1
+                for data in datas:
+                    if isinstance(data, dict) and data.get('testSet'):
+                        cls_name = cls_name + str(index)
+                        index += 1
+                        cls = GenerateTest(cls_name, (HttpCase,), data.get('testSet'))
+                        suite.addTest(load.loadTestsFromTestCase(cls))
+        return suite
+
+    @staticmethod
+    def parser_data_create_cases(datas):
+        """
+        解析数据创建用例
+        :param data:
+        :return:
+        """
+        suite = unittest.TestSuite()
+        load = unittest.TestLoader()
+        for index, data in enumerate(datas):
+            cls_name = 'TestCase{}'.format(index)
+            cls = GenerateTest(cls_name, (HttpCase,), data)
+            suite.addTest(load.loadTestsFromTestCase(cls))
+        return suite
