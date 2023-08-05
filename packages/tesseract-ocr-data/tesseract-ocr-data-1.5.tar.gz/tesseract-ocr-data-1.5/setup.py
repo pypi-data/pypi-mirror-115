e1b651157808b3eeac0c6b788bd418a9196c0520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:18 +0000

    Add package tesseract-ocr-data/1.5/tesseract-ocr-data-1.5.tar.gz

---
+++ packages/tesseract-ocr-data/tesseract-ocr-data-1.5.tar.gz/tesseract-ocr-data-1.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tesseract-ocr-data",
+    version="1.5",
+    author="Suprime",
+    license = 'MIT',
+    author_email="suprime.sendings@gmail.com",
+    description="tesseract-ocr data for projects using it without having to install it.",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MOBSkuchen/tesseract-ocr-data",
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
