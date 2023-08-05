17b40711ae99e7f6f1e07726c84963d368ef9881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:29 +0000

    Add package svmon-python-client/2.1.9/svmon-python-client-2.1.9.tar.gz

---
+++ packages/svmon-python-client/svmon-python-client-2.1.9.tar.gz/svmon-python-client-2.1.9/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from setuptools import setup
+
+setup(
+name='svmon-python-client',
+version='2.1.9',
+packages=['svmon_python_client'],
+scripts=['svmon'],
+package_data={'' : ['*.json','*.pem']},
+description='This is a python implementation of SVMON client',
+long_description=open('README.rst').read(),
+author='Jie Yuan',
+author_email='jie.yuan@kit.edu',
+maintainer='Agustin Pane',
+maintainer_email='agustin.pane@kit.edu',
+license='MIT License',
+platforms=["all"],
+url='https://gitlab.eudat.eu/EUDAT-TOOLS/SVMON/pysvmon',
+classifiers=[
+    'Development Status :: 4 - Beta',
+    'Operating System :: OS Independent',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: Implementation',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Libraries' ]
+)
