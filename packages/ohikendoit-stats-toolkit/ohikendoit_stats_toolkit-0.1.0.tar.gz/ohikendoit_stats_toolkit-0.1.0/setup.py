1927c2424cad501d08bf39f330864ff860229d9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:39 +0000

    Add package ohikendoit-stats-toolkit/0.1.0/ohikendoit_stats_toolkit-0.1.0.tar.gz

---
+++ packages/ohikendoit-stats-toolkit/ohikendoit_stats_toolkit-0.1.0.tar.gz/ohikendoit_stats_toolkit-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ohikendoit_stats_toolkit',
+      version='0.1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ohikendoit_stats_toolkit'],
+      author = 'Yoonseong Jung',
+      author_email = 'ohikendoit@gmail.com',
+      zip_safe=False)
