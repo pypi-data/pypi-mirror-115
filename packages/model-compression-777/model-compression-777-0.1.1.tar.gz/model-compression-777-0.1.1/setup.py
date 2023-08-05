301941573282143e6de007b6b034ae97895203c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:03 +0000

    Add package model-compression-777/0.1.1/model-compression-777-0.1.1.tar.gz

---
+++ packages/model-compression-777/model-compression-777-0.1.1.tar.gz/model-compression-777-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['model_compression']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.21.1,<2.0.0', 'torch>=1.0.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'model-compression-777',
+    'version': '0.1.1',
+    'description': 'Pre-pruned pytorch model compression toolkit',
+    'long_description': None,
+    'author': 'Richard Yan',
+    'author_email': 'yrh@berkeley.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/model-compression-777/model-compression-777-0.1.1.tar.gz/model-compression-777-0.1.1/src/model_compression/__init__.py
@@ -0,0 +1,1 @@
+from model_compression_777 import *
