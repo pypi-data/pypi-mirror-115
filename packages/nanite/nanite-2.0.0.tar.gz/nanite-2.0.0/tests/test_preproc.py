bdaef97cf1940998f466b88ae634b607d639ef97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:14 +0000

    Add package nanite/2.0.0/nanite-2.0.0.tar.gz

---
+++ packages/nanite/nanite-2.0.0.tar.gz/nanite-2.0.0/tests/test_preproc.py
@@ -0,0 +1,58 @@
+"""Test preprocessing"""
+import pathlib
+
+import pytest
+
+from nanite import IndentationGroup
+
+
+data_path = pathlib.Path(__file__).resolve().parent / "data"
+bad_files = list(data_path.glob("bad*"))
+
+
+@pytest.mark.filterwarnings('ignore::nanite.preproc.CannotSplitWarning',
+                            'ignore::UserWarning')
+def test_process_bad():
+    for bf in bad_files:
+        ds = IndentationGroup(bf)
+        for idnt in ds:
+            # Walk through the standard analysis pipeline without
+            # throwing any exceptions.
+            idnt.apply_preprocessing(["compute_tip_position",
+                                      "correct_force_offset",
+                                      "correct_tip_offset",
+                                      "correct_split_approach_retract"])
+            idnt.fit_model()
+
+
+def test_correct_split_approach_retract():
+    fd = IndentationGroup(data_path / "spot3-0192.jpk-force")[0]
+
+    fd.apply_preprocessing(["compute_tip_position",
+                            "correct_force_offset",
+                            "correct_tip_offset"])
+    assert fd.appr["segment"].size == 2000
+    fd.apply_preprocessing(["compute_tip_position",
+                            "correct_force_offset",
+                            "correct_tip_offset",
+                            "correct_split_approach_retract"])
+    assert fd.appr["segment"].size == 2006
+
+
+def test_unknown_method():
+    idnt = IndentationGroup(data_path / "spot3-0192.jpk-force")[0]
+    try:
+        idnt.apply_preprocessing(["compute_tip_position",
+                                  "unknown_method"])
+    except KeyError:
+        pass
+    else:
+        assert False, "Preprocessing with an unknown method must not work."
+
+
+if __name__ == "__main__":
+    # Run all tests
+    loc = locals()
+    for key in list(loc.keys()):
+        if key.startswith("test_") and hasattr(loc[key], "__call__"):
+            loc[key]()
