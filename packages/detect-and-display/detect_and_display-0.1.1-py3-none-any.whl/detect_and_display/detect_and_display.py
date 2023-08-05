ae9efed28314b7a8b92f55cba2bb354354cf7a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:43 +0000

    Add package detect-and-display/0.1.1/detect_and_display-0.1.1-py3-none-any.whl

---
+++ packages/detect-and-display/detect_and_display-0.1.1-py3-none-any.whl/detect_and_display/detect_and_display.py
@@ -0,0 +1,5 @@
+class DetectAndDisplay:
+    """Class that encapsulates the detection and display of license plates."""
+    def display(self, text: str) -> None:
+        """Display the text"""
+        return f'the screen is displaying: {text}'
