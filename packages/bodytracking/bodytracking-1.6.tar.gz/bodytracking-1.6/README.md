eabc8e559b354a98b0a213db442077c3b365af4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:59 +0000

    Add package bodytracking/1.6/bodytracking-1.6.tar.gz

---
+++ packages/bodytracking/bodytracking-1.6.tar.gz/bodytracking-1.6/README.md
@@ -0,0 +1,2 @@
+# bodytracking
+## bodytracking is a collection of functions powered by mediapipe and opencv-python
---
+++ packages/bodytracking/bodytracking-1.6.tar.gz/bodytracking-1.6/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bodytracking",
+    version="1.6",
+    author="Suprime",
+    license = 'MIT',
+    author_email="suprime.sendings@gmail.com",
+    description="bodytracking is a collection of functions powered by mediapipe and opencv-python.",
+    long_description= long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MOBSkuchen/tkt-Toolkit",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'mediapipe',
+        'opencv-python'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
