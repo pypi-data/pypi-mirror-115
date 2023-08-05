f2baf5f5504fc6951b4480e1e701f8e19e399bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:21 +0000

    Add package nikel-py/0.1.5/nikel-py-0.1.5.tar.gz

---
+++ packages/nikel-py/nikel-py-0.1.5.tar.gz/nikel-py-0.1.5/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup
+from codecs import open
+from os import path
+
+
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+
+with open(path.join(HERE,'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name="nikel-py",
+    version="0.1.5",
+    description="Python API Wrapper for the Nikel API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Multivalence/nikel-py",
+    author="Multivalence",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities'
+
+
+    ],
+    packages=["nikel_py", "nikel_py.utils"],
+    include_package_data=True,
+    install_requires=requirements,
+    python_requires='>=3.6.0'
+)
