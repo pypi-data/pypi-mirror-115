3914cac33d6227a7c5d2de037f2a3ae4677fc68b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:11 +0000

    Add package middletools/0.1.0/middletools-0.1.0-py3-none-any.whl

---
+++ packages/middletools/middletools-0.1.0-py3-none-any.whl/middletools/exceptions.py
@@ -0,0 +1,26 @@
+import dataclasses
+
+from middletools.types import MiddlewareHandler
+
+
+@dataclasses.dataclass(frozen=True, repr=False)
+class _ContainMiddleware(Exception):
+    middleware: MiddlewareHandler
+
+    __slots__ = ("middleware",)
+
+
+class CallNextNotUsedError(_ContainMiddleware):
+    def __repr__(self):
+        return (
+            f"Middleware {self.middleware} doesn't call `call_next`, "
+            f"dispatching declined forcibly"
+        )
+
+
+class NothingReturnedError(_ContainMiddleware):
+    def __repr__(self):
+        return (
+            f"Middleware {self.middleware} "
+            f"returned nothing. Decline dispatching"
+        )
