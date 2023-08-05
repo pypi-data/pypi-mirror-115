504fc2c1f902c1f97ffb445df4b7774f8a34ec8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:57 +0000

    Add package aicrowd-cli/0.1.9/aicrowd_cli-0.1.9-py3-none-any.whl

---
+++ packages/aicrowd-cli/aicrowd_cli-0.1.9-py3-none-any.whl/aicrowd/notebook/exceptions.py
@@ -0,0 +1,53 @@
+"""
+Exceptions for notebook subcommand
+"""
+
+from aicrowd.exceptions import CLIException
+
+
+class NotebookException(CLIException):
+    """
+    Base exception for the notebook subcommand
+    """
+
+
+class NotebookAppImportException(NotebookException):
+    """
+    Exception for notebook app import errors
+    """
+
+
+class InvalidJupyterResponse(NotebookException):
+    """
+    Exception for bad responses from jupyter REST API
+    """
+
+
+class NotebookNotFound(NotebookException):
+    """
+    Exception for missing jupyter notebook
+    """
+
+
+class SubmissionFileNotFound(NotebookException):
+    """
+    Exception for missing submission zip file
+    """
+
+
+class AssetsDirectoryError(NotebookException):
+    """
+    Exception for assets directory validations
+    """
+
+
+class FeatureNotReady(NotebookException):
+    """
+    Exception for unshipped features
+    """
+
+
+class LocalEvaluationError(NotebookException):
+    """
+    Exception for local evaluation failure
+    """
