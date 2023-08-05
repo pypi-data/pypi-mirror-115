b05326ecc82ad56b5c5528312382ca5043289402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:11 +0000

    Add package piscola/0.1.4/piscola-0.1.4.tar.gz

---
+++ packages/piscola/piscola-0.1.4.tar.gz/piscola-0.1.4/tests/test_piscola.py
@@ -0,0 +1,15 @@
+import unittest
+import piscola
+import numpy as np
+
+class TestPiscola(unittest.TestCase):
+
+    def test_lc_correction(self):
+
+        sn = piscola.call_sn('03D1au')
+        sn.normalize_data()
+        sn.fit_lcs()
+        sn.mangle_sed(-1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
