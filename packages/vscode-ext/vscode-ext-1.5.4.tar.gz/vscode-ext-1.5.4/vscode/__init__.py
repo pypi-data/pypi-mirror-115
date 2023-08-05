0b2778c3ba2d5f17184f37d7d470e737ef4d9414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:10 +0000

    Add package vscode-ext/1.5.4/vscode-ext-1.5.4.tar.gz

---
+++ packages/vscode-ext/vscode-ext-1.5.4.tar.gz/vscode-ext-1.5.4/vscode/__init__.py
@@ -0,0 +1,14 @@
+__author__ = "Swas.py"
+__title__ = "vscode"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Swas.py"
+__version__ = "1.5.4"
+
+from . import window
+from .compiler import build, build_theme
+from .extension import *
+from .themes import ColorTheme
+from .env_methods import env
+from . import types as ext
+from .types import *
+from .webviews import *
