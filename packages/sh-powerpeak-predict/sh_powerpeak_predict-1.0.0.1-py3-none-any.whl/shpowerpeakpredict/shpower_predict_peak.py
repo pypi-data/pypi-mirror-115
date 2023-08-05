e537ceb52136b0ef6a7917855d6c792e9b8c0700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:23 +0000

    Add package sh-powerpeak-predict/1.0.0.1/sh_powerpeak_predict-1.0.0.1-py3-none-any.whl

---
+++ packages/sh-powerpeak-predict/sh_powerpeak_predict-1.0.0.1-py3-none-any.whl/shpowerpeakpredict/shpower_predict_peak.py
@@ -0,0 +1,58 @@
+﻿###################################
+## filename : shpower_predict_peak.py
+###################################
+
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.api import VAR
+import datetime
+import warnings
+
+warnings.filterwarnings(action='ignore')
+
+class ShPowerPeakPredict:
+    def powerPeakPredict(self, power_total_df, pred_cnt):
+
+        power_total_df.reset_index(drop=True)
+
+        # 새로운 15분 전력 속성(power15) 새로 만들기
+        power_total_df['power15'] = None
+        power_total_df['power15'] = power_total_df['power15'].astype('float')
+
+        power_len =len(power_total_df)
+
+        # 15분 전력량 속성(power15)값 계산하기
+        for i in range (2, power_len) :
+            power_total_df.iloc[i, 2] = round(power_total_df.iloc[i-2, 1] + power_total_df.iloc[i-1, 1] + power_total_df.iloc[i, 1],2)
+
+        # 15분 전력량 nuul행(2개) 제거
+        power_total_df = power_total_df.dropna(subset=['power15'], axis=0).reset_index(drop=True)
+
+        # 데이터의 마지막 시각 설정
+        current_time = power_total_df.iloc[-1]['date']
+
+        # 시계열 date를 index로 지정
+        power_total_df.index = power_total_df['date']
+        power_total_df.set_index('date', inplace=True)
+
+        # 훈련 및 시험 실측 데이터
+        train_df = power_total_df[:-pred_cnt]
+
+        test_df = power_total_df[-pred_cnt:]
+
+        train = train_df.values
+        test_power = test_df.values
+
+        # VAR 모델 설정
+        forecasting_model = VAR(train)
+        results_aic = []
+
+        # VAR 모델 학습
+        results = forecasting_model.fit(maxlags=12, trend='ct')
+
+        # 지연 12(60분), 6개(현재시각 포함 25분 후까지) 예측
+        laaged_values = train_df.values[-12:]
+        forecast = pd.DataFrame(results.forecast(y=laaged_values, steps=pred_cnt), index=test_df.index, 
+                                columns=['predictpower5', 'predictpower15'])
+
+        return forecast
---
+++ packages/sh-powerpeak-predict/sh_powerpeak_predict-1.0.0.1-py3-none-any.whl/shpowerpeakpredict/shsqlpeak.py
@@ -0,0 +1,62 @@
+﻿###################################
+## filename : shsqlpeak.py
+###################################
+
+import pandas as pd
+import pymssql as sql
+
+# 원시데이터 로딩 및 예측결과 데이터 저장    
+class ShPowerPeakData():
+    # 검색조건에 대한 데이터를 읽어옴
+    # 5분 전력량 데이터를 읽어오는 Query
+    def selectPowerPeak(self, conn, siteCode, buildingNo, mmicd):
+        query = "SELECT CONVERT(CHAR(16), CONVERT(DATETIME, SaveTime), 120) AS date"
+        # 테스트를 위해 전력량 데이터가 작아 전력 데이터로 대치
+        query += " , IIF(CurrentPowerKw < 0, 0, CurrentPowerKw) AS power"
+        # 전력량 데이터(15분 데이터로 계산하기 위해 4를 곱해줌)
+        #query += " , IIF(PowerKwInc < 0, 0, PowerKwInc)*4 AS power"
+        query += " FROM T_POWER_PEAK_SAVE"
+        query += " WHERE SITECODE = '" + siteCode + "'"
+        query += " AND BuildingNo = '" + buildingNo + "'"
+        query += " AND MMIEquipmentID = '" + mmicd + "'"
+        query += " AND SaveTime BETWEEN CONVERT(VARCHAR(23), DATEADD(DAY, -14, GETDATE()),120) AND CONVERT(VARCHAR(23), GETDATE(),120)"
+        query += " AND CurrentPowerKw > 0"
+        query += " ORDER BY date"
+
+        # 읽어온 데이터를 DataFrame에 저장
+        total_df = pd.read_sql(query, conn, parse_dates='date')
+
+        return total_df
+
+    # 예측 결과데이터 저장
+    def savePowerPeak(self, conn, siteCode, buildingNo, current_time, pred_cnt, forecast):
+        # 예측 소비전력량 1일 단위 전력량 예측 테이블에 데이터 저장
+        cursor = conn.cursor()
+        for k in range(pred_cnt):
+            predict_time = current_time + pd.DateOffset(minutes=(k+1)*5)
+            #print(predict_time, "예측 5분전력: {:.2f}, 예측 15분전력: {:.2f}". 
+            #      format(forecast.iloc[k,0], forecast.iloc[k,1]))
+            
+            #예측정보를 DB에 저장할 SQL 조립
+            insertSql = "MERGE INTO T_POWER_PREDICT_15M F"
+            insertSql += " USING (SELECT '" + siteCode + "', '" + buildingNo + "'"
+            insertSql += " , CONVERT(CHAR(16), CONVERT(DATETIME, '" + str(predict_time) + "'), 120)) AS S"
+            insertSql += " (SITECODE, BUILDINGNO, PREDICTDATETIME)"
+            insertSql += " ON F.SITECODE = S.SITECODE AND F.BUILDINGNO= S.BUILDINGNO"
+            insertSql += " AND F.PREDICTDATETIME = S.PREDICTDATETIME"
+            insertSql += " WHEN MATCHED THEN"
+            insertSql += " UPDATE SET"
+            insertSql += " PREDICTPOWER5M = '" + str(round(forecast.iloc[k,0],2)) + "'"
+            insertSql += " , PREDICTPOWER15M = '" + str(round(forecast.iloc[k,1],2)) + "'"
+            insertSql += " , SAVETIME = GETDATE()"
+            insertSql += " WHEN NOT MATCHED THEN"
+            insertSql += " INSERT (SITECODE, BUILDINGNO, PREDICTDATETIME, PREDICTPOWER5M, PREDICTPOWER15M, SAVETIME)"
+            insertSql += " VALUES('" + siteCode + "', '" + buildingNo + "'"
+            insertSql += " , CONVERT(CHAR(16), CONVERT(DATETIME, '" + str(predict_time) + "'),120)"
+            insertSql += " , '" + str(round(forecast.iloc[k,0],2)) + "', '" + str(round(forecast.iloc[k,1],2)) + "', GETDATE());"
+
+            #예측정보를 테이블에 저장
+            cursor.execute(insertSql)
+
+        conn.commit()
+    
