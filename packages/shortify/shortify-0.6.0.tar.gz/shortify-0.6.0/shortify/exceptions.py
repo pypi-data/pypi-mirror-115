96629c841628501fb8e2a1ab6ac866f0e645d6b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package shortify/0.6.0/shortify-0.6.0.tar.gz

---
+++ packages/shortify/shortify-0.6.0.tar.gz/shortify-0.6.0/shortify/exceptions.py
@@ -0,0 +1,37 @@
+__all__ = (
+    "InvalidURL",
+    "ShorteningError",
+    "ExpandingError",
+)
+
+
+class ShortifyError(Exception):
+    """The base class for all Shortify exceptions.
+
+    Any other exceptions should likely inherit from this class.
+    """
+
+
+class InvalidURL(ShortifyError):
+    """Raised when the given URL fails regular-expression check."""
+
+    def __init__(self, url: str) -> None:
+        super().__init__(f'The URL "{url}" is invalid.')
+
+
+class ShorteningError(ShortifyError):
+    """Raised when the shortening from one service (e.g TinyURL) fails."""
+
+    def __init__(self, url: str) -> None:
+        super().__init__(
+            f"Unexpected problem occurred on trying to shorten this URL: {url}"
+        )
+
+
+class ExpandingError(ShortifyError):
+    """Raised when the expanding from one service (e.g TinyURL) fails."""
+
+    def __init__(self, url: str) -> None:
+        super().__init__(
+            f"Unexpected problem occurred on trying to expand this URL: {url}"
+        )
---
+++ packages/shortify/shortify-0.6.0.tar.gz/shortify-0.6.0/shortify/services/shrtcode.py
@@ -0,0 +1,38 @@
+from ..abc import ShortifyBase
+from ..exceptions import ShorteningError
+
+
+class Shortener(ShortifyBase):
+    """Shrtco.de shortener.
+
+    This class inherits from `shortify.base.ShortifyBase`.
+    """
+
+    def shorten(self, url: str) -> str:
+        """Shorten function.
+
+        Parameters
+        ----------
+        url : str
+            The URL you need to shorten.
+
+        Returns
+        -------
+        str
+            The shortened URL.
+
+        Raises
+        ------
+        ShorteningError
+            If the response status was not around 200 (not ok).
+        """
+        print("This API may work slow, please consider waiting!\n")
+
+        response = self.get("https://api.shrtco.de/v2/shorten", params={"url": url})
+        if response.ok:
+            result = response.json()["result"]
+            return "\n".join(
+                result[res] for res in result if res.startswith("full_short_link")
+            )
+
+        raise ShorteningError(response.content)
