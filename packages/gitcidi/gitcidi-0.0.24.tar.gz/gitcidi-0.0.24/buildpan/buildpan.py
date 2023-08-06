9c7f0fc4dc88ae62773ba37e261d954acc1d851a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:19 +0000

    Add package gitcidi/0.0.24/gitcidi-0.0.24.tar.gz

---
+++ packages/gitcidi/gitcidi-0.0.24.tar.gz/gitcidi-0.0.24/buildpan/buildpan.py
@@ -0,0 +1,66 @@
+
+"""
+    Title: buildpan
+    Module Name: buildpan
+    Author: Akash Dwivedi
+    Language: Python
+    Date Created: 26-07-2021
+    Date Modified: 29-07-2021
+    Description:
+        ###############################################################
+        ##  Main operating file for all the cli commands             ## 
+         ###############################################################
+ """
+import git
+import os
+import click
+import git
+import sys
+from github import Github
+from commands import auth
+from commands import allrepo
+from commands import selectrepo
+from commands import webhook
+
+
+
+
+def main():
+    buildpan()
+
+@click.group(help="CLI tool to manage full development cycle of projects")
+def buildpan():
+    pass
+
+
+@buildpan.command()
+# @click.option('--token', '-e', default="dev", prompt='Enter token ', help='Token for authentiication')
+def print():
+    """Authenticate your self"""
+    print(auth.tk2)
+@buildpan.command()
+# # @click.option('--token', '-e', default="dev", prompt='Enter token ', help='Token for authentiication')
+@click.option('--folder',  prompt='Enter directory name ', help='Shows all the repo in the account')
+@click.option('--path',  prompt='Enter path name ', help='Shows all the repo in the account')
+
+def pull(folder,path):
+    """Roll back to the previous version"""
+    '''
+       
+    '''
+    os.chdir(path)
+    # folder=input("Enter the path")
+    repo = git.Repo(folder)
+    origin = repo.remote(name='origin')
+    origin.pull()
+
+buildpan.add_command(auth.auth)
+buildpan.add_command(allrepo.allrepo)
+buildpan.add_command(selectrepo.selectrepo)
+# buildpan.add_command(reset.reset)
+buildpan.add_command(webhook.create_webhook)
+if __name__ == '__main__':
+    main()
+    
+
+
