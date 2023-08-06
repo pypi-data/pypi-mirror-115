21c25a250f1315cc6010dff645012b1cfbe9c8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:40 +0000

    Add package realesrgan/0.2.0/realesrgan-0.2.0.tar.gz

---
+++ packages/realesrgan/realesrgan-0.2.0.tar.gz/realesrgan-0.2.0/inference_realesrgan.py
@@ -0,0 +1,66 @@
+import argparse
+import cv2
+import glob
+import os
+
+from realesrgan import RealESRGANer
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', type=str, default='inputs', help='Input image or folder')
+    parser.add_argument(
+        '--model_path',
+        type=str,
+        default='experiments/pretrained_models/RealESRGAN_x4plus.pth',
+        help='Path to the pre-trained model')
+    parser.add_argument('--output', type=str, default='results', help='Output folder')
+    parser.add_argument('--scale', type=int, default=4, help='Upsample scale factor')
+    parser.add_argument('--suffix', type=str, default='out', help='Suffix of the restored image')
+    parser.add_argument('--tile', type=int, default=0, help='Tile size, 0 for no tile during testing')
+    parser.add_argument('--tile_pad', type=int, default=10, help='Tile padding')
+    parser.add_argument('--pre_pad', type=int, default=0, help='Pre padding size at each border')
+    parser.add_argument('--half', action='store_true', help='Use half precision during inference')
+    parser.add_argument(
+        '--alpha_upsampler',
+        type=str,
+        default='realesrgan',
+        help='The upsampler for the alpha channels. Options: realesrgan | bicubic')
+    parser.add_argument(
+        '--ext',
+        type=str,
+        default='auto',
+        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs')
+    args = parser.parse_args()
+
+    upsampler = RealESRGANer(
+        scale=args.scale,
+        model_path=args.model_path,
+        tile=args.tile,
+        tile_pad=args.tile_pad,
+        pre_pad=args.pre_pad,
+        half=args.half)
+    os.makedirs(args.output, exist_ok=True)
+    if os.path.isfile(args.input):
+        paths = [args.input]
+    else:
+        paths = sorted(glob.glob(os.path.join(args.input, '*')))
+
+    for idx, path in enumerate(paths):
+        imgname, extension = os.path.splitext(os.path.basename(path))
+        print('Testing', idx, imgname)
+
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        output, img_mode = upsampler.enhance(img)
+        if args.ext == 'auto':
+            extension = extension[1:]
+        else:
+            extension = args.ext
+        if img_mode == 'RGBA':  # RGBA images should be saved in png format
+            extension = 'png'
+        save_path = os.path.join(args.output, f'{imgname}_{args.suffix}.{extension}')
+        cv2.imwrite(save_path, output)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/realesrgan/realesrgan-0.2.0.tar.gz/realesrgan-0.2.0/realesrgan/version.py
@@ -0,0 +1,5 @@
+# GENERATED VERSION FILE
+# TIME: Sun Aug  8 08:33:22 2021
+__version__ = '0.2.0'
+__gitsha__ = '13186ac'
+version_info = (0, 2, 0)
