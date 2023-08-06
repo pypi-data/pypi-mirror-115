2a301f29e3239296a8ba9bb0fd4b59fee3b75e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:55 +0000

    Add package harvest-for-mightyhive/0.0.2/harvest-for-mightyhive-0.0.2.tar.gz

---
+++ packages/harvest-for-mightyhive/harvest-for-mightyhive-0.0.2.tar.gz/harvest-for-mightyhive-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="harvest-for-mightyhive",
+    version="0.0.2",
+    author="Yang Dai",
+    author_email="yang.dai2020@gmail.com",
+    description="An automation tool to speed up the process of updating entries in harvest",
+    long_description=long_description,
+    download_url = "https://github.com/superyang713/harvest_automation/archive/refs/tags/v0.0.2.tar.gz", 
+    url="https://github.com/superyang713/harvest_automation/blob/main/README.md",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+    ],
+    install_requires=[
+        "selenium",
+    ],
+)
+
