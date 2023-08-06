6a8bea29228b0e96244d1ed035f7681bce152612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:25 +0000

    Add package django-comment-migrate/0.1.5/django-comment-migrate-0.1.5.tar.gz

---
+++ packages/django-comment-migrate/django-comment-migrate-0.1.5.tar.gz/django-comment-migrate-0.1.5/django_comment_migrate/utils.py
@@ -0,0 +1,40 @@
+from django.db import router, DEFAULT_DB_ALIAS
+from django.db.migrations import Migration
+from django.db.models import Field
+
+from django_comment_migrate.config import dcm_config
+
+
+def get_field_comment(field: Field):
+
+    value = getattr(field, dcm_config.DCM_COMMENT_KEY)
+    if value is not None:
+        return str(value)
+
+
+def get_table_comment(model):
+    value = getattr(model._meta, dcm_config.DCM_TABLE_COMMENT_KEY)
+    if value is not None:
+        return str(value)
+
+
+def get_migrations_app_models(
+    migrations: [Migration], apps, using=DEFAULT_DB_ALIAS
+) -> set:
+    models = set()
+    for migration in migrations:
+        if not isinstance(migration, Migration):
+            continue
+        app_label = migration.app_label
+        if not router.allow_migrate(using, app_label):
+            continue
+        operations = getattr(migration, "operations", [])
+        for operation in operations:
+            model_name = getattr(operation, "model_name", None) or getattr(
+                operation, "name", None
+            )
+            if model_name is None:
+                continue
+            model = apps.get_model(app_label, model_name=model_name)
+            models.add(model)
+    return models
