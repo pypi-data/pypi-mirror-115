fa5395526fa6766acc456e0a883217bec7105eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:18 +0000

    Add package powerbot-backtesting/1.0.10/powerbot-backtesting-1.0.10.tar.gz

---
+++ packages/powerbot-backtesting/powerbot-backtesting-1.0.10.tar.gz/powerbot-backtesting-1.0.10/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='powerbot-backtesting',
+      version='1.0.10',
+      description="All of PowerBot's backtesting functions in one package",
+      url='https://github.com/powerbot-trading/powerbot_backtesting',
+      author='PowerBot Trading',
+      author_email='techinfo@powerbot-trading.com',
+      license='MIT',
+      packages=['powerbot_backtesting'],
+      package_data={'powerbot_backtesting': ['exceptions/*', 'utils/*']},
+      install_requires=[
+	      'urllib3',
+	      'certifi',
+	      'python-dateutil',
+	      'pandas',
+	      'plotly',
+	      'SQLAlchemy',
+	      'requests',
+	      'powerbot-client'],
+      python_requires='>= 3.9',
+      zip_safe=False,
+      long_description_content_type='text/markdown',
+      long_description=open('README.md').read()
+      )
