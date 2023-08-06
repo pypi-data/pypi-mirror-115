280552baa35ef7b72ac1327cdd0da16c3045d6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:14 +0000

    Add package fastel/1.1.3/fastel-1.1.3.tar.gz

---
+++ packages/fastel/fastel-1.1.3.tar.gz/fastel-1.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fastel']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi>=0.63.0', 'revdb>=1.0.5,<2.0.0', 'revjwt>=1.0.4,<2.0.0']
+
+setup_kwargs = {
+    'name': 'fastel',
+    'version': '1.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Chien',
+    'author_email': 'a0186163@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
