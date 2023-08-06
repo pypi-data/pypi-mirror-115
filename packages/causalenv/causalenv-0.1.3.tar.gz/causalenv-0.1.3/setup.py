4713efaecb3d55c6748162e1e665a6b7da48e439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:52 +0000

    Add package causalenv/0.1.3/causalenv-0.1.3.tar.gz

---
+++ packages/causalenv/causalenv-0.1.3.tar.gz/causalenv-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['causalenv', 'causalenv.envs']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gym>=0.18.0,<0.19.0', 'setuptools>=57.0,<58.0']
+
+setup_kwargs = {
+    'name': 'causalenv',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Elias Aoun Durand',
+    'author_email': 'elias.aoundurand@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
