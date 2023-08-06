75f88e9248845d3928e29b02c41f2be6edd52571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:39 +0000

    Add package probdistributions/0.1/probdistributions-0.1.tar.gz

---
+++ packages/probdistributions/probdistributions-0.1.tar.gz/probdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probdistributions'],
+      author = 'Oluwaseyi Akinwande',
+      author_email = 'oia0001@auburn.edu',
+      zip_safe=False)
