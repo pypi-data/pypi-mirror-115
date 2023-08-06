be76a5cc1e343ebe7d1d96c04bb3990b7a0fb918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:37 +0000

    Add package discordLevelingSystem/1.0.0/discordLevelingSystem-1.0.0.tar.gz

---
+++ packages/discordLevelingSystem/discordLevelingSystem-1.0.0.tar.gz/discordLevelingSystem-1.0.0/discordLevelingSystem/__init__.py
@@ -0,0 +1,17 @@
+"""
+Discord Leveling System
+~~~~~~~~~~~~~~~~~~~~~~~
+
+A library to implement a leveling system into a discord bot. Contains features such as XP, level, ranks, and role awards.
+
+:copyright: (c) 2021 Defxult#8269
+:license: MIT
+
+"""
+
+from .announcement import LevelUpAnnouncement
+from .leveling_system import DiscordLevelingSystem
+from .role_awards import RoleAward
+
+__version__ = '1.0.0'
+__source__ = 'https://github.com/Defxult/discordLevelingSystem'
