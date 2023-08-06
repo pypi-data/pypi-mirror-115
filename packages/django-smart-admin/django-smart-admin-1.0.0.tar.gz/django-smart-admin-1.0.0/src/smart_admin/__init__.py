b9e38746829981c390d0a5eaa8865e69fe1f8ae7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:28 +0000

    Add package django-smart-admin/1.0.0/django-smart-admin-1.0.0.tar.gz

---
+++ packages/django-smart-admin/django-smart-admin-1.0.0.tar.gz/django-smart-admin-1.0.0/src/smart_admin/__init__.py
@@ -0,0 +1,7 @@
+default_app_config = 'smart_admin.apps.SmartConfig'
+
+VERSION = "1.0.0"
+
+
+def get_full_version():
+    return VERSION
---
+++ packages/django-smart-admin/django-smart-admin-1.0.0.tar.gz/django-smart-admin-1.0.0/src/smart_admin/logs/admin.py
@@ -0,0 +1,25 @@
+from adminfilters.autocomplete import AutoCompleteFilter
+from adminfilters.filters import RelatedFieldComboFilter
+from django.contrib.admin import register
+from django.contrib.admin.models import LogEntry
+from django.contrib import admin
+
+from smart_admin.mixins import SmartMixin
+
+
+@register(LogEntry)
+class LogEntryAdmin(SmartMixin, admin.ModelAdmin):
+    list_display = ('action_time', 'user', 'action_flag', 'content_type', 'object_repr')
+    readonly_fields = ('__all__',)
+    search_fields = ('object_repr',)
+    list_filter = (('user', AutoCompleteFilter),
+                   ('content_type', RelatedFieldComboFilter),
+                   'action_time',
+                   'action_flag')
+    date_hierarchy = 'action_time'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('user', 'content_type')
+
+    def has_add_permission(self, request):
+        return False
