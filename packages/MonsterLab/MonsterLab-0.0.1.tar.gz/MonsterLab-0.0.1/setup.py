508c476a29d223fe87c41e1273f876a9bb4a624c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:33 +0000

    Add package MonsterLab/0.0.1/MonsterLab-0.0.1.tar.gz

---
+++ packages/MonsterLab/MonsterLab-0.0.1.tar.gz/MonsterLab-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+dev_status = {
+    "Alpha": "Development Status :: 3 - Alpha",
+    "Beta": "Development Status :: 4 - Beta",
+    "Pro": "Development Status :: 5 - Production/Stable",
+    "Mature": "Development Status :: 6 - Mature",
+}
+
+setup(
+    name="MonsterLab",
+    url='https://github.com/BrokenShell/MonsterLab',
+    author="Robert Sharp",
+    author_email="webmaster@sharpdesigndigital.com",
+    version="0.0.1",
+    description="Monster Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="Free for non-commercial use",
+    modules=["MonsterLab.py"],
+    classifiers=[
+        dev_status["Alpha"],
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords=[
+    ],
+    python_requires=">=3.6",
+)
