1b182c4745bf1a61c7b4fac81b4157483bd35b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:38 +0000

    Add package requests-cache/0.7.3/requests-cache-0.7.3.tar.gz

---
+++ packages/requests-cache/requests-cache-0.7.3.tar.gz/requests-cache-0.7.3/tests/unit/test_serializers.py
@@ -0,0 +1,33 @@
+# Note: Almost all serializer logic is covered by parametrized integration tests.
+# Any additional serializer-specific tests can go here.
+import json
+import sys
+from importlib import reload
+from unittest.mock import patch
+
+import pytest
+
+pytestmark = pytest.mark.skipif(
+    sys.version_info < (3, 7), reason='Requires python 3.7+ version of cattrs'
+)
+
+
+@patch.dict(sys.modules, {'ujson': None, 'cattr.preconf.ujson': None})
+def test_stdlib_json():
+    import requests_cache.serializers.preconf
+
+    reload(requests_cache.serializers.preconf)
+    from requests_cache.serializers.preconf import json as module_json
+
+    assert module_json is json
+
+
+def test_ujson():
+    import ujson
+
+    import requests_cache.serializers.preconf
+
+    reload(requests_cache.serializers.preconf)
+    from requests_cache.serializers.preconf import json as module_json
+
+    assert module_json is ujson
