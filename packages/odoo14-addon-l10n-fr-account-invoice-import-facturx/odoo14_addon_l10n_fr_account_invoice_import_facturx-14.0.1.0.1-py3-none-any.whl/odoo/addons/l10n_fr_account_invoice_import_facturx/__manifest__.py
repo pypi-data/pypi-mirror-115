54d07ad021c48c0420abfe03fc3b10c8ce1a33c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:41 +0000

    Add package odoo14-addon-l10n-fr-account-invoice-import-facturx/14.0.1.0.1/odoo14_addon_l10n_fr_account_invoice_import_facturx-14.0.1.0.1-py3-none-any.whl

---
+++ packages/odoo14-addon-l10n-fr-account-invoice-import-facturx/odoo14_addon_l10n_fr_account_invoice_import_facturx-14.0.1.0.1-py3-none-any.whl/odoo/addons/l10n_fr_account_invoice_import_facturx/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2018-2021 Akretion France (http://www.akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Factur-X Invoices Import for France",
+    "version": "14.0.1.0.1",
+    "category": "Localisation",
+    "license": "AGPL-3",
+    "summary": "France-specific module to import Factur-X invoices",
+    "author": "Akretion,Odoo Community Association (OCA)",
+    "maintainers": ["alexis-via"],
+    "development_status": "Alpha",
+    "website": "https://github.com/OCA/l10n-france",
+    "depends": [
+        "account_invoice_import_facturx",
+        "l10n_fr_business_document_import",
+    ],
+    "auto_install": True,
+    "installable": True,
+}
