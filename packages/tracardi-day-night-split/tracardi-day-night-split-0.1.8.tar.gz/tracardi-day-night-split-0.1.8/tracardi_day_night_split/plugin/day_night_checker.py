7147cb445ad5b2d60e820ded4c9a5aaec5a3987b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:22 +0000

    Add package tracardi-day-night-split/0.1.8/tracardi-day-night-split-0.1.8.tar.gz

---
+++ packages/tracardi-day-night-split/tracardi-day-night-split-0.1.8.tar.gz/tracardi-day-night-split-0.1.8/tracardi_day_night_split/plugin/day_night_checker.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from typing import Tuple
+
+from geopy import Nominatim
+from astral import LocationInfo
+from astral.sun import sun
+
+
+def day_night_split(town: str, time_now: datetime) -> Tuple[datetime, datetime]:
+    locator = Nominatim(user_agent="Tracardi")
+    location = locator.geocode(town)
+
+    loc_info = LocationInfo(latitude=location.latitude, longitude=location.longitude)
+
+    sun_info = sun(loc_info.observer, date=time_now)
+
+    return sun_info['sunrise'], sun_info['sunset']
+
+
+def is_day(time_zone):
+    _, town = time_zone.split('/')
+    now = datetime.utcnow()
+
+    sun_rise, sun_set = day_night_split(town, now)
+
+    return sun_rise < now < sun_set
