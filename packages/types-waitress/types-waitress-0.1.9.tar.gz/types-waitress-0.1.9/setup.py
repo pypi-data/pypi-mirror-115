b0a63eed2ee6bfe7ce3fe7c98021ed8f08b4ba02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:44 +0000

    Add package types-waitress/0.1.9/types-waitress-0.1.9.tar.gz

---
+++ packages/types-waitress/types-waitress-0.1.9.tar.gz/types-waitress-0.1.9/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-waitress"
+description = "Typing stubs for waitress"
+long_description = '''
+## Typing stubs for waitress
+
+This is a PEP 561 type stub package for the `waitress` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `waitress`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/waitress. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `8b9d771b67acf13a35905c7d4996382394f23181`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.1.9",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['waitress-stubs'],
+      package_data={'waitress-stubs': ['runner.pyi', 'trigger.pyi', 'parser.pyi', 'buffers.pyi', 'rfc7230.pyi', '__init__.pyi', 'receiver.pyi', 'task.pyi', 'compat.pyi', 'channel.pyi', 'utilities.pyi', 'wasyncore.pyi', 'proxy_headers.pyi', 'adjustments.pyi', 'server.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
