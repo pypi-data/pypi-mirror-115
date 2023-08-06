19e06bd073478840a74e04258f8420adc963b2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:25 +0000

    Add package kjy-distributions/0.1/kjy_distributions-0.1.tar.gz

---
+++ packages/kjy-distributions/kjy_distributions-0.1.tar.gz/kjy_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='kjy_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['kjy_distributions'],
+      zip_safe=False)
