b15a5065c025566c2d83435f19e7d511dfaa1cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:37 +0000

    Add package pymars/0.7.0/pymars-0.7.0-cp38-cp38-win_amd64.whl

---
+++ packages/pymars/pymars-0.7.0-cp38-cp38-win_amd64.whl/mars/services/subtask/worker/service.py
@@ -0,0 +1,47 @@
+# Copyright 1999-2021 Alibaba Group Holding Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict
+
+from .... import oscar as mo
+from .manager import SubtaskManagerActor
+
+
+async def start(config: Dict, address: str):
+    """
+    Start task service on worker.
+
+    Parameters
+    ----------
+    config
+        Service config.
+        {
+            "subtask" : {
+
+            }
+        }
+    address : str
+        Actor pool address.
+    """
+    subtask_config = config.get('subtask', dict())
+    subtask_processor_cls = subtask_config.get('subtask_processor_cls')
+    await mo.create_actor(SubtaskManagerActor,
+                          subtask_processor_cls=subtask_processor_cls,
+                          address=address,
+                          uid=SubtaskManagerActor.default_uid())
+
+
+async def stop(config: dict, address: str):
+    await mo.destroy_actor(mo.create_actor_ref(
+        uid=SubtaskManagerActor.default_uid(), address=address))
