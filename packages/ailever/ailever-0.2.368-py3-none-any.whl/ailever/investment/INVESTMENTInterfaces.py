966ec4be2c8527c8c5e434d17aef221fd5413b47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:12 +0000

    Add package ailever/0.2.368/ailever-0.2.368-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.368-py3-none-any.whl/ailever/investment/INVESTMENTInterfaces.py
@@ -0,0 +1,6 @@
+from .initializer import initialize
+from .finance_datasets import integrated_dataloader
+from .parallelizer import parallelize
+from .screening_markets import reits_screening
+from .portfolio_optimization import portfolio_optimize
+from .forecasting_models import Forecaster
