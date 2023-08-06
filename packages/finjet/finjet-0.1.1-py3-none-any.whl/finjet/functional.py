1503dd5665de3976e98060ce9b772a74d144880b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:37 +0000

    Add package finjet/0.1.1/finjet-0.1.1-py3-none-any.whl

---
+++ packages/finjet/finjet-0.1.1-py3-none-any.whl/finjet/functional.py
@@ -0,0 +1,43 @@
+
+from functools import wraps
+from typing import Any, Callable, Optional, TypeVar
+from finjet.container import Container
+
+
+def get_global_container() -> Optional[Container]:
+    """get global container object
+
+    Returns
+    -------
+    Optional[Container]
+        Container object
+    """
+    if Container.current is None:
+        Container.current = Container()
+    return Container.current
+
+
+T = TypeVar('T')
+
+
+def inject(func: T) -> T:
+    """Decorator function of dependency injection.
+
+    Parameters
+    ----------
+    func : Callable[[Any], Any]
+        Any function or class.
+
+    Returns
+    -------
+    Callable[[Any], Any]
+        func
+    """
+    @wraps(func)
+    def _(*args, **kwargs):
+        container = get_global_container()
+        if container is not None:
+            return container.inject(func)(*args, **kwargs)
+        else:
+            return func(*args, **kwargs)
+    return _
