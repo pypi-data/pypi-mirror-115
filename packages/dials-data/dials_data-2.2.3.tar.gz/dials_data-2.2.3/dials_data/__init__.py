06b344afb526a630b11548c6036927c19935494b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:39 +0000

    Add package dials-data/2.2.3/dials_data-2.2.3.tar.gz

---
+++ packages/dials-data/dials_data-2.2.3.tar.gz/dials_data-2.2.3/dials_data/__init__.py
@@ -0,0 +1,11 @@
+"""
+DIALS Regression Data Manager
+https://github.com/dials/data
+"""
+
+__all__ = []
+__author__ = """Markus Gerstel"""
+__email__ = "dials-support@lists.sourceforge.net"
+__version__ = "2.2.3"
+__commit__ = "9030fbc3d999e63f745c4e31e50d54e94b46d0ea"
+__version_tuple__ = tuple(int(x) for x in __version__.split("."))
