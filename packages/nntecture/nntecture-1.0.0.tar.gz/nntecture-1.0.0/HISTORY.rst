de14e2e2abcd7035b6137343905d46d1ad5ce613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:51 +0000

    Add package nntecture/1.0.0/nntecture-1.0.0.tar.gz

---
+++ packages/nntecture/nntecture-1.0.0.tar.gz/nntecture-1.0.0/HISTORY.rst
@@ -0,0 +1,13 @@
+=======
+History
+=======
+
+1.0.0 (2021-08-07)
+------------------
+
+* Neural Network architecture visualization with DrawNN.
+
+0.1.0 (2021-08-05)
+------------------
+
+* First release on PyPI.
---
+++ packages/nntecture/nntecture-1.0.0.tar.gz/nntecture-1.0.0/nntecture/__init__.py
@@ -0,0 +1,12 @@
+"""
+Neural Network architecture visualization with Python
+=====================================================
+
+NNtecture is a package for Neural Network architecture visualization.
+"""
+
+__author__ = """Kristian Bonnici"""
+__email__ = 'kristiandaaniel@gmail.com'
+__version__ = '1.0.0'
+
+from ._base import DrawNN
