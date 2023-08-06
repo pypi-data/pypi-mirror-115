63074f938441055501c35f6b762ec59d25d473aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:11 +0000

    Add package streammachine-schemas-demo-avro/1.0.1/streammachine_schemas_demo_avro-1.0.1-py3-none-any.whl

---
+++ packages/streammachine-schemas-demo-avro/streammachine_schemas_demo_avro-1.0.1-py3-none-any.whl/streammachine_io_streammachine_schemas_demo_v1/io/streammachine/schemas/demo/v1/__init__.py
@@ -0,0 +1,3 @@
+from ......schema_classes import SchemaClasses
+DemoEvent = SchemaClasses.io.streammachine.schemas.demo.v1.DemoEventClass
+StrmMeta = SchemaClasses.io.streammachine.schemas.demo.v1.StrmMetaClass
