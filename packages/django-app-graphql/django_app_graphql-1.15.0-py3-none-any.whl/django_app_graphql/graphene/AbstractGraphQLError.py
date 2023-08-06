5fe48374b76b7318fd61292a45bce08666ec2dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:38 +0000

    Add package django-app-graphql/1.15.0/django_app_graphql-1.15.0-py3-none-any.whl

---
+++ packages/django-app-graphql/django_app_graphql-1.15.0-py3-none-any.whl/django_app_graphql/graphene/AbstractGraphQLError.py
@@ -0,0 +1,18 @@
+from graphql import GraphQLError
+
+
+class AbstractGraphQLError(GraphQLError):
+    """
+    A graphql error that send several inputs to the client. In order to use it, just derive the class
+    """
+    def __init__(self, error_code: any, **kwargs):
+        super().__init__(
+            message=f"Code={int(self.code):04d} ({str(self.code)})",
+            extensions=dict(kwargs)
+        )
+        self.code = error_code
+
+    def __str__(self) -> str:
+        return f"Code={int(self.code):04d} ({str(self.code)}) data={self.extensions}"
+
+
