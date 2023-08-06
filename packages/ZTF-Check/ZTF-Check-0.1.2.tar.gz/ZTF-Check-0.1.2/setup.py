500f0759dcb8b5009052e566183d691eb7e22d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:06 +0000

    Add package ZTF-Check/0.1.2/ZTF-Check-0.1.2.tar.gz

---
+++ packages/ZTF-Check/ZTF-Check-0.1.2.tar.gz/ZTF-Check-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setup(
+	name="ZTF-Check",
+	version="0.1.2",
+	author='Joseph Guidry',
+	author_email="jaguidry@bu.edu",
+	description="ZTF Check! The quick way to check out your target's FOV in ZTF.",
+    url='https://github.com/astrojoeg/ZTF-Check',
+    install_requires=requirements,
+	packages=find_packages(),
+	entry_points ={
+            'console_scripts': [
+                'ztfcheck = ztfcheck:main'
+            ]
+        },
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Astronomy"
+    ]
+)
