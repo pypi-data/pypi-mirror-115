6d97efa18f6fb2efb222742d17c4d5b11fd200a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:40 +0000

    Add package strawberry-graphql/0.70.4/strawberry_graphql-0.70.4-py3-none-any.whl

---
+++ packages/strawberry-graphql/strawberry_graphql-0.70.4-py3-none-any.whl/strawberry/types/info.py
@@ -0,0 +1,27 @@
+import dataclasses
+from typing import Any, Dict, Generic, List, Optional, TypeVar, Union
+
+from graphql import OperationDefinitionNode
+from graphql.language import FieldNode
+from graphql.pyutils.path import Path
+
+from strawberry.type import StrawberryType
+
+
+ContextType = TypeVar("ContextType")
+RootValueType = TypeVar("RootValueType")
+
+
+@dataclasses.dataclass
+class Info(Generic[ContextType, RootValueType]):
+    field_name: str
+    field_nodes: List[FieldNode]
+    context: ContextType
+    root_value: RootValueType
+    variable_values: Dict[str, Any]
+    # TODO: merge type with StrawberryType when StrawberryObject is implemented
+    return_type: Optional[Union[type, StrawberryType]]
+    # TODO: create an abstraction on these fields
+    operation: OperationDefinitionNode
+    path: Path
+    # TODO: parent_type as strawberry types
