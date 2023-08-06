ade8e0ccfb074093faf68e97231ba874f79e04a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:56 +0000

    Add package scanimate/0.0.5.dev0/scanimate-0.0.5.dev0.tar.gz

---
+++ packages/scanimate/scanimate-0.0.5.dev0.tar.gz/scanimate-0.0.5.dev0/scanimate/scanimate.py
@@ -0,0 +1,12 @@
+# @Email: rah3156@rit.edu
+# @Website: https://ryanhartzell.github.io/scanimate
+# @License: https://github.com/RyanHartzell/scanimate/blob/master/LICENSE
+# @github: https://github.com/RyanHartzell/scanimate
+#
+# Copyright (c) 2020 Ryan Hartzell
+#
+
+if __name__=="__main__":
+    from version_info import __version__
+    print(f"Scanimate Library Version: {__version__}")
+    print("Scanimate script is working!")
