0feba6821a0107bba95a5140048174c21b3ef00f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:59 +0000

    Add package nimbleos/0.0.1/nimbleos-0.0.1.tar.gz

---
+++ packages/nimbleos/nimbleos-0.0.1.tar.gz/nimbleos-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nimbleos',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.nimblebox.ai/',
+    author='nimblebox',
+    author_email='admin@nimblebox.ai',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
