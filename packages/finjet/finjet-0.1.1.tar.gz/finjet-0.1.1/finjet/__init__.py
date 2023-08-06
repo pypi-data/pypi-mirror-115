c16a69a24ba71818447e39668f856c1fca0f7861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:37 +0000

    Add package finjet/0.1.1/finjet-0.1.1.tar.gz

---
+++ packages/finjet/finjet-0.1.1.tar.gz/finjet-0.1.1/finjet/__init__.py
@@ -0,0 +1,5 @@
+from finjet.functional import get_global_container, inject  # NOQA
+from finjet.dependency import Dependency, Depends, Singleton  # NOQA
+from finjet.container import Container  # NOQA
+
+__version__ = '0.1.1'
