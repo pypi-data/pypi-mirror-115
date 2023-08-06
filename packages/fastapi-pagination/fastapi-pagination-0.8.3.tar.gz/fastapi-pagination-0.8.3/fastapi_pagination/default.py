894c8e94e01df3777d8ddc58a0df712441a21501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:44 +0000

    Add package fastapi-pagination/0.8.3/fastapi-pagination-0.8.3.tar.gz

---
+++ packages/fastapi-pagination/fastapi-pagination-0.8.3.tar.gz/fastapi-pagination-0.8.3/fastapi_pagination/default.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+from typing import Any, Generic, Sequence, TypeVar
+
+from fastapi import Query
+from pydantic import BaseModel, conint
+
+from .bases import AbstractParams, BasePage, RawParams
+from .utils import deprecated
+
+T = TypeVar("T")
+
+
+class Params(BaseModel, AbstractParams):
+    page: int = Query(1, ge=1, description="Page number")
+    size: int = Query(50, ge=1, le=100, description="Page size")
+
+    def to_raw_params(self) -> RawParams:
+        return RawParams(
+            limit=self.size,
+            offset=self.size * (self.page - 1),
+        )
+
+
+class PaginationParams(Params):
+    @deprecated  # type: ignore
+    def __init__(self, **kwargs: Any) -> None:  # pragma: no cover
+        super().__init__(**kwargs)
+
+
+class Page(BasePage[T], Generic[T]):
+    page: conint(ge=1)  # type: ignore
+    size: conint(ge=1)  # type: ignore
+
+    __params_type__ = Params
+
+    @classmethod
+    def create(
+        cls,
+        items: Sequence[T],
+        total: int,
+        params: AbstractParams,
+    ) -> Page[T]:
+        if not isinstance(params, Params):
+            raise ValueError("Page should be used with Params")
+
+        return cls(
+            total=total,
+            items=items,
+            page=params.page,
+            size=params.size,
+        )
+
+
+__all__ = [
+    "Params",
+    "Page",
+    "PaginationParams",
+]
