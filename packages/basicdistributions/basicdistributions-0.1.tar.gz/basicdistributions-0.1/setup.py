0feb2d31ca0e5cbdc292cde500e58f039c9a5769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:09 +0000

    Add package basicdistributions/0.1/basicdistributions-0.1.tar.gz

---
+++ packages/basicdistributions/basicdistributions-0.1.tar.gz/basicdistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='basicdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['basicdistributions'],
+      author='Anudeep Vanjavakam',
+      author_email="vanjavakamanudeep@gmail.com",
+      zip_safe=False)
