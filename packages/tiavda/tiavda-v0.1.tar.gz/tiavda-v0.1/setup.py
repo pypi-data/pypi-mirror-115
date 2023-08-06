bac3d17beaa24fd6553f79f1aa08367eaf4170e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:30 +0000

    Add package tiavda/v0.1/tiavda-v0.1.tar.gz

---
+++ packages/tiavda/tiavda-v0.1.tar.gz/tiavda-v0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='tiavda',
+    packages=['tiavda'],
+    version='v0.1',  # Ideally should be same as your GitHub release tag version
+    description='tivda util package',
+    author='Advait Pavuluri',
+    author_email='advait.pavuluri@gmail.com',
+    url='https://github.com/AdvaitPavuluri/tiavda',
+    download_url='https://github.com/AdvaitPavuluri/tiavda/archive/refs/tags/v0.1.tar.gz',
+    keywords=['tiavda'],
+    classifiers=[],
+)
