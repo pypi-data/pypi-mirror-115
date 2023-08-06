84a046290d588be7129dcb217fe2e495146a962c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:27 +0000

    Add package preprocessing-functions/0.1.0/preprocessing_functions-0.1.0.tar.gz

---
+++ packages/preprocessing-functions/preprocessing_functions-0.1.0.tar.gz/preprocessing_functions-0.1.0/preprocessing_functions/img_to_array.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def img_to_array_fn(img, data_format='channels_last', dtype='float32'):
+    """Converts a PIL Image instance to a Numpy array.
+
+    # Arguments
+        img: PIL Image instance.
+        data_format: Image data format,
+            either "channels_first" or "channels_last".
+        dtype: Dtype to use for the returned array.
+
+    # Returns
+        A 3D Numpy array.
+
+    # Raises
+        ValueError: if invalid `img` or `data_format` is passed.
+    """
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format: %s' % data_format)
+    # Numpy array x has format (height, width, channel)
+    # or (channel, height, width)
+    # but original PIL image has format (width, height, channel)
+    x = np.asarray(img, dtype=dtype)
+    if len(x.shape) == 3:
+        if data_format == 'channels_first':
+            x = x.transpose(2, 0, 1)
+    elif len(x.shape) == 2:
+        if data_format == 'channels_first':
+            x = x.reshape((1, x.shape[0], x.shape[1]))
+        else:
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+    else:
+        raise ValueError('Unsupported image shape: %s' % (x.shape,))
+    return x
---
+++ packages/preprocessing-functions/preprocessing_functions-0.1.0.tar.gz/preprocessing_functions-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['preprocessing_functions']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=8.3.1,<9.0.0', 'numpy>=1.19,<2.0']
+
+setup_kwargs = {
+    'name': 'preprocessing-functions',
+    'version': '0.1.0',
+    'description': 'Common preprocessing functions for image deep learning applications packaged independently in order to avoid installing hefty packages such as tensorflow or pytorch in production.',
+    'long_description': None,
+    'author': 'Panagiotis Galopoulos',
+    'author_email': 'gl.panagiotis@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
