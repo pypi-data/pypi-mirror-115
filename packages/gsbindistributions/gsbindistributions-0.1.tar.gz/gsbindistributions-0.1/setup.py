d05d33a6c4c170e0d7f2ec5c9420bc67afbec8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:03 +0000

    Add package gsbindistributions/0.1/gsbindistributions-0.1.tar.gz

---
+++ packages/gsbindistributions/gsbindistributions-0.1.tar.gz/gsbindistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gsbindistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gsbindistributions'],
+      zip_safe=False)
