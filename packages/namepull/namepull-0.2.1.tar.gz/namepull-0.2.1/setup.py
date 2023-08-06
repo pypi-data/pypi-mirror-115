3ef5bafd03cccb3978996b6403f5e4be875ac5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:48 +0000

    Add package namepull/0.2.1/namepull-0.2.1.tar.gz

---
+++ packages/namepull/namepull-0.2.1.tar.gz/namepull-0.2.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['namepull']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyMySQL>=1.0.2,<2.0.0',
+ 'absl-py>=0.13.0,<0.14.0',
+ 'aiohttp>=3.7.4,<4.0.0',
+ 'backoff>=1.11.1,<2.0.0',
+ 'python-sql>=1.2.2,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['namepull = namepull:run']}
+
+setup_kwargs = {
+    'name': 'namepull',
+    'version': '0.2.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Artanicus',
+    'author_email': 'artanicus@nocturnal.fi',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
