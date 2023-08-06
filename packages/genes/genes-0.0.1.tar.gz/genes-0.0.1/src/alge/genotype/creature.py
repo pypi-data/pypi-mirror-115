562cb681f3c03c4ac054f1b8bc29d4d9a5e9427a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package genes/0.0.1/genes-0.0.1.tar.gz

---
+++ packages/genes/genes-0.0.1.tar.gz/genes-0.0.1/src/alge/genotype/creature.py
@@ -0,0 +1,21 @@
+
+from alge.genotype.phenotype import Phenotype
+
+
+class Creature():
+
+    _phenotype: Phenotype
+    _fitness: float
+
+    def __init__(self, phenotype: Phenotype, fitness: float = 0):
+        self._phenotype = phenotype
+        self._fitness = fitness
+
+    def phenotype(self) -> Phenotype:
+        return self._phenotype
+
+    def fitness(self) -> float:
+        return self._fitness
+
+    def set_fitness(self, value: float) -> None:
+        self._fitness = value
