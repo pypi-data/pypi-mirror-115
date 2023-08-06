0fa618f57f04e62ea6d82a2aafbb21f0a9adcc83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:34 +0000

    Add package jelastic-client/1.0.12/jelastic_client-1.0.12-py3-none-any.whl

---
+++ packages/jelastic-client/jelastic_client-1.0.12-py3-none-any.whl/jelastic_client/control_client.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from typing import Dict
+
+from .core import ApiClient, BaseClient, ApiClientException
+
+
+class Status(Enum):
+    Running = 1
+    Down = 2
+    Launching = 3
+    Sleep = 4
+    Unknown = 5
+    Creating = 6
+    Cloning = 7
+    NotExists = 8
+    Exporting = 9
+    Migrating = 10
+    Broken = 11
+    Updating = 12
+    Stopping = 13
+    GoingToSleep = 14
+    Restoring = 15
+
+
+class ControlClient(BaseClient):
+    jelastic_group = "environment"
+
+    jelastic_class = "control"
+
+    def __init__(self, api_client: ApiClient):
+        super().__init__(api_client)
+
+    def delete_env(self, env_name: str) -> None:
+        self.execute(
+            "DeleteEnv",
+            envName=env_name
+        )
+
+    def get_env_info(self, env_name: str) -> Dict:
+        try:
+            response = self.execute(
+                "GetEnvInfo",
+                envName=env_name
+            )
+            return response
+        except ApiClientException as e:
+            return {
+                "result": e.response["result"],
+                "env": {
+                    "status": Status.NotExists
+                }
+            }
+
+    def env_is_running(self, env_name: str) -> bool:
+        env_info = self.get_env_info(env_name)
+        env_status = Status(env_info["env"]["status"])
+
+        return env_status == Status.Running
+
+    def env_exists(self, env_name: str) -> bool:
+        env_info = self.get_env_info(env_name)
+        env_status = Status(env_info["env"]["status"])
+
+        return env_status is not Status.NotExists and env_status is not Status.Unknown
