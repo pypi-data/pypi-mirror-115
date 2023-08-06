b3e28d8fd57170f94b6248745b830af8579ad5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:18 +0000

    Add package fastel/1.1.5/fastel-1.1.5-py3-none-any.whl

---
+++ packages/fastel/fastel-1.1.5-py3-none-any.whl/fastel/__init__.py
@@ -0,0 +1,24 @@
+"""revtel fastapi package"""
+
+from fastel.authorizers import (
+    ClientIdAuth,
+    ClientSecretAuth,
+    Credential,
+    JWBaseAuth,
+    StaticClientAuth,
+    StaticJWTAuth,
+    StaticSecretAuth,
+)
+from fastel.exceptions import APIException
+
+__version__ = "1.1.5"
+__all__ = [
+    "APIException",
+    "ClientIdAuth",
+    "ClientSecretAuth",
+    "Credential",
+    "JWBaseAuth",
+    "StaticClientAuth",
+    "StaticSecretAuth",
+    "StaticJWTAuth",
+]
