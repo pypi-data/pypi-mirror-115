3de2ea646358c1d9401b2b3e4964a24f68c0d14a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:04 +0000

    Add package clearml/1.0.5/clearml-1.0.5-py2.py3-none-any.whl

---
+++ packages/clearml/clearml-1.0.5-py2.py3-none-any.whl/clearml/automation/__init__.py
@@ -0,0 +1,9 @@
+from .parameters import UniformParameterRange, DiscreteParameterRange, UniformIntegerParameterRange, ParameterSet
+from .optimization import GridSearch, RandomSearch, HyperParameterOptimizer, Objective
+from .job import ClearmlJob
+from .controller import PipelineController
+from .scheduler import TaskScheduler
+
+__all__ = ["UniformParameterRange", "DiscreteParameterRange", "UniformIntegerParameterRange", "ParameterSet",
+           "GridSearch", "RandomSearch", "HyperParameterOptimizer", "Objective", "ClearmlJob", "PipelineController",
+           "TaskScheduler"]
