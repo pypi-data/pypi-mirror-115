32d723c32ac3a90149e1d839d07e165a818a9243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:12 +0000

    Add package ailever/0.2.417/ailever-0.2.417-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.417-py3-none-any.whl/ailever/investment/_base_trigger_blocks.py
@@ -0,0 +1,424 @@
+from .__base_structures import BaseTriggerBlock
+from ._fmlops_policy import fmlops_bs
+from ._base_transfer import ModelTransferCore
+from .fmlops_nomenclatures import F_MRN
+
+from importlib import import_module
+from functools import partial
+import torch
+
+
+class TorchTriggerBlock(BaseTriggerBlock):
+    def __init__(self, training_info:dict, local_environment:dict=local_environment, remote_environment:dict=None):
+        """
+        local_environment['feature_store'] = ''
+        local_environment['source_repository'] = ''
+        local_environment['model_registry'] = ''
+        local_environment['metadata_store'] = ''
+        local_environment['model_loading_path'] = ''
+        local_environment['model_saving_path'] = ''
+        """
+        self.local_environment = local_environment
+        self.remote_environment = remote_environment
+
+        self.prediction() 
+        self.outcome_report()
+
+    def initializing_local_model_registry(self):
+        pass
+
+    def initializing_remote_model_registry(self):
+        pass
+    
+    @staticmethod
+    def _dynamic_import(architecture, module):
+        return getattr(import_module(f'.fmlops_forecasters.torch.{architecture}'), module)
+
+    def _instance_basis(self, train_specification):
+        architecutre = train_specification['architecture']
+        InvestmentDataLoader = self.dynamic_import(architecture, 'InvestmentDataLoader')
+        Model = self.dynamic_import(architecture, 'Model')
+        Criterion = self.dynamic_import(architecture, 'Criterion')
+        Optimizer = self.dynamic_import(architecture, 'Optimizer')
+
+        train_dataloader, test_dataloader = InvestmentDataLoader(train_specification)
+        model = Model(train_specification)
+        criterion = Criterion(train_specification)
+        optimizer = Optimizer(train_specification)
+
+        # instance update
+        checkpoint = torch.load(os.path.join(source_repository['source_repository'], source))
+        model.load_state_dict(checkpoint['model_state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        return train_dataloader, test_dataloader, model, criterion, optimizer
+
+    def train(self, train_specification):
+        train_dataloader, test_dataloader, model, criterion, optimizer = self._instance_basis(train_specification)
+        for epoch in range(epochs):
+            training_losses = []
+            model.train()
+            for batch_idx, (x_train, y_train) in enumerate(train_dataloader):
+                # Forward
+                hypothesis = model(x_train.squeeze().float().to(training_info['device']))
+                cost = criterion(hypothesis.squeeze().float().to(training_info['device']), y_train.squeeze().float().to(training_info['device']))
+                # Backward
+                optimizer.zero_grad()
+                cost.backward()
+                optimizer.step()
+                training_losses.append(cost)
+            # Alert
+            TrainMSE = torch.mean(torch.tensor(training_losses)).data
+            if 'cumulative_epochs' in training_info.keys():
+                previous_cumulative_epochs = training_info['cumulative_epochs']
+                cumulative_epochs = training_info['cumulative_epochs'] + epochs
+                print(f'[Training  ][{epoch+1+previous_cumulative_epochs}/{cumulative_epochs}]', float(TrainMSE))
+            else:
+                print(f'[Training  ][{epoch+1}/{epochs}]', float(TrainMSE))
+            
+            if torch.isnan(TrainMSE).all():
+                break 
+
+            with torch.no_grad():
+                validation_losses = []
+                model.eval()
+                for batch_idx, (x_train, y_train) in enumerate(test_dataloader):
+                    # Forward
+                    hypothesis = model(x_train.squeeze().float().to(training_info['device']))
+                    cost = criterion(hypothesis.squeeze().float().to(training_info['device']), y_train.squeeze().float().to(training_info['device']))
+                    validation_losses.append(cost)
+                # Alert
+                ValidationMSE = torch.mean(torch.tensor(validation_losses)).data
+                if 'cumulative_epochs' in training_info.keys():
+                    previous_cumulative_epochs = training_info['cumulative_epochs']
+                    cumulative_epochs = training_info['cumulative_epochs'] + epochs
+                    print(f'[Validation][{epoch+1+previous_cumulative_epochs}/{cumulative_epochs}]', float(ValidationMSE))
+                else:
+                    print(f'[Validation][{epoch+1}/{epochs}]', float(ValidationMSE))
+
+        self.save_in_local_model_registry()
+        self.save_in_remote_model_registry()
+
+    def prediction(self):
+        pass
+
+    def load_from_ailever_source_repository(self):
+        return None
+
+    def load_from_local_rawdata_repository(self):
+        return None
+
+    def load_from_local_feature_store(self):
+        return None
+
+    def load_from_local_source_repository(self):
+        return None
+
+    def load_from_local_model_registry(self):
+        return None
+
+    def load_from_local_metadata_store(self):
+        return None
+
+    def remote_from_local_rawdata_repository(self):
+        return None
+
+    def remote_from_local_feature_store(self):
+        return None
+
+    def remote_from_remote_source_repository(self):
+        return None
+
+    def remote_from_remote_model_registry(self):
+        return None
+
+    def remote_from_remote_matadata_store(self):
+        return None
+
+    def save_in_local_feature_store(self):
+        pass
+
+    def save_in_local_source_repository(self):
+        pass
+
+    def save_in_local_model_registry(self):
+        pass
+
+    def save_in_local_metadata_store(self):
+        pass
+
+    def save_in_remote_feature_store(self):
+        pass
+
+    def save_in_remote_source_repository(self):
+        pass
+
+    def save_in_remote_model_registry(self):
+        pass
+
+    def save_in_remote_metadata_store(self):
+        pass
+
+    def ModelTransferCore(self):
+        modelcore = ModelTransferCore()
+        return modelcore
+
+
+
+class TensorflowTriggerBlock(BaseTriggerBlock):
+    def __init__(self, training_info:dict, local_environment:dict=local_environment, remote_environment:dict=None):
+        self.local_environment = local_environmnet
+        self.remote_environment = remote_environment
+
+    def initializing_local_model_registry(self):
+        pass    
+
+    def initializing_remote_model_registry(self):
+        pass
+
+    @staticmethod
+    def _dynamic_import(model_specification, module):
+        return getattr(import_module(f'.fmlops_forecasters.tensorflow.{model_specification}'), module)
+
+    def _instance_basis(self, train_specification):
+        pass
+
+    def train(self):
+        pass
+
+    def prediction(self):
+        pass
+
+    def load_from_ailever_source_repository(self):
+        return None
+
+    def load_from_local_rawdata_repository(self):
+        return None
+
+    def load_from_local_feature_store(self):
+        return None
+
+    def load_from_local_source_repository(self):
+        return None
+
+    def load_from_local_model_registry(self):
+        return None
+
+    def load_from_local_metadata_store(self):
+        return None
+
+    def remote_from_local_rawdata_repository(self):
+        return None
+
+    def remote_from_local_feature_store(self):
+        return None
+
+    def remote_from_remote_source_repository(self):
+        return None
+
+    def remote_from_remote_model_registry(self):
+        return None
+
+    def remote_from_remote_matadata_store(self):
+        return None
+
+    def save_in_local_feature_store(self):
+        pass
+
+    def save_in_local_source_repository(self):
+        pass
+
+    def save_in_local_model_registry(self):
+        pass
+
+    def save_in_local_metadata_store(self):
+        pass
+
+    def save_in_remote_feature_store(self):
+        pass
+
+    def save_in_remote_source_repository(self):
+        pass
+
+    def save_in_remote_model_registry(self):
+        pass
+
+    def save_in_remote_metadata_store(self):
+        pass
+
+    def ModelTransferCore(self):
+        modelcore = ModelTransferCore()
+        return modelcore
+
+
+
+class SklearnTriggerBlock(BaseTriggerBlock):
+    def __init__(self, training_info:dict, local_environment:dict=local_environment, remote_environment:dict=None):
+        self.local_environment = local_environmnet
+        self.remote_environment = remote_environment
+
+    def initializing_local_model_registry(self):
+        pass
+
+    def initializing_remote_model_registry(self):
+        pass
+
+    @staticmethod
+    def _dynamic_import(model_specification, module):
+        return getattr(import_module(f'.fmlops_forecasters.sklearn.{model_specification}'), module)
+
+    def _instance_basis(self, train_specification):
+        pass
+
+    def train(self):
+        pass
+
+    def prediction(self):
+        pass
+
+    def load_from_ailever_source_repository(self):
+        return None
+
+    def load_from_local_rawdata_repository(self):
+        return None
+
+    def load_from_local_feature_store(self):
+        return None
+
+    def load_from_local_source_repository(self):
+        return None
+
+    def load_from_local_model_registry(self):
+        return None
+
+    def load_from_local_metadata_store(self):
+        return None
+
+    def remote_from_local_rawdata_repository(self):
+        return None
+
+    def remote_from_local_feature_store(self):
+        return None
+
+    def remote_from_remote_source_repository(self):
+        return None
+
+    def remote_from_remote_model_registry(self):
+        return None
+
+    def remote_from_remote_matadata_store(self):
+        return None
+
+    def save_in_local_feature_store(self):
+        pass
+
+    def save_in_local_source_repository(self):
+        pass
+
+    def save_in_local_model_registry(self):
+        pass
+
+    def save_in_local_metadata_store(self):
+        pass
+
+    def save_in_remote_feature_store(self):
+        pass
+
+    def save_in_remote_source_repository(self):
+        pass
+
+    def save_in_remote_model_registry(self):
+        pass
+
+    def save_in_remote_metadata_store(self):
+        pass
+
+    def ModelTransferCore(self):
+        modelcore = ModelTransferCore()
+        return modelcore
+
+class StatsmodelsTriggerBlock(BaseTriggerBlock):
+    def __init__(self, training_info:dict, local_environment:dict=local_environment, remote_environment:dict=None):
+        self.local_environment = local_environmnet
+        self.remote_environment = remote_environment
+
+    def initializing_local_model_registry(self):
+        pass
+
+    def initializing_remote_model_registry(self):
+        pass
+
+    @staticmethod
+    def _dynamic_import(model_specification, module):
+        return getattr(import_module(f'.fmlops_forecasters.statsmodels.{model_specification}'), module)
+
+    def _instance_basis(self, source):
+        pass
+
+    def train(self):
+        pass
+
+    def prediction(self):
+        pass
+
+    def load_from_ailever_source_repository(self):
+        return None
+
+    def load_from_local_rawdata_repository(self):
+        return None
+
+    def load_from_local_feature_store(self):
+        return None
+
+    def load_from_local_source_repository(self):
+        return None
+
+    def load_from_local_model_registry(self):
+        return None
+
+    def load_from_local_metadata_store(self):
+        return None
+
+    def remote_from_local_rawdata_repository(self):
+        return None
+
+    def remote_from_local_feature_store(self):
+        return None
+
+    def remote_from_remote_source_repository(self):
+        return None
+
+    def remote_from_remote_model_registry(self):
+        return None
+
+    def remote_from_remote_matadata_store(self):
+        return None
+
+    def save_in_local_feature_store(self):
+        pass
+
+    def save_in_local_source_repository(self):
+        pass
+
+    def save_in_local_model_registry(self):
+        pass
+
+    def save_in_local_metadata_store(self):
+        pass
+
+    def save_in_remote_feature_store(self):
+        pass
+
+    def save_in_remote_source_repository(self):
+        pass
+
+    def save_in_remote_model_registry(self):
+        pass
+
+    def save_in_remote_metadata_store(self):
+        pass
+
+    def ModelTransferCore(self):
+        modelcore = ModelTransferCore()
+        return modelcore
+
+
---
+++ packages/ailever/ailever-0.2.417-py3-none-any.whl/ailever/investment/forecasting_models.py
@@ -0,0 +1,48 @@
+from ._fmlops_policy import fmlops_bs
+
+class Forecaster:
+    def __init__(self, local_environment:dict=None, remote_environment:dict=None, framework:str='torch'):
+        if framework == 'torch':
+            from ._base_trigger_blocks import TorchTriggerBlock
+            self.trigger_block = TorchTriggerBlock(local_environment=local_environment, remote_environment=remote_environment)
+        elif framework == 'tensorflow':
+            from ._base_trigger_blocks import TensorflowTriggerBlock
+            self.trigger_block = TensorflowForecaster(local_environment=local_environment, remote_environment=remote_environment)
+        elif framework == 'sklearn':
+            from ._base_trigger_blocks import SklearnTriggerBlock
+            self.trigger_block = SklearnForecaster(local_environment=local_environment, remote_environment=remote_environment)
+        elif framework == 'statsmodels':
+            from ._base_trigger_blocks import StatsmodelsTriggerBlock
+            self.trigger_block = StatsmodelsForecaster(local_environment=local_environment, remote_environment=remote_environment)
+        else:
+            assert False, '[AILEVER] The base framework for training models was not yet prepared.'
+
+    def train_trigger(self, baskets:list, train_specifications:dict):
+        for security in baskets:
+            self.trigger_block.train(train_specifications[security])
+            self.trigger_block.save()
+
+    def remove(self, baskets:list):
+        answer = input("Type 'Yes' if you really want to delete the baskets")
+        if answer == 'Yes':
+            return
+        else:
+            return
+
+    def evaluation_trigger(self):
+        pass
+
+    def report(self, baskets:list):
+        modelcore = self.trigger_block.ModelTransferCore()
+        return modelcore
+
+    def upload(self):
+        pass
+
+    def max_profit(self):
+        pass
+
+    def summary(self):
+        pass
+
+
