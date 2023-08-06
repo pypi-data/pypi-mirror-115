f05a22b4287ab2389278cc706431d9ce22ab822f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:45 +0000

    Add package eyecu-pulsar/0.1.3/eyecu_pulsar-0.1.3.tar.gz

---
+++ packages/eyecu-pulsar/eyecu_pulsar-0.1.3.tar.gz/eyecu_pulsar-0.1.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pulsar']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['opencv-python>=4,<5']
+
+setup_kwargs = {
+    'name': 'eyecu-pulsar',
+    'version': '0.1.3',
+    'description': 'RTSP Sync Library',
+    'long_description': None,
+    'author': 'Oguz Vuruskaner',
+    'author_email': 'ovuruska@outlook.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/eyecuvision/pulsar',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
