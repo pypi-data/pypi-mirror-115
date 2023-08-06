4f98858aa533a3828f02cafa38a4be81720fad11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:48 +0000

    Add package distribution-models/0.1/distribution_models-0.1.tar.gz

---
+++ packages/distribution-models/distribution_models-0.1.tar.gz/distribution_models-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution_models',
+      version='0.1',
+      description='some distribution models',
+      packages=['distribution_models'],
+      zip_safe=False)
