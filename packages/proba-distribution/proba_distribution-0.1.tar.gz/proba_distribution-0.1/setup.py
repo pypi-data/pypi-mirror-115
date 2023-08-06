03efc9a43869d5cc6fc14823a21e6a0664aa3a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:03 +0000

    Add package proba-distribution/0.1/proba_distribution-0.1.tar.gz

---
+++ packages/proba-distribution/proba_distribution-0.1.tar.gz/proba_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='proba_distribution',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['proba_distribution'],
+      author = 'Chidi Chukwumezie',
+      author_email = 'austinmilan@yahoo.com',
+      zip_safe=False)
