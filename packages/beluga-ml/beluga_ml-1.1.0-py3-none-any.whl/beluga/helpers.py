abb0b7951dc63bec1682a75ec74ed1f2991971b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:08 +0000

    Add package beluga-ml/1.1.0/beluga_ml-1.1.0-py3-none-any.whl

---
+++ packages/beluga-ml/beluga_ml-1.1.0-py3-none-any.whl/beluga/helpers.py
@@ -0,0 +1,140 @@
+from typing import Tuple, Collection, Union
+import numpy as np
+
+
+def display_helper(raw_metrics: dict, header: str = "") -> bool:
+    """A helper function to print out the calculated metrics
+
+    Args:
+        raw_metrics (dict): The input metrics to print
+        header (str, optional): The title of the metrics. Defaults to "".
+
+    Raises:
+        ValueError: If the input metrics are not in Dictionary format
+
+    Returns:
+        bool: True if the print succeeds, False otherwise
+    """
+
+    if not isinstance(raw_metrics, dict):
+        raise ValueError("Your raw metrics is not a dictionary")
+
+    max_label_len = max([len(str(lab)) for lab in raw_metrics.keys()] + [5])
+
+    try:
+        print()
+        if header:
+            print(header)
+
+        print("=" * (max_label_len + 9))
+        for key, val in raw_metrics.items():
+            key = str(key).ljust(max_label_len + 2, " ")
+            print("{0} {1:.4f}".format(key, val))
+        print("=" * (max_label_len + 9))
+        print()
+
+    except Exception:
+        return False
+
+    return True
+
+
+def summary_display(summary_data: dict, total_acc: Union[dict, bool]) -> bool:
+    """Displays the given metrics in a formatted way
+
+    Args:
+        summary_data (dict): The input metrics to be displayed
+        total_acc (Union[dict, bool]): The total model accuracy
+
+    Raises:
+        ValueError: If the input metrics are not in Dictionary format
+
+    Returns:
+        bool: True if the print succeeds, False otherwise
+    """
+
+    if not isinstance(summary_data, dict) or not isinstance(total_acc, dict):
+        raise ValueError("Your summary data is not a dictionary")
+
+    labels = list([i.keys() for i in summary_data.values()][0])
+    max_label_len = max([len(str(labe)) for labe in labels] + [5])
+    max_len = max_label_len + 60
+
+    headers = list(summary_data.keys())
+    data: dict = {}
+    for label in labels:
+        for metrics in summary_data.values():
+            for key, val in metrics.items():
+                if key == label:
+                    data[label] = data.get(label, []) + [val]
+
+    total_acc = round(list(total_acc.values())[0], 4)
+
+    try:
+        print()
+        print(("{0:^" + str(max_len) + "}").format("Machine Learning Model Summary"))
+        print("=" * (max_len))
+        print(
+            (
+                "| {0:^"
+                + str(max_label_len)
+                + "}  {1:^12}  {2:^12}  {3:^12}  {4:^12} |"
+            ).format("Label", headers[0], headers[1], headers[2], headers[3])
+        )
+        for lab, dat in data.items():
+            print(
+                (
+                    "| {0:^"
+                    + str(max_label_len)
+                    + "}  {1:^12.4f}  {2:^12.4f}  {3:^12.4f}  {4:^12.4f} |"
+                ).format(lab, dat[0], dat[1], dat[2], dat[3])
+            )
+        print("-" * (max_len))
+        print(
+            ("| {0:^" + str(max_len - 4) + "} |").format(
+                "Total Model Accuracy =  " + str(total_acc)
+            )
+        )
+        print("=" * (max_len))
+        print()
+
+    except Exception:
+        return False
+
+    return True
+
+
+def array_check(
+    array_1: Collection, array_2: Collection
+) -> Tuple[np.ndarray, np.ndarray]:
+    """A helper function to check if the given arrays are valid
+
+    Args:
+        array_1 (Collection): The first array to check
+        array_2 (Collection): The second array to check
+
+    Raises:
+        TypeError: If either of the given arrays are not iterable
+        ValueError: If the arrays are not of the same length
+        ValueError: If both of the arrays are empty
+
+    Returns:
+        Tuple[np.ndarray, np.ndarray]: Numpy arrays ready for future processing
+    """
+
+    try:
+        iter(array_1)
+        iter(array_2)
+    except TypeError:
+        raise TypeError("Your data is not iterable, check your input data") from None
+
+    if len(array_1) != len(array_2):
+        raise ValueError("Your arrays do not match in length")
+
+    if len(array_1) == 0 and len(array_2) == 0:
+        raise ValueError("You passed empty arrays")
+
+    if isinstance(array_1, np.ndarray) and isinstance(array_2, np.ndarray):
+        return array_1, array_2
+
+    return np.array(array_1), np.array(array_2)
---
+++ packages/beluga-ml/beluga_ml-1.1.0-py3-none-any.whl/beluga/metrics.py
@@ -0,0 +1,410 @@
+from .helpers import np, Collection, Union
+from . import helpers
+
+
+def true_positive(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Number of correctly classified labels of the positive class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    predictions, ground_truth = helpers.array_check(predictions, ground_truth)
+
+    labels = np.unique(ground_truth)
+
+    raw_metrics = {}
+
+    for lab in labels:
+
+        pred_true = np.where(predictions == lab, 1, 0)
+        truth_true = np.where(ground_truth == lab, 1, -1)
+
+        raw_metrics[lab] = np.sum(np.where(pred_true == truth_true, 1, 0))
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "True Positive")
+
+
+def true_negative(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Number of correctly classified labels of the negative class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    predictions, ground_truth = helpers.array_check(predictions, ground_truth)
+
+    labels = np.unique(ground_truth)
+
+    raw_metrics = {}
+
+    for lab in labels:
+
+        pred_true = np.where(predictions != lab, 1, 0)
+        truth_true = np.where(ground_truth != lab, 1, -1)
+
+        raw_metrics[lab] = np.sum(np.where(pred_true == truth_true, 1, 0))
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "True Negative")
+
+
+def false_positive(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Number of incorrectly classified labels of the postive class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    predictions, ground_truth = helpers.array_check(predictions, ground_truth)
+
+    labels = np.unique(ground_truth)
+
+    raw_metrics = {}
+
+    for lab in labels:
+
+        pred_true = np.where(predictions == lab, 1, 0)
+        truth_true = np.where(ground_truth != lab, 1, -1)
+
+        raw_metrics[lab] = np.sum(np.where(pred_true == truth_true, 1, 0))
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "False Positive")
+
+
+def false_negative(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Number of incorrectly classified labels of the negative class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    predictions, ground_truth = helpers.array_check(predictions, ground_truth)
+
+    labels = np.unique(ground_truth)
+
+    raw_metrics = {}
+
+    for lab in labels:
+
+        pred_true = np.where(predictions != lab, 1, 0)
+        truth_true = np.where(ground_truth == lab, 1, -1)
+
+        raw_metrics[lab] = np.sum(np.where(pred_true == truth_true, 1, 0))
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "False Negative")
+
+
+def precision(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of positive class predictions that are correct
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    tp = true_positive(predictions, ground_truth, raw=True)
+    fp = false_positive(predictions, ground_truth, raw=True)
+
+    raw_metrics = {}
+
+    if isinstance(tp, dict) and isinstance(fp, dict):
+
+        for (tp_key, tp_val) in tp.items():
+            if tp_val == 0 and fp[tp_key] == 0:
+                raw_metrics[tp_key] = 0
+            else:
+                raw_metrics[tp_key] = tp_val / (tp_val + fp[tp_key])
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "Precision")
+
+
+def recall(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of correctly classified labels from the positive class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    tp = true_positive(predictions, ground_truth, raw=True)
+    fn = false_negative(predictions, ground_truth, raw=True)
+
+    raw_metrics = {}
+
+    if isinstance(tp, dict) and isinstance(fn, dict):
+
+        for (tp_key, tp_val) in tp.items():
+            if tp_val == 0 and fn[tp_key] == 0:
+                raw_metrics[tp_key] = 0
+            else:
+                raw_metrics[tp_key] = tp_val / (tp_val + fn[tp_key])
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "Recall")
+
+
+def sensitivity(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of correctly classified labels from the positive class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    raw_metrics = recall(predictions, ground_truth, raw=True)
+
+    if raw:
+        return raw_metrics
+
+    if isinstance(raw_metrics, dict):
+        output = helpers.display_helper(raw_metrics, "Sensitivity")
+
+    return output
+
+
+def specificity(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of correctly classified labels from the negative class
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    tn = true_negative(predictions, ground_truth, raw=True)
+    fp = false_positive(predictions, ground_truth, raw=True)
+
+    raw_metrics = {}
+
+    if isinstance(tn, dict) and isinstance(fp, dict):
+
+        for (tn_key, tn_val) in tn.items():
+            if tn_val == 0 and fp[tn_key] == 0:
+                raw_metrics[tn_key] = 0
+            else:
+                raw_metrics[tn_key] = tn_val / (tn_val + fp[tn_key])
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "Specificity")
+
+
+def f1(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """The harmomic mean of precision and recall
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    prec = precision(predictions, ground_truth, raw=True)
+    rec = recall(predictions, ground_truth, raw=True)
+
+    raw_metrics = {}
+
+    if isinstance(prec, dict) and isinstance(rec, dict):
+
+        for (prec_key, prec_val) in prec.items():
+            if prec_val == 0 and rec[prec_key] == 0:
+                raw_metrics[prec_key] = 0
+            else:
+                raw_metrics[prec_key] = (2 * prec_val * rec[prec_key]) / (
+                    prec_val + rec[prec_key]
+                )
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "F1 score")
+
+
+def accuracy(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of correctly classified labels
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    tp = true_positive(predictions, ground_truth, raw=True)
+    tn = true_negative(predictions, ground_truth, raw=True)
+    fp = false_positive(predictions, ground_truth, raw=True)
+    fn = false_negative(predictions, ground_truth, raw=True)
+
+    raw_metrics = {}
+
+    if (
+        isinstance(tp, dict)
+        and isinstance(tn, dict)
+        and isinstance(fp, dict)
+        and isinstance(fn, dict)
+    ):
+
+        for (tp_key, tp_val) in tp.items():
+            raw_metrics[tp_key] = (tp_val + tn[tp_key]) / (
+                tp_val + fp[tp_key] + fn[tp_key] + tn[tp_key]
+            )
+
+    if raw:
+        return raw_metrics
+
+    return helpers.display_helper(raw_metrics, "Accuracy")
+
+
+def model_accuracy(
+    predictions: Collection, ground_truth: Collection, raw: bool = False
+) -> Union[dict, bool]:
+    """Percentage of correctly classified labels for the total model
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        raw (bool, optional): If the raw metrics are to be returned.
+            Defaults to False.
+
+    Returns:
+        Union[dict, bool]: The raw metrics if specified,
+            otherwise the output of the printing function
+    """
+
+    predictions, ground_truth = helpers.array_check(predictions, ground_truth)
+
+    acc: dict = {"Total": np.sum(predictions == ground_truth) / len(ground_truth)}
+
+    if raw:
+        return acc
+
+    return helpers.display_helper(acc, "Model Accuracy")
+
+
+def summary(
+    predictions: Collection, ground_truth: Collection, conditions: bool = False
+) -> bool:
+    """Prints a summary of the given metrics
+
+    Args:
+        predictions (Collection): Predicted classes from the model
+        ground_truth (Collection): Correct classes from the data
+        conditions (bool, optional): If the Recall and F1 are to be displayed,
+            or the Sensitivity and Specificity. Defaults to False.
+
+    Returns:
+        bool: True if the print succeeds, False otherwise
+    """
+
+    summary_data = {
+        "Accuracy": accuracy(predictions, ground_truth, raw=True),
+        "Precision": precision(predictions, ground_truth, raw=True),
+    }
+
+    if conditions:
+        summary_data["Sensitivity"] = sensitivity(predictions, ground_truth, raw=True)
+        summary_data["Specificity"] = specificity(predictions, ground_truth, raw=True)
+    else:
+        summary_data["Recall"] = recall(predictions, ground_truth, raw=True)
+        summary_data["F1"] = f1(predictions, ground_truth, raw=True)
+
+    total_acc = model_accuracy(predictions, ground_truth, raw=True)
+
+    return helpers.summary_display(summary_data, total_acc)
