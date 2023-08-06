dfe11e5cc1b6009f82123dda64f71c7cff9c27e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:25 +0000

    Add package rejected-peps/0.1.0/rejected-peps-0.1.0.tar.gz

---
+++ packages/rejected-peps/rejected-peps-0.1.0.tar.gz/rejected-peps-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rejected-peps",
+    version="0.1.0",
+    author="wyz23x2",
+    author_email="wyz23x2@163.com",
+    description="A python package that implements some rejected or withdrawn PEPs.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/wyz23x2/rejected/",
+    project_urls={
+        "Bug Tracker": "https://gitee.com/wyz23x2/rejected-peps/issues",
+        "Wiki": "https://gitee.com/wyz23x2/rejected-peps/wikis"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Development Status :: 3 - Alpha",
+    ],
+    package_dir={"": "rejected-peps"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
