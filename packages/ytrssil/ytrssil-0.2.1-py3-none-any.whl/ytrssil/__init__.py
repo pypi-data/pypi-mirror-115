750bb4da2d799e99c35764ac028a45919da7bd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:19 +0000

    Add package ytrssil/0.2.1/ytrssil-0.2.1-py3-none-any.whl

---
+++ packages/ytrssil/ytrssil-0.2.1-py3-none-any.whl/ytrssil/__init__.py
@@ -0,0 +1,10 @@
+from collections.abc import Sequence
+
+from ytrssil.api import get_new_videos
+from ytrssil.datatypes import Channel, Video
+
+__all__: Sequence[str] = (
+    'Channel',
+    'Video',
+    'get_new_videos',
+)
