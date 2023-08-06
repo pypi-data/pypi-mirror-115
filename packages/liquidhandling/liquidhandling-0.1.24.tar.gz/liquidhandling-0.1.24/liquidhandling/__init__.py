8a14a79e9442295f979d4da0cb0479f32e6edd30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:42 +0000

    Add package liquidhandling/0.1.24/liquidhandling-0.1.24.tar.gz

---
+++ packages/liquidhandling/liquidhandling-0.1.24.tar.gz/liquidhandling-0.1.24/liquidhandling/__init__.py
@@ -0,0 +1,7 @@
+from liquidhandling.hudson.CherryPicking import CherryPicking
+from liquidhandling.hudson.RapidPick import RapidPick
+from liquidhandling.hudson.SoftLinx import SoftLinx
+from liquidhandling.hudson.SoloSoft import SoloSoft
+from liquidhandling.Properties import Properties
+from liquidhandling.VolumeManager import VolumeManager
+from liquidhandling.Plates import *
