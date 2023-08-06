33e5583764bd7aba7ab71a5ee5798b86c8f16f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:55 +0000

    Add package ScrollableFrame/0.0.1/ScrollableFrame-0.0.1.tar.gz

---
+++ packages/ScrollableFrame/ScrollableFrame-0.0.1.tar.gz/ScrollableFrame-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="ScrollableFrame",
+    version='0.0.1',
+    author="larryw3i",
+    author_email="",
+    description="Your tkinter ScrollableFrame",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/larryw3i/Funing",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    include_package_data = True,
+)
