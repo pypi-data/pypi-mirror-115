5507bb33b1f10c8d336b4a06aabd830135599744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:22 +0000

    Add package PyScoreSaber/0.0.2/PyScoreSaber-0.0.2-py3-none-any.whl

---
+++ packages/PyScoreSaber/PyScoreSaber-0.0.2-py3-none-any.whl/pyscoresaber/models/__init__.py
@@ -0,0 +1,3 @@
+from .player import Player
+from .score import Score
+from .score_stats import ScoreStats
---
+++ packages/PyScoreSaber/PyScoreSaber-0.0.2-py3-none-any.whl/pyscoresaber/models/score.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from dataclasses_json import dataclass_json
+
+from .fields import datetime_field, default
+
+
+@dataclass_json
+@dataclass
+class Score:
+    """Score data from ScoreSaber"""
+
+    rank: int = default()
+    score_id: int = default("scoreId")
+    score: int = default()
+    unmodified_score: int = default("unmodififiedScore")
+    mods: str = default()
+    pp: float = default()
+    weight: float = default()
+    time_set: datetime = datetime_field("timeSet")
+    leaderboard_id: int = default("leaderboardId")
+    song_hash: str = default("songHash")
+    song_name: str = default("songName")
+    song_sub_name: str = default("songSubName")
+    song_author_name: str = default("songAuthorName")
+    level_author_name: str = default("levelAuthorName")
+    difficulty: int = default()
+    difficulty_raw: str = default("difficultyRaw")
+    max_score: int = default("maxScore")
