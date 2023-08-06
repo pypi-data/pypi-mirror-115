a7bffc4772080e631b082d14bf05a4b3e74ebf43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:30 +0000

    Add package Ocean-Agent-Worker/0.0.3/Ocean_Agent_Worker-0.0.3-py3-none-any.whl

---
+++ packages/Ocean-Agent-Worker/Ocean_Agent_Worker-0.0.3-py3-none-any.whl/worker/cmd/join.py
@@ -0,0 +1,33 @@
+import json
+import os
+import subprocess
+import logging
+
+import requests
+
+
+def join(endpoint):
+    uri = f"http://{endpoint}/join" # TODO: insecure -> https로 바꿀 것
+    response = requests.post(uri)
+    join_info = response.json()
+
+    # Run install-master script from internet
+    install(join_info)
+
+
+def install(join_info):
+    logging.info("[INSTALL]")
+    env = dict(os.environ, **join_info)
+    command = "cd /tmp && curl -s https://raw.githubusercontent.com/AI-Ocean/kubernetes-install-scripts/main/install-worker.sh | bash"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+
+    # print progress
+    while True:
+        output = process.stdout.readline()
+        if output == b'' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip().decode())
+
+    print(process.stderr.readline().decode())
+
