e51d548718ec70f84177fcae87c1f3eddff1b5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:51 +0000

    Add package google-vision-wrapper/0.0.2/google_vision_wrapper-0.0.2-py3-none-any.whl

---
+++ packages/google-vision-wrapper/google_vision_wrapper-0.0.2-py3-none-any.whl/gvision/__init__.py
@@ -0,0 +1,8 @@
+# author:	Giulio Cornelio Grossi, Ph.D.
+# website:	https://gcgrossi.github.io/
+
+# set the version number
+__version__ = "0.0.1"
+
+# import the necessary packages
+from .gvision import GVisionAPI
