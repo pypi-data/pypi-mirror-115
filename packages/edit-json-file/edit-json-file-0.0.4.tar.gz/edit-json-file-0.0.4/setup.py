9761aa11aae795072da5d5f705f660f678a51492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:47 +0000

    Add package edit-json-file/0.0.4/edit-json-file-0.0.4.tar.gz

---
+++ packages/edit-json-file/edit-json-file-0.0.4.tar.gz/edit-json-file-0.0.4/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="edit-json-file",
+    version="0.0.4",
+    author="Hexye",
+    author_email="dragonsale22@gmail.com",
+    description="An api wrapper to edit json files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HexyeDEV/edit-json-file",
+    download_url="https://github.com/HexyeDEV/edit-json-file/releases",
+    project_urls={
+        "Bug Tracker": "https://github.com/HexyeDEV/edit-json-file/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
