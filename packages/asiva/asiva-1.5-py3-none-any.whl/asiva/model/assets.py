9bdb5123176e6f932d3f15143f99eede8f4a8eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:59 +0000

    Add package asiva/1.5/asiva-1.5-py3-none-any.whl

---
+++ packages/asiva/asiva-1.5-py3-none-any.whl/asiva/model/assets.py
@@ -0,0 +1,16 @@
+import os
+import joblib
+
+def asiva_model():
+    path = os.path.dirname(os.path.realpath(__file__))
+    clf = joblib.load(f"{path}/asiva_model.joblib")
+    return clf
+    
+def custom_model():
+    path = os.path.dirname(os.path.realpath(__file__))
+    clf = joblib.load(f"{path}/custom_model.joblib")
+    return clf
+
+def asiva_cm():
+    path = os.path.dirname(os.path.realpath(__file__))
+    return open(f"{path}/asiva_cm.png", "rb").read()
