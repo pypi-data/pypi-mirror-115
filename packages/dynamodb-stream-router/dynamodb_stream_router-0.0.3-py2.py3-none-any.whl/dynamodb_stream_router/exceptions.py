45888830c3bb72b2119d09c0c395ace6ba5d8ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:30 +0000

    Add package dynamodb-stream-router/0.0.3/dynamodb_stream_router-0.0.3-py2.py3-none-any.whl

---
+++ packages/dynamodb-stream-router/dynamodb_stream_router-0.0.3-py2.py3-none-any.whl/dynamodb_stream_router/exceptions.py
@@ -0,0 +1,16 @@
+
+
+class SyntaxError(Exception):
+    pass
+
+
+class KeywordError(Exception):
+    pass
+
+
+class ConditionError(Exception):
+    pass
+
+
+class MultipleRouteMatches(Exception):
+    pass
