37b953e6c75a0505796367c9951b6c099226cb32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:52 +0000

    Add package gamere/2.1.1/gamere-2.1.1.tar.gz

---
+++ packages/gamere/gamere-2.1.1.tar.gz/gamere-2.1.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='gamere',
+    version='2.1.1',
+    author='stripe-python',
+    author_email='13513519246@139.com',
+    py_modules=['gamere'],
+    description="""
+Gamere is a third-party library of python games based on pygame.
+It's suitable for writing grid games.
+Please use python 3.8 or above.
+You need to install pygame, cv2(opencv-python), PIL(pillow), matplotlib.
+You can go to the source folder and use "pip install -r requirement.txt" command to install all third party libraries.
+Version 2.1.1
+"""
+)
