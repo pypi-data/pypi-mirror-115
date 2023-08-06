95bed965921f7bc23cec1e8fd2ca59621c0c421a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:45 +0000

    Add package chassisml/0.0.2/chassisml-0.0.2.tar.gz

---
+++ packages/chassisml/chassisml-0.0.2.tar.gz/chassisml-0.0.2/chassisml/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Chassis Python API Client."""
+
+name = 'chassisml'
+__version__ = '0.0.2'
+
+from .chassisml import publish, get_job_status, download_tar
