6d68c01453d0f36f0389c30a84c2269443ab089e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:51 +0000

    Add package rtsf-web/1.3.0/rtsf-web-1.3.0.tar.gz

---
+++ packages/rtsf-web/rtsf-web-1.3.0.tar.gz/rtsf-web-1.3.0/webuidriver/__init__.py
@@ -0,0 +1,6 @@
+from .firefox.webdriver import WebDriver as Firefox  # noqa
+from .chrome.webdriver import WebDriver as Chrome  # noqa
+from .ie.webdriver import WebDriver as Ie  # noqa
+from .edge.webdriver import WebDriver as Edge  # noqa
+from .opera.webdriver import WebDriver as Opera  # noqa
+from .safari.webdriver import WebDriver as Safari  # noqa
---
+++ packages/rtsf-web/rtsf-web-1.3.0.tar.gz/rtsf-web-1.3.0/webuidriver/edge/webdriver.py
@@ -0,0 +1,23 @@
+#! python3
+# -*- encoding: utf-8 -*-
+
+from webuidriver.remote.until_fiind import UntilFind
+from selenium.webdriver.edge.webdriver import WebDriver as EdgeWebDriver
+
+
+class WebDriver(EdgeWebDriver):
+    def __init__(self, *args, **kwargs):
+        EdgeWebDriver.__init__(self, *args, **kwargs)
+        self._until_find = UntilFind(self)
+
+    @property
+    def until_find(self):
+        """
+        :Returns:
+            - UntilFind: an object containing all options for dynamically waiting and finding elements.
+
+        :Usage:
+            element = driver.until_find.element_by_id('#username')
+            element.send_keys("admin")
+        """
+        return self._until_find
