f1ef87fdf5945fe1b98952ed114674b7a0059a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:04 +0000

    Add package seven-cloudapp-frame/1.0.0.28/seven_cloudapp_frame-1.0.0.28.tar.gz

---
+++ packages/seven-cloudapp-frame/seven_cloudapp_frame-1.0.0.28.tar.gz/seven_cloudapp_frame-1.0.0.28/seven_cloudapp_frame/handlers/client/act.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: HuangJianYi
+@Date: 2021-08-02 14:03:12
+@LastEditTime: 2021-08-04 18:30:07
+@LastEditors: HuangJianYi
+@Description: 
+"""
+from seven_cloudapp_frame.handlers.frame_base import *
+from seven_cloudapp_frame.models.app_base_model import *
+from seven_cloudapp_frame.models.act_base_model import *
+
+
+
+class ActInfoHandler(TaoBaseHandler):
+    """
+    :description: 获取活动信息
+    """
+    @filter_check_params("act_id")
+    def get_async(self):
+        """
+        :description: 获取活动信息
+        :param act_id：活动标识
+        :return: 
+        :last_editors: HuangJianYi
+        """
+        app_id = self.get_taobao_param().source_app_id
+        act_id = int(self.get_param("act_id", 0))
+        app_base_model = AppBaseModel(context=self)
+        act_base_model = ActBaseModel(context=self)
+        app_info_dict = app_base_model.get_app_info_dict(app_id)
+        if not app_info_dict:
+            return self.response_json_error("error", "小程序不存在")
+        act_info_dict = act_base_model.get_act_info_dict(act_id)
+        if not act_info_dict:
+            return self.response_json_error("error", "活动不存在")
+
+        act_info_dict["seller_id"] = app_info_dict["seller_id"]
+        act_info_dict["store_id"] = app_info_dict["store_id"]
+        act_info_dict["store_name"] = app_info_dict["store_name"]
+        act_info_dict["store_icon"] = app_info_dict["store_icon"]
+        act_info_dict["app_icon"] = app_info_dict["app_icon"]
+        return self.response_json_success(act_info_dict)
