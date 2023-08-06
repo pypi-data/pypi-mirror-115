a8590691d7b8f258322584137fba143772556914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:20 +0000

    Add package javscraper/1.0.4/javscraper-1.0.4.tar.gz

---
+++ packages/javscraper/javscraper-1.0.4.tar.gz/javscraper-1.0.4/javscraper/__init__.py
@@ -0,0 +1,9 @@
+from .javlibrary import JAVLibrary
+from .mgstage import MGStage
+from .r18 import R18
+from .s1 import S1
+from .sod import SOD
+from .tenmusume import TenMusume
+from .ideapocket import IdeaPocket
+
+__version__ = "1.0.4"
