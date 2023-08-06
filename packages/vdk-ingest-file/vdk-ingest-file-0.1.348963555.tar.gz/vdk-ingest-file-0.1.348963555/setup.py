28948db771532c73a6d1eedaadefef218eee66ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:50 +0000

    Add package vdk-ingest-file/0.1.348963555/vdk-ingest-file-0.1.348963555.tar.gz

---
+++ packages/vdk-ingest-file/vdk-ingest-file-0.1.348963555.tar.gz/vdk-ingest-file-0.1.348963555/setup.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+__version__ = "0.1.348963555"
+
+setuptools.setup(
+    name="vdk-ingest-file",
+    version=__version__,
+    description="Versatile Data Kit SDK ingestion plugin to ingest data into a file.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    entry_points={
+        "vdk.plugin.run": ["vdk-ingest-file = taurus.vdk.ingest_file_plugin"]
+    },
+)
