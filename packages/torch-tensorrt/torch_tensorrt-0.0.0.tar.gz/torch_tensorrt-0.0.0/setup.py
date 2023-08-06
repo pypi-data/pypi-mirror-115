c29fd8ca1e0add8886854f599b737196c4e29256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package torch-tensorrt/0.0.0/torch_tensorrt-0.0.0.tar.gz

---
+++ packages/torch-tensorrt/torch_tensorrt-0.0.0.tar.gz/torch_tensorrt-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'torch_tensorrt',        
+  packages = ['torch_tensorrt'],  
+  version = '0.0.0',     
+  description = 'Placeholder for upcoming torch-tensorrt package',   
+  author = 'Naren Dasan',                  
+  author_email = 'narens@nvidia.com',      
+  url = 'https://github.com/nvidia/trtorch',
+)
