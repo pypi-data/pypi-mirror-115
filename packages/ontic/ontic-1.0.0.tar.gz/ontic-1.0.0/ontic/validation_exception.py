a8be4f2aa2bdd1367de61a19fac6c3dfa64c29a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:54 +0000

    Add package ontic/1.0.0/ontic-1.0.0.tar.gz

---
+++ packages/ontic/ontic-1.0.0.tar.gz/ontic-1.0.0/ontic/validation_exception.py
@@ -0,0 +1,24 @@
+"""An exception to signal a validation failure."""
+
+
+class ValidationException(Exception):
+    """Exception used to signal validation errors."""
+
+    def __init__(self, validation_errors=None):
+        """
+
+        :param validation_errors: A list of validation failures that are
+            triggering the *ValidationException*.
+        :type validation_errors: list<str>
+        """
+        self._validation_errors = validation_errors if validation_errors else []
+        message = str.join(' \n', validation_errors)
+        super(ValidationException, self).__init__(message)
+
+    @property
+    def validation_errors(self) -> list[str]:
+        """List of validation errors that triggered the *ValidationException*.
+
+        :return: A list of the validation errors encountered.
+        """
+        return self._validation_errors
---
+++ packages/ontic/ontic-1.0.0.tar.gz/ontic-1.0.0/setup.py
@@ -0,0 +1,34 @@
+"""The setuptools setup file."""
+from setuptools import setup
+
+# todo: raul - if the setup barfs on README.txt the use the setup.cfg file.as
+# the tag is [metadata]/n description-file = README.md
+
+with open('README.md') as file:
+    long_description = file.read()
+
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+
+setup(
+    name='ontic',
+    version=version,
+    author='Raul Gonzalez',
+    author_email='mindbender@gmail.com',
+    url='https://github.com/neoinsanity/ontic',
+    license='Apache License 2.0',
+    description='Qualities and Quantities are the stuff of Objects.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['ontic', ],
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+    ]
+)
