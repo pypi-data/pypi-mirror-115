f354854d80fe58540defeb59137ac3a2d219c03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:09 +0000

    Add package pl-minio-callback/1.0.4/pl_minio_callback-1.0.4-py3-none-any.whl

---
+++ packages/pl-minio-callback/pl_minio_callback-1.0.4-py3-none-any.whl/pl_minio_callback/minio_util.py
@@ -0,0 +1,24 @@
+from minio import Minio
+
+
+class MinioUtil(Minio):
+    def __init__(self, bucket=None, *args, **kwargs):
+        super(MinioUtil, self).__init__(*args, **kwargs)
+        self.bucket = bucket
+        self.create_bucket(self.bucket)
+
+    def create_bucket(self, bucket):
+        found = self.bucket_exists(bucket)
+        if not found:
+            self.make_bucket(bucket)
+
+    def upload_file(self, src, dst, bucket=None):
+        bucket = self.__get_bucket(bucket)
+        self.fput_object(bucket, dst, src)
+
+    def minio_utils(self, file, bucket=None):
+        bucket = self.__get_bucket(bucket)
+        self.remove_object(bucket, file)
+
+    def __get_bucket(self, bucket):
+        return bucket if bucket else self.bucket
