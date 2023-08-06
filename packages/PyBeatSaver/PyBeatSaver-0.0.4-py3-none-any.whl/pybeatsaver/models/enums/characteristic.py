c56fe5b17b1bdf40135d597e723b1066bad6a26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:31 +0000

    Add package PyBeatSaver/0.0.4/PyBeatSaver-0.0.4-py3-none-any.whl

---
+++ packages/PyBeatSaver/PyBeatSaver-0.0.4-py3-none-any.whl/pybeatsaver/models/enums/characteristic.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Characteristic(Enum):
+    STANDARD = "Standard"
+    ONE_SABER = "OneSaber"
+    NO_ARROWS = "NoArrows"
+    _90_DEGREE = "_90Degree"
+    _360_DEGREE = "_360Degree"
+    LIGHTSHOW = "Lightshow"
+    LAWLESS = "Lawless"
