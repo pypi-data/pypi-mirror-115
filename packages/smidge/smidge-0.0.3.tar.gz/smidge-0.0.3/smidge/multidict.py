436561b52abf9d31795910f513b16d653122ad11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:45 +0000

    Add package smidge/0.0.3/smidge-0.0.3.tar.gz

---
+++ packages/smidge/smidge-0.0.3.tar.gz/smidge-0.0.3/smidge/multidict.py
@@ -0,0 +1,8 @@
+import collections
+# this concept could be generalisable
+# to a dict of arbitrary iterables
+# this one uses a set
+#   doing arbitrary iterables would mean
+#   turning the get-one-ability
+#   of set.py into 
+class multidict(collections.defaultdict):
