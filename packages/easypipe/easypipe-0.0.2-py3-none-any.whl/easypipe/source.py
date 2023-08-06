4f7d8cf1ae774f0604e0c31218c1de180f3ff4ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:09 +0000

    Add package easypipe/0.0.2/easypipe-0.0.2-py3-none-any.whl

---
+++ packages/easypipe/easypipe-0.0.2-py3-none-any.whl/easypipe/source.py
@@ -0,0 +1,28 @@
+from src.utils import CV
+
+
+class Video(CV):
+    def __init__(self, video_source: int = 0) -> None:
+        super().__init__()
+        self.keys = [27, 113]
+        self.video_source = self.source(video_source)
+
+    def stream(self, flip=True):
+        while self.video_source.isOpened():
+            success, image = self.video_source.read()
+            if not success:
+                print("Ignoring empty frame.")
+                continue
+
+            image.flags.writeable = False
+            yield self.flip(image) if flip else image
+
+            if self.wait(1) & 0xFF in self.keys:
+                break
+
+        self.video_source.release()
+
+
+class Image:
+    def __init__(self) -> None:
+        pass
