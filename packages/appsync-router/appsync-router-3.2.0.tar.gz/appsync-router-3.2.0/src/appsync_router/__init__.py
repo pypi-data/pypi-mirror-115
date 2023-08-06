b85486771145502b1fc2dab819c4709b9e8b95e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package appsync-router/3.2.0/appsync-router-3.2.0.tar.gz

---
+++ packages/appsync-router/appsync-router-3.2.0.tar.gz/appsync-router-3.2.0/src/appsync_router/__init__.py
@@ -0,0 +1,11 @@
+from .router import Router
+from .types import (
+    Route,
+    NamedRoute,
+    MatchedRoute,
+    GlobbedRoute,
+    DefaultRoute,
+    Item,
+    Response
+)
+from .router import logger
