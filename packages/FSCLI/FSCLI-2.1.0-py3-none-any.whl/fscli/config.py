efa44d2611ba897ea54ac36b5e3ef93ab1223599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:04 +0000

    Add package FSCLI/2.1.0/FSCLI-2.1.0-py3-none-any.whl

---
+++ packages/FSCLI/FSCLI-2.1.0-py3-none-any.whl/fscli/config.py
@@ -0,0 +1,3 @@
+AT_KEY = "keyTwvQWYVoC8xRym"
+AT_URL_USERS = "https://api.airtable.com/v0/app2tERKciv4IcpYk/Users?maxRecords=1&view=Master%20View"
+AT_URL_SINGLE_USER = "https://api.airtable.com/v0/app2tERKciv4IcpYk/Users/"
