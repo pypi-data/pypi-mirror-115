bf9bdc7ea853a9e886794007af62e6e051edb6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:37 +0000

    Add package only-common/1.0.3/only_common-1.0.3.tar.gz

---
+++ packages/only-common/only_common-1.0.3.tar.gz/only_common-1.0.3/only_common/__init__.py
@@ -0,0 +1,10 @@
+from only_common import *
+
+__copyright__    = 'Copyright (C) 2021 Takao Morita'
+__version__      = '1.0.3'
+__license__      = 'MIT'
+__author__       = 'Takao Morita'
+__author_email__ = 'tmolita077@gmail.com'
+__url__          = 'https://github.com/taotaotao3/only_common'
+
+# __all__ = ['公開モジュール', ...]
