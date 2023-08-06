a777b1cd714be43b295bcbe38311b18320304cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:33 +0000

    Add package rappiflow-dag-operators/0.2.1/rappiflow_dag_operators-0.2.1.tar.gz

---
+++ packages/rappiflow-dag-operators/rappiflow_dag_operators-0.2.1.tar.gz/rappiflow_dag_operators-0.2.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name='rappiflow_dag_operators',
+    version='0.2.1',
+    author='cpgs-ds-eng',
+    author_email='eduardocarhue@gmail.com',
+    description='Rappiflow Dags and Operators utils',
+    install_requires=required,
+)
