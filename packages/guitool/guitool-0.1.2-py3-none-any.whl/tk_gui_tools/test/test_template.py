b58b8d5848fb2e060c342dcd37a77460e4359f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:18 +0000

    Add package guitool/0.1.2/guitool-0.1.2-py3-none-any.whl

---
+++ packages/guitool/guitool-0.1.2-py3-none-any.whl/tk_gui_tools/test/test_template.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf8 -*-
+
+import pytest
+import tkinter as tk
+
+import tk_gui_tools.template_manager as template_manager
+
+
+@pytest.fixture
+def param_tkinter():
+    # Init Tkinter
+    root = tk.Tk()
+    root.title("Python Project")
+    root.geometry("1920x1080")
+    root.attributes("-fullscreen", False)
+    root.resizable(height=False, width=False)
+    return root
+
+
+class TestTemplate:
+    def test_template_title(self, param_tkinter):
+        self.template_manager = template_manager.Template(param_tkinter, True)
+        assert self.template_manager.active_template.template_name == "Dashboard"
