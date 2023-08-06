ec6d3136b537b64bae8dbdf28ef500a4bb381ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:24 +0000

    Add package nero-dsdn/0.2/nero_dsdn-0.2.tar.gz

---
+++ packages/nero-dsdn/nero_dsdn-0.2.tar.gz/nero_dsdn-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nero_dsdn',
+      version='0.2',
+      description='Gaussian distributions',
+      packages=['nero_dsdn'],
+      author = 'NA Jackson',
+      author_email = 'njackson.gis@gmail.com',
+      zip_safe=False)
