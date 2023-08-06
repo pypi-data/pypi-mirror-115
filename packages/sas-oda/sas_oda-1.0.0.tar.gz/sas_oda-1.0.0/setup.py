e969f22ee501d574ee5b2b5d48647b2f4218aec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:40 +0000

    Add package sas-oda/1.0.0/sas_oda-1.0.0.tar.gz

---
+++ packages/sas-oda/sas_oda-1.0.0.tar.gz/sas_oda-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='sas_oda',
+version='1.0.0',
+description='',
+author='hoor0015',
+author_email='hoor0015@gmail.com',
+license='MIT',
+install_requires=['saspy>=3.7.3'],
+packages=find_packages(),
+)
