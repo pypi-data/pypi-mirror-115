1b04f8075ea1f7ca1d26e00b6c3c801cd799cd8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:35 +0000

    Add package ferriswheel/0.0.1.4/ferriswheel-0.0.1.4-py3-none-any.whl

---
+++ packages/ferriswheel/ferriswheel-0.0.1.4-py3-none-any.whl/ferriswheel/__init__.py
@@ -0,0 +1,19 @@
+import aiohttp
+import asyncio
+
+api_uri = "https://api.ferris.chat/api/v0/"
+
+async def run_client(token: str):
+    auth_headers = {"Authorization": token}
+
+    async with aiohttp.ClientSession(headers=authheaders) as session:
+        async with session.get(api_uri) as response:
+
+            print("Status:", response.status)
+            html = await response.text()
+            print("Body:", html[:15], "...")
+
+
+def run(token: str):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run_client(token))
