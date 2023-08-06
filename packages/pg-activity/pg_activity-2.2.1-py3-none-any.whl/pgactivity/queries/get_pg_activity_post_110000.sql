cd712049a685a74b9d1a507823f6d42c742d366f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package pg-activity/2.2.1/pg_activity-2.2.1-py3-none-any.whl

---
+++ packages/pg-activity/pg_activity-2.2.1-py3-none-any.whl/pgactivity/queries/get_pg_activity_post_110000.sql
@@ -0,0 +1,26 @@
+-- Get data from pg_activity since pg 11
+SELECT
+      pg_stat_activity.pid AS pid,
+      pg_stat_activity.application_name AS application_name,
+      pg_stat_activity.datname AS database,
+      CASE WHEN pg_stat_activity.client_addr IS NULL
+          THEN 'local'
+          ELSE pg_stat_activity.client_addr::TEXT
+      END AS client,
+      EXTRACT(epoch FROM (NOW() - pg_stat_activity.{duration_column})) AS duration,
+      pg_stat_activity.wait_event AS wait,
+      pg_stat_activity.usename AS user,
+      pg_stat_activity.state AS state,
+      pg_stat_activity.query AS query,
+      pg_stat_activity.backend_type = 'parallel worker' AS is_parallel_worker
+ FROM
+      pg_stat_activity
+ WHERE
+      state <> 'idle'
+  AND pid <> pg_backend_pid()
+  AND CASE WHEN %(min_duration)s = 0
+          THEN true
+          ELSE extract(epoch from now() - {duration_column}) > %(min_duration)s
+      END
+ORDER BY
+      EXTRACT(epoch FROM (NOW() - pg_stat_activity.{duration_column})) DESC;
