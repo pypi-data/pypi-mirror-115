6ff95b7fd5bb6310ce472830fe29a61ca08b6866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:04 +0000

    Add package bpptkg-querybulletin/0.2.0/bpptkg-querybulletin-0.2.0.tar.gz

---
+++ packages/bpptkg-querybulletin/bpptkg-querybulletin-0.2.0.tar.gz/bpptkg-querybulletin-0.2.0/querybulletin/settings.py
@@ -0,0 +1,18 @@
+import os
+
+USER_HOME = os.path.expanduser('~')
+BULLETIN_DIR = os.path.join(USER_HOME, '.bulletin')
+CONFIG_DIR = os.path.join(BULLETIN_DIR, 'querybulletin')
+if not os.path.isdir(CONFIG_DIR):
+    os.makedirs(CONFIG_DIR)
+
+CONFIG_PATH = os.path.join(CONFIG_DIR, 'config.json')
+
+DEFAULT_COLUMNS = [
+    'eventid',
+    'eventdate',
+    'duration',
+    'eventtype',
+]
+
+CSV_DELIMITER = '|'
