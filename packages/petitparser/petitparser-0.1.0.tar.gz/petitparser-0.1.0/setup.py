67c0a74d902ea32dc43eea35d36f4d87c97ec222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:17 +0000

    Add package petitparser/0.1.0/petitparser-0.1.0.tar.gz

---
+++ packages/petitparser/petitparser-0.1.0.tar.gz/petitparser-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='petitparser',
+    version='0.1.0',
+    description='PetitParser implemented in Python',
+    url='https://github.com/profMagija/py-petitparser',
+    author='Nikola Bebić',
+    author_email='nikola.bebic99@gmail.com',
+    license='MIT',
+    packages=['petitparser'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
