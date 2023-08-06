a8bc6069df514d90e9b061dae969ad4203140a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:22 +0000

    Add package scrcpy-client/0.2.1/scrcpy-client-0.2.1.tar.gz

---
+++ packages/scrcpy-client/scrcpy-client-0.2.1.tar.gz/scrcpy-client-0.2.1/README.md
@@ -0,0 +1,38 @@
+# Python Scrcpy Client
+<p>
+    <a href="https://pypi.org/project/scrcpy-client/" target="_blank">
+        <img src="https://img.shields.io/pypi/v/scrcpy-client" />
+    </a>
+    <a href="https://github.com/leng-yue/py-scrcpy-client/actions/workflows/ci.yml" target="_blank">
+        <img src="https://img.shields.io/github/workflow/status/leng-yue/py-scrcpy-client/CI" />
+    </a>
+    <a href="https://app.codecov.io/gh/leng-yue/py-scrcpy-client" target="_blank">
+        <img src="https://img.shields.io/codecov/c/github/leng-yue/py-scrcpy-client" />
+    </a>
+    <img src="https://img.shields.io/github/license/leng-yue/py-scrcpy-client" />
+    <a href="https://github.com/Genymobile/scrcpy/tree/v1.18" target="_blank">
+        <img src="https://img.shields.io/badge/scrcpy-v1.18-violet" />
+    </a>
+</p>
+
+This package allows you to view and control android device in realtime. 
+
+![demo gif](https://raw.githubusercontent.com/leng-yue/py-scrcpy-client/main/demo.gif)
+
+## How to use
+To begin with, you need to install this package via pip:
+```shell
+pip install scrcpy-client
+```
+Then, you can start `script/demo.py`:
+```shell
+python -m scrcpy
+// or python script/demo.py
+```
+
+## Contribution & Development
+Please check scrcpy server 1.18 source code: [Link](https://github.com/Genymobile/scrcpy/tree/v1.18/server)
+
+## Reference
+- [py-android-viewer](https://github.com/razumeiko/py-android-viewer)
+- [scrcpy](https://github.com/Genymobile/scrcpy)
---
+++ packages/scrcpy-client/scrcpy-client-0.2.1.tar.gz/scrcpy-client-0.2.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['scrcpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['adbutils>=0.11.0,<0.12.0',
+ 'av>=8.0.3,<9.0.0',
+ 'click>=8.0.1,<9.0.0',
+ 'opencv-python>=4.5.3,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['scrcpy = script.demo:main']}
+
+setup_kwargs = {
+    'name': 'scrcpy-client',
+    'version': '0.2.1',
+    'description': 'A client of scrcpy',
+    'long_description': '# Python Scrcpy Client\n<p>\n    <a href="https://pypi.org/project/scrcpy-client/" target="_blank">\n        <img src="https://img.shields.io/pypi/v/scrcpy-client" />\n    </a>\n    <a href="https://github.com/leng-yue/py-scrcpy-client/actions/workflows/ci.yml" target="_blank">\n        <img src="https://img.shields.io/github/workflow/status/leng-yue/py-scrcpy-client/CI" />\n    </a>\n    <a href="https://app.codecov.io/gh/leng-yue/py-scrcpy-client" target="_blank">\n        <img src="https://img.shields.io/codecov/c/github/leng-yue/py-scrcpy-client" />\n    </a>\n    <img src="https://img.shields.io/github/license/leng-yue/py-scrcpy-client" />\n    <a href="https://github.com/Genymobile/scrcpy/tree/v1.18" target="_blank">\n        <img src="https://img.shields.io/badge/scrcpy-v1.18-violet" />\n    </a>\n</p>\n\nThis package allows you to view and control android device in realtime. \n\n![demo gif](https://raw.githubusercontent.com/leng-yue/py-scrcpy-client/main/demo.gif)\n\n## How to use\nTo begin with, you need to install this package via pip:\n```shell\npip install scrcpy-client\n```\nThen, you can start `script/demo.py`:\n```shell\npython -m scrcpy\n// or python script/demo.py\n```\n\n## Contribution & Development\nPlease check scrcpy server 1.18 source code: [Link](https://github.com/Genymobile/scrcpy/tree/v1.18/server)\n\n## Reference\n- [py-android-viewer](https://github.com/razumeiko/py-android-viewer)\n- [scrcpy](https://github.com/Genymobile/scrcpy)\n',
+    'author': 'lengyue',
+    'author_email': 'lengyue@lengyue.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/leng-yue/py-scrcpy-client',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
