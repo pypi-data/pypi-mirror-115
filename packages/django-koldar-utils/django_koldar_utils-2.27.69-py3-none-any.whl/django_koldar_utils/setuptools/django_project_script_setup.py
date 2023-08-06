c1e94e03e650e34f90b4cd4d3dee0234c86531ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:52 +0000

    Add package django-koldar-utils/2.27.69/django_koldar_utils-2.27.69-py3-none-any.whl

---
+++ packages/django-koldar-utils/django_koldar_utils-2.27.69-py3-none-any.whl/django_koldar_utils/setuptools/django_project_script_setup.py
@@ -0,0 +1,27 @@
+from typing import List, Dict
+
+
+from django_koldar_utils.setuptools.commons import  AbstractScriptSetup
+
+
+class DjangoProjectScriptSetup(AbstractScriptSetup):
+    """
+    A package that provides an ENTIRE django project.
+
+    When installing, you can just call "console_script_name" program you have chosen
+    """
+
+    def __init__(self, author: str, author_mail: str, description: str, keywords: List[str], home_page: str,
+                 python_minimum_version: str, license_name: str, main_package: str, console_script_name: str, classifiers: List[str] = None,
+                 package_data: str = "package_data", required_dependencies: List[str] = None):
+        super().__init__(author, author_mail, description, keywords, home_page, python_minimum_version, license_name,
+                         main_package, classifiers, package_data, required_dependencies)
+        self.console_script_name = console_script_name
+
+    def get_entry_points(self) -> Dict[str, any]:
+        return {"console_scripts": [f"{self.console_script_name}={self.main_package}.manage:main"]}
+
+    def perform_setup(self, **kwargs):
+        super().perform_setup(
+            entry_points=self.get_entry_points()
+        )
