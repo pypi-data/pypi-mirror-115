c5c2c3c830e8f72c766e14ddd7ae0e16c731281e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:45 +0000

    Add package casadi-horizon/0.0.1/casadi_horizon-0.0.1.tar.gz

---
+++ packages/casadi-horizon/casadi_horizon-0.0.1.tar.gz/casadi_horizon-0.0.1/README.md
@@ -0,0 +1,8 @@
+# horizon_gui
+A framework for trajectory optimization and optimal control for robotics based on CasADi
+
+## Install
+```python setup.py install```
+
+## Documentations
+Don't forget to check the [**documentation**](https://francescoruscelli.github.io/horizon_gui/)! 
---
+++ packages/casadi-horizon/casadi_horizon-0.0.1.tar.gz/casadi_horizon-0.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name="casadi_horizon",
+    version="0.0.1",
+    author="Francesco Ruscelli",
+    author_email="francesco.ruscelli@iit.it",
+    description="Library for Trajectory Optimization based on CasADi",
+    # long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FrancescoRuscelli/horizon_gui",
+    # project_urls={
+    #     "Bug Tracker": "no",
+    # },
+    # classifiers=[
+    #     "Programming Language :: Python :: 3",
+    #     "Operating System :: Ubuntu",
+    # ],
+    # package_dir={"": "horizon"},
+    packages=['horizon', 'horizon_gui'],
+    install_requires=['casadi', 'numpy'],
+    # packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+    
+)
