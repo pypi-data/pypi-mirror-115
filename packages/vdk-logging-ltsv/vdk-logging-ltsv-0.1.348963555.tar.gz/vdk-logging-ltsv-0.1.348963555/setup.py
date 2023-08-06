0e7320f98ce197f4eae8029e9cd125a4cede7906
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package vdk-logging-ltsv/0.1.348963555/vdk-logging-ltsv-0.1.348963555.tar.gz

---
+++ packages/vdk-logging-ltsv/vdk-logging-ltsv-0.1.348963555.tar.gz/vdk-logging-ltsv-0.1.348963555/setup.py
@@ -0,0 +1,19 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+__version__ = "0.1.348963555"
+
+setuptools.setup(
+    name="vdk-logging-ltsv",
+    version=__version__,
+    description="Versatile Data Kit SDK plugin that changes logging output to LTSV format.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    entry_points={"vdk.plugin.run": ["vdk-logging-ltsv = taurus.vdk.logging_ltsv"]},
+)
