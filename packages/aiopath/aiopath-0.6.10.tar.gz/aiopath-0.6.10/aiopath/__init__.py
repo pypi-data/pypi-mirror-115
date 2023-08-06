77abbec65542e4673cf2aaa9a97ebb536a0a7769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:45 +0000

    Add package aiopath/0.6.10/aiopath-0.6.10.tar.gz

---
+++ packages/aiopath/aiopath-0.6.10.tar.gz/aiopath-0.6.10/aiopath/__init__.py
@@ -0,0 +1,6 @@
+# export class hierarchy
+from .path import AsyncPath, AsyncPurePath, AsyncWindowsPath, \
+  AsyncPosixPath, AsyncPurePosixPath, AsyncPureWindowsPath
+
+# export iterable async file handle
+from .handle import IterableAIOFile
