81513b74f49fe351a87e5c727d5209a2ca9ff116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:38 +0000

    Add package dbcpy/1.0.0/dbcpy-1.0.0.tar.gz

---
+++ packages/dbcpy/dbcpy-1.0.0.tar.gz/dbcpy-1.0.0/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='dbcpy',
+    version='1.0.0',
+    description="simple python library for reading WoW's DBC files",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='jacadzaca',
+    author_email='vitouejj@gmail.com',
+    url='https://github.com/jacadzaca/dbcpy',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'venv'))
+)
