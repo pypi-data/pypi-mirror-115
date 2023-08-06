9af0a02aff53f40afb86cf0abdd3005559af8a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:18 +0000

    Add package PyKill/1.0.1/PyKill-1.0.1.tar.gz

---
+++ packages/PyKill/PyKill-1.0.1.tar.gz/PyKill-1.0.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+""" setup.py: install pykill v 0.1.6.3
+
+Copyright (C) 2016  Dale V. Patterson (wraith.wireless@yandex.com)
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+Redistribution and use in source and binary forms, with or without modifications,
+are permitted provided that the following conditions are met:
+ o Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+ o Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+ o Neither the name of the orginal author Dale V. Patterson nor the names of any
+   contributors may be used to endorse or promote products derived from this
+   software without specific prior written permission.
+
+sudo pip install pykill
+"""
+
+from setuptools import setup, find_packages
+
+
+setup(name='PyKill',
+      version='1.0.1',
+      description="Python Wireless Library",
+      author='Kast Stephan',
+      license='GPLv3',
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'Topic :: Security',
+                   'Topic :: Software Development',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Security',
+                   'Topic :: System :: Networking',
+                   'Topic :: Utilities',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.0',
+                   'Programming Language :: Python :: 3.7'],
+    keywords='Linux Python nl80211 iw iwconfig ifconfig wireless WLAN WiFi pentest',
+    packages=find_packages(),
+    package_data={'pykill':['nlhelp/*.help','utils/data/*.txt']}
+)
---
+++ packages/PyKill/PyKill-1.0.1.tar.gz/PyKill-1.0.1/tests/test_wireless.py
@@ -0,0 +1,62 @@
+from unittest.mock import patch, mock_open
+from pykill.pyw import interfaces, isinterface
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+enp12s0: 1263265923  910548    0   12    0     0          0       190 28743630  303148    0    0    0     0       0          0
+    lo:       0       0    0    0    0     0          0         0        0       0    0    0    0     0       0          0
+"""
+    ),
+    spec=True,
+)
+def test_interfaces_two_interfaces():
+    """Test the interfaces function when two interfaces are available."""
+    assert interfaces() == ["enp12s0", "lo"]
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+"""
+    ),
+    spec=True,
+)
+def test_interfaces_no_interface():
+    """Test interfaces function when no interfaces are available."""
+    assert interfaces() == []
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+"""
+    ),
+    spec=True,
+)
+def test_isinterface_no_interface():
+    """Test isinterfaces function when device is not an interface."""
+    assert not isinterface("SomeDevice")
+
+
+@patch(
+    "builtins.open",
+    mock_open(
+        read_data="""Inter-|   Receive                                                |  Transmit
+ face |bytes    packets errs drop fifo frame compressed multicast|bytes    packets errs drop fifo colls carrier compressed
+valid_interface: 1263265923  910548    0   12    0     0          0       190 28743630  303148    0    0    0     0       0          0
+    lo:       0       0    0    0    0     0          0         0        0       0    0    0    0     0       0          0
+"""
+    ),
+    spec=True,
+)
+def test_isinterface_is_interface():
+    """Test isinterfaces function when device is an interface."""
+    assert isinterface("valid_interface")
