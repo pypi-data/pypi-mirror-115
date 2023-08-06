7729c4315da4fc55162fc5344a7fff524b7f0c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:45 +0000

    Add package tst2021-distribution/1.0/tst2021_distribution-1.0.tar.gz

---
+++ packages/tst2021-distribution/tst2021_distribution-1.0.tar.gz/tst2021_distribution-1.0/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="tst2021_distribution",
+    version="1.0",
+    description="Gaussian and Binomial distribution",
+    packages=['tst2021_distribution'],
+    zip_safe=False
+)
