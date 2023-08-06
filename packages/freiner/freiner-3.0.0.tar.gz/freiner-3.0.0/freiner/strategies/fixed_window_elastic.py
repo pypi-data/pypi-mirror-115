eb20aa70e5b8d69cabc88a312750a85c27e53434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:22 +0000

    Add package freiner/3.0.0/freiner-3.0.0.tar.gz

---
+++ packages/freiner/freiner-3.0.0.tar.gz/freiner-3.0.0/freiner/strategies/fixed_window_elastic.py
@@ -0,0 +1,31 @@
+from typing import Any
+
+from freiner.limits import RateLimitItem
+
+from .fixed_window import FixedWindowRateLimiter
+
+
+class FixedWindowElasticExpiryRateLimiter(FixedWindowRateLimiter):
+    """
+    Reference: :ref:`fixed-window-elastic`
+    """
+
+    def hit(self, item: RateLimitItem, *identifiers: Any) -> bool:
+        """
+        Creates a hit on the rate limit and returns ``True`` if successful.
+
+        :param item: A :class:`freiner.limits.RateLimitItem` instance.
+        :param identifiers: variable list of stringable objects to uniquely identify the limit
+        :rtype: bool
+        :return: ``True`` if the request was successful, or ``False`` if the rate limit had been exceeded.
+        """
+
+        counter = self.storage.incr(
+            item.key_for(*identifiers), item.get_expiry(), elastic_expiry=True
+        )
+        return counter <= item.amount
+
+
+__all__ = [
+    "FixedWindowElasticExpiryRateLimiter",
+]
