0f49ce8fb25155a0a25cec998367c15c77e0fce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package musamusa-fal/0.1.5/musamusa_fal-0.1.5.tar.gz

---
+++ packages/musamusa-fal/musamusa_fal-0.1.5.tar.gz/musamusa_fal-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['musamusa_fal']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['iaswn>=0.0.7,<0.0.8', 'rich>=10.2.0,<11.0.0']
+
+setup_kwargs = {
+    'name': 'musamusa-fal',
+    'version': '0.1.5',
+    'description': 'Use this package to store a filename and a line number.',
+    'long_description': None,
+    'author': 'suizokukan',
+    'author_email': 'suizokukan@orange.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
