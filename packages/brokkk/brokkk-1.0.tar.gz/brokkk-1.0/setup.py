70dfab85eb1059f4082c677c8baac2197ef989cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:20 +0000

    Add package brokkk/1.0/brokkk-1.0.tar.gz

---
+++ packages/brokkk/brokkk-1.0.tar.gz/brokkk-1.0/setup.py
@@ -0,0 +1,10 @@
+
+from setuptools import  setup,find_packages
+
+setup(name='brokkk',
+      version='1.0',
+      description='description',
+      author='M.Mortaz,M.Moalaghi',
+      packages=find_packages(),
+     )
+     
