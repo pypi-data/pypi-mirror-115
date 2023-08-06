47a81879e3838d8017e4ecaed879de47a1e1d6b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:31 +0000

    Add package mlpro/0.0.0/mlpro-0.0.0-py3-none-any.whl

---
+++ packages/mlpro/mlpro-0.0.0-py3-none-any.whl/mlpro/helloworld.py
@@ -0,0 +1,24 @@
+## -------------------------------------------------------------------------------------------------
+## -- Project : MLPro
+## -- Package : mlpro
+## -- Module  : helloworld.py
+## -------------------------------------------------------------------------------------------------
+## -- History :
+## -- yyyy-mm-dd  Ver.      Auth.    Description
+## -- 2021-08-07  0.0.0     DA       Creation
+## -------------------------------------------------------------------------------------------------
+
+"""
+Ver. 0.0.0 (2021-08-07)
+
+Description coming soon...
+"""
+
+
+class HelloWorld:
+    def __init__(self) -> None:
+        print('\n\nWelcome to MLPro! More details coming soon...\n\n')
+
+
+if (__name__ == '__main__'): 
+    HelloWorld()
