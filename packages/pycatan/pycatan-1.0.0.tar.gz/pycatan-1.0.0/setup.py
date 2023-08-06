1715f0a70a59d182f65705987567f56cc203c783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:53 +0000

    Add package pycatan/1.0.0/pycatan-1.0.0.tar.gz

---
+++ packages/pycatan/pycatan-1.0.0.tar.gz/pycatan-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['pycatan', 'pycatan.board']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colored>=1.4.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pycatan',
+    'version': '1.0.0',
+    'description': 'A library for running games of The Settlers of Catan',
+    'long_description': None,
+    'author': 'Josef Waller',
+    'author_email': 'josef@siriusapplications.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
