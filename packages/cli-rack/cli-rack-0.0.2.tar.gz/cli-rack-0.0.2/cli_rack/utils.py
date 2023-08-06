723fd9dad085164bde2093de5db84983e5bd798e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:03 +0000

    Add package cli-rack/0.0.2/cli-rack-0.0.2.tar.gz

---
+++ packages/cli-rack/cli-rack-0.0.2.tar.gz/cli-rack-0.0.2/cli_rack/utils.py
@@ -0,0 +1,59 @@
+#    CLI Rack - Lightweight set of tools for building pretty-looking CLI applications in Python
+#    Copyright (C) 2021 Dmitry Berezovsky
+#    The MIT License (MIT)
+#
+#    Permission is hereby granted, free of charge, to any person obtaining
+#    a copy of this software and associated documentation files
+#    (the "Software"), to deal in the Software without restriction,
+#    including without limitation the rights to use, copy, modify, merge,
+#    publish, distribute, sublicense, and/or sell copies of the Software,
+#    and to permit persons to whom the Software is furnished to do so,
+#    subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be
+#    included in all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+#    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import argparse
+from typing import Sequence, Union, Dict, AbstractSet, TypeVar, Iterable
+
+
+def any_of_keys_exists(keys: Sequence[str], _dict: Union[Dict, Sequence, AbstractSet]) -> bool:
+    for k in keys:
+        if k in _dict:
+            return True
+    return False
+
+
+def parse_bool_val(v: str) -> bool:
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("on", "yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("off", "no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
+AnyScalarType = TypeVar("AnyScalarType", str, int, float, dict)
+
+
+def scalar_to_list(obj: Union[Iterable[AnyScalarType], AnyScalarType]) -> Iterable[AnyScalarType]:
+    """
+    Expects either a sequence of objects or just object.
+    In case of scalar value returns this value wrapped into list
+    If iterable is given - just returns it as it is.
+    :param obj:
+    :return:
+    """
+    if isinstance(obj, Iterable) and not isinstance(obj, str):
+        return obj
+    return [obj]  # type: ignore
