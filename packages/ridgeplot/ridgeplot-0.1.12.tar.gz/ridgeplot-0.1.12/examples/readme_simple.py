ba28bdcc830973d0d21dce350ae0578e9ec5d07c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:22 +0000

    Add package ridgeplot/0.1.12/ridgeplot-0.1.12.tar.gz

---
+++ packages/ridgeplot/ridgeplot-0.1.12.tar.gz/ridgeplot-0.1.12/examples/readme_simple.py
@@ -0,0 +1,15 @@
+from numpy.random import normal
+
+from ridgeplot import ridgeplot
+
+# Put your real samples here...
+synthetic_samples = [normal(n / 1.2, size=600) for n in reversed(range(9))]
+
+# The `ridgeplot()` helper comes packed with sensible defaults
+fig = ridgeplot(samples=synthetic_samples)
+
+# and the returned Plotly figure is still fully customizable
+fig.update_layout(height=500, width=800)
+
+# show us the work!!
+fig.show()
