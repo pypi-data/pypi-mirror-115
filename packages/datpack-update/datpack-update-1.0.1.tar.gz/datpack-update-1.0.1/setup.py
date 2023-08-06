e46b75c60491cf4085e0436f6a572cda5bf5c017
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:24 +0000

    Add package datpack-update/1.0.1/datpack-update-1.0.1.tar.gz

---
+++ packages/datpack-update/datpack-update-1.0.1.tar.gz/datpack-update-1.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['datpack_update']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['datpack-update = datpack_update.datpack_update:main']}
+
+setup_kwargs = {
+    'name': 'datpack-update',
+    'version': '1.0.1',
+    'description': 'Update No-Intro DAT files',
+    'long_description': '[![PyPI](https://img.shields.io/pypi/v/datpack-update)](https://pypi.org/project/datpack-update/)\n\n# datpack-update\nUpdate No-Intro DAT files\n\n## Description\nThis script updates your current No-Intro dat files with new versions that you download. The benefit over doing this manually is that this program automatically finds which dat files you have in your existing folder, and only updates those dat files. This is helpful if you download the Daily datfile pack from [DAT-o-MATIC](https://datomatic.no-intro.org/).\n\nYour dat files **must** be named exactly as DAT-o-MATIC names them. This program relies on the specific timestamp format being used. Some examples names are:\n\n```\nAtari - 2600 (20200514-091155).dat\nBandai - WonderSwan (20210609-224257).dat\nSega - Master System - Mark III (20210527-151920).dat\n```\n\n## Usage\n```\nusage: datpack-update.py [-h] src dest\n\nUpdate No-Intro DAT files\n\npositional arguments:\n  src         Source directory of new DAT files\n  dest        Destination directory of updated DAT files\n\noptional arguments:\n  -h, --help  show this help message and exit\n```\n',
+    'author': 'Andrew Simmons',
+    'author_email': 'agsimmons0@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/agsimmons/datpack-update',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/datpack-update/datpack-update-1.0.1.tar.gz/datpack-update-1.0.1/src/datpack_update/datpack_update.py
@@ -0,0 +1,91 @@
+import argparse
+from datetime import datetime
+from pathlib import Path
+import shutil
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Update No-Intro DAT files")
+    parser.add_argument("src", type=Path, help="Source directory of new DAT files")
+    parser.add_argument(
+        "dest", type=Path, help="Destination directory of updated DAT files"
+    )
+
+    return parser.parse_args()
+
+
+def split_date_from_filename(file_name):
+    """Returns a tuple of (system, date)"""
+
+    split_parts = file_name.split()
+    system = " ".join(split_parts[:-1])
+    date = datetime.strptime(split_parts[-1][:-4], r"(%Y%m%d-%H%M%S)")
+
+    return (system, date)
+
+
+def parse_dat_files_in_dir(dat_path):
+    dat_files = dat_path.glob("*.dat")
+
+    dat_structure = {}
+    for dat_file in dat_files:
+        system, date = split_date_from_filename(dat_file.name)
+
+        dat_structure[system] = {"timestamp": date, "path": dat_file}
+
+    return dat_structure
+
+
+def determine_transformations(source_files, dest_files):
+    transformations = []
+
+    for system, file_data in source_files.items():
+        # Check if the system is present in dest_files
+        if system in dest_files:
+            dest_file = dest_files[system]
+
+            # Check if the source file is newer than the dest file
+            if file_data["timestamp"] > dest_file["timestamp"]:
+                transformations.append((file_data["path"], dest_file["path"]))
+
+    return transformations
+
+
+def apply_transformations(transformations):
+    for src, dst in transformations:
+        print(f"{src.name} -> {dst.name}\n")
+
+        dest_dir = dst.parent
+
+        print(f"Deleting {dst.name}...")
+        dst.unlink()
+
+        print(f"Copying {src.name} to {dest_dir}...")
+        shutil.copy(src, dest_dir)
+
+
+def run(source_dir, dest_dir):
+    source_files = parse_dat_files_in_dir(source_dir)
+    dest_files = parse_dat_files_in_dir(dest_dir)
+
+    transformations = determine_transformations(source_files, dest_files)
+
+    for src, dst in transformations:
+        print(f"{src.name} -> {dst.name}")
+
+    if len(transformations) > 0:
+        response = None
+        while response not in ["y", "n"]:
+            response = input("Transform? (y/n): ").lower()
+
+        if response == "y":
+            apply_transformations(transformations)
+
+
+def main():
+    args = parse_args()
+    run(args.source_dir, args.dest_dir)
+
+
+if __name__ == "__main__":
+    main()
