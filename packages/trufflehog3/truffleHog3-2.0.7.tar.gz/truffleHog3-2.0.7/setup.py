b564c5f10b6bb72023a7a8b01ed274eb3211f26b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:22 +0000

    Add package trufflehog3/2.0.7/truffleHog3-2.0.7.tar.gz

---
+++ packages/trufflehog3/truffleHog3-2.0.7.tar.gz/truffleHog3-2.0.7/setup.py
@@ -0,0 +1,27 @@
+"""Distribution settings."""
+
+import pathlib
+
+from setuptools import setup
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+REQUIREMENTS = (HERE / "requirements.txt").read_text().splitlines()
+
+setup(
+    name="truffleHog3",
+    version="2.0.7",
+    packages=["truffleHog3", "truffleHog3.lib"],
+    license="GNU",
+    description="Find secrets in your codebase.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/feeltheajf/truffleHog3",
+    author="Ilya Radostev",
+    author_email="feeltheajf@gmail.com",
+    include_package_data=True,
+    python_requires=">=3.6",
+    install_requires=REQUIREMENTS,
+    entry_points={"console_scripts": ["trufflehog3 = truffleHog3.cli:run"]},
+)
