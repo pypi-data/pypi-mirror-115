caf2da21fba046d3e4fc56730b3b54b1c4104b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:14 +0000

    Add package pymirror/0.3.1/pymirror-0.3.1-py2.py3-none-any.whl

---
+++ packages/pymirror/pymirror-0.3.1-py2.py3-none-any.whl/pymirror/__init__.py
@@ -0,0 +1,8 @@
+"""pymirror"""
+
+__version__ = '0.3.1'
+__description__ = 'Upload files to multiple free hosting servers'
+
+from .config import *
+from .pymirror import PyMirror
+from .cli import *
