0d5ddaa8f98a17b14ff5f8d61225e4eb757e34cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:35 +0000

    Add package say2hello/0.5/say2hello-0.5-py3.8.egg

---
+++ packages/say2hello/say2hello-0.5-py3.8.egg/say2hello/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/7 17:06
+# @Author  : JiangWenKe
+# @Site    : 
+# @File    : say2hello.py
+# @Software: PyCharm
+def say():
+    print("say2hello pip...")
