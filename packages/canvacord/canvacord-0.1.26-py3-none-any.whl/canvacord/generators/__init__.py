7635468e97639442fe28942d94d93c303696db1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:32 +0000

    Add package canvacord/0.1.26/canvacord-0.1.26-py3-none-any.whl

---
+++ packages/canvacord/canvacord-0.1.26-py3-none-any.whl/canvacord/generators/__init__.py
@@ -0,0 +1,7 @@
+from .rankcard import rankcard
+from .trigger import trigger
+from .communism import communism
+from .gay import gay
+from .jail import jail
+from .hitler import hitler
+from .spank import spank
