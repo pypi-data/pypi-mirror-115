8e7afabdeaec1fbbe0e08c357f66ee6f24817d57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:51 +0000

    Add package isee/0.1.17/isee-0.1.17.tar.gz

---
+++ packages/isee/isee-0.1.17.tar.gz/isee-0.1.17/isee/pip_utils.py
@@ -0,0 +1,26 @@
+import configparser
+import pip
+
+from isee.common import get_env_var, get_file_path
+
+
+def install_requires(project_dir=None):
+    if not project_dir:
+        project_dir = get_env_var('GITHUB_WORKSPACE')
+    path = get_file_path('setup.cfg', project_dir)
+    config = configparser.ConfigParser()
+    config.read(path)
+    pkgs = [x for x in config['options']['install_requires'].split('\n') if x]
+    pip.main(['install'] + pkgs)
+
+
+def build_wheel(repository_dir, wheelhouse):
+    args = [
+        'wheel',
+        '--wheel-dir',
+        wheelhouse,
+        '--find-links',
+        wheelhouse,
+        repository_dir,
+    ]
+    pip.main(args)
