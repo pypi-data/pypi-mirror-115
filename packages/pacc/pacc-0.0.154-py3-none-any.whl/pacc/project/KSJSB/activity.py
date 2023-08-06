da10dee6089a8acfbfea10db5da3229376045d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:49 +0000

    Add package pacc/0.0.154/pacc-0.0.154-py3-none-any.whl

---
+++ packages/pacc/pacc-0.0.154-py3-none-any.whl/pacc/project/KSJSB/activity.py
@@ -0,0 +1,9 @@
+# 返回
+HomeActivity = 'com.kuaishou.nebula/com.yxcorp.gifshow.HomeActivity'  # 主界面
+PhotoDetailActivity = 'com.kuaishou.nebula/com.yxcorp.gifshow.detail.PhotoDetailActivity'  # 直播
+MiniAppActivity0 = 'com.kuaishou.nebula/com.mini.app.activity.MiniAppActivity0'  # 小程序
+TopicDetailActivity = 'com.kuaishou.nebula/com.yxcorp.plugin.tag.topic.TopicDetailActivity'  # 每日书单
+UserProfileActivity = 'com.kuaishou.nebula/com.yxcorp.gifshow.profile.activity.UserProfileActivity'  # 用户主页
+AdYodaActivity = 'com.kuaishou.nebula/com.yxcorp.gifshow.ad.webview.AdYodaActivity'  # 广告
+# 重启
+KRT1Activity = 'com.kuaishou.nebula/com.kwai.frog.game.engine.adapter.engine.base.KRT1Activity'  # 拯救小金鱼游戏
---
+++ packages/pacc/pacc-0.0.154-py3-none-any.whl/pacc/project/__init__.py
@@ -0,0 +1,14 @@
+from .KSJSB import KSJSB
+from .hzdyx import HZDYX
+from .dyfd import DYFD
+from .iq import IQ
+from .sd import SD
+
+
+__all__ = [
+    "KSJSB",
+    'HZDYX',
+    'DYFD',
+    'IQ',
+    'SD'
+]
