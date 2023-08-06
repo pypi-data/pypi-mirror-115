aacd98a4e3751e2015791d01806865fd6b65186f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:41 +0000

    Add package openfabric-pysdk/0.1.3/openfabric-pysdk-0.1.3.tar.gz

---
+++ packages/openfabric-pysdk/openfabric-pysdk-0.1.3.tar.gz/openfabric-pysdk-0.1.3/openfabric_pysdk/starter.py
@@ -0,0 +1,23 @@
+import pathlib
+
+from flask import Flask, app, render_template
+
+from openfabric_pysdk.manifest import manifest
+from openfabric_pysdk.service import OpenfabricService
+
+app = Flask(__name__, template_folder=f"{pathlib.Path(__file__).parent}/templates")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", manifest=manifest.all())
+
+
+class OpenfabricStarter:
+
+    @staticmethod
+    def ignite(debug, host):
+        service = OpenfabricService(app)
+        service.install_rest('/app')
+        service.install_socket('/app')
+        service.run(debug=debug, host=host)
