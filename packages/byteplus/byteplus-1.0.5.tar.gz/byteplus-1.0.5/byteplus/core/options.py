e630fe27196e6f069423d53c5fc6e29722c2f225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:52 +0000

    Add package byteplus/1.0.5/byteplus-1.0.5.tar.gz

---
+++ packages/byteplus/byteplus-1.0.5.tar.gz/byteplus-1.0.5/byteplus/core/options.py
@@ -0,0 +1,12 @@
+import datetime
+from typing import Optional
+
+
+class _Options(object):
+    def __init__(self):
+        self.timeout: Optional[datetime.timedelta] = None
+        self.request_id: Optional[str] = None
+        self.headers: Optional[dict] = None
+        self.data_date: Optional[datetime] = None
+        self.date_end: Optional[bool] = None
+        self.server_timeout: Optional[datetime.timedelta] = None
