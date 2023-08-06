6d2196daf26eb3a25dfc52122b41a48729ebd2f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:58 +0000

    Add package Jun-distributions/1.1/Jun_distributions-1.1.tar.gz

---
+++ packages/Jun-distributions/Jun_distributions-1.1.tar.gz/Jun_distributions-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Jun_distributions',
+      version='1.1',
+      description='Gaussian and binomial distributions',
+      packages=['Jun_distributions'],
+      author = 'Jun Meng',
+      author_email = 'jjunmeng@gmail.com',
+      zip_safe=False)
