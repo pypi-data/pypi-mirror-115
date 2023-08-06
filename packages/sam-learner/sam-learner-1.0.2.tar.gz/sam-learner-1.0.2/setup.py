5720728c939d9a3039bed977cab20230b4529c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:35 +0000

    Add package sam-learner/1.0.2/sam-learner-1.0.2.tar.gz

---
+++ packages/sam-learner/sam-learner-1.0.2.tar.gz/sam-learner-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='sam-learner',
+      version='1.0.2',
+      python_requires=">=3.8",
+      description='Safe Action Model Learner',
+      author='Argaman Mordoch',
+      packages=['sam_learner'],
+     )
