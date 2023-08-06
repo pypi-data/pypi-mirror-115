f93f9a88bd1451b15c91c4ba98bbeeb52a282e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:03 +0000

    Add package KevinSR/0.1.16/KevinSR-0.1.16.tar.gz

---
+++ packages/KevinSR/KevinSR-0.1.16.tar.gz/KevinSR-0.1.16/KevinSR/SOUP.py
@@ -0,0 +1,69 @@
+import os
+import numpy as np
+from scipy import ndimage, interpolate
+from scipy.ndimage import zoom
+#import time
+#import random
+#import math
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Conv3DTranspose, concatenate
+import tensorflow.keras.backend as K
+#from processing import *
+
+
+def SOUP_GAN(thicks_ori, Z_FAC):
+
+    thicks_ori = rescale_img(thicks_ori, max_val= 10000)
+
+    thins = zoom(thicks_ori, (1,1,Z_FAC))
+    
+    filedir = os.path.dirname(os.path.abspath('__file__'))    
+
+    new_model=keras.models.load_model(filedir + '/Thin-to-thin')
+    thins_gen = thins.copy()
+
+    target = np.moveaxis(thins,-1,0)
+    target = target [...,np.newaxis]
+    target = target [np.newaxis,...]
+
+    index  = attention_coeff(target, Z_FAC)
+
+    target = new_model.predict([target,index])
+     
+    target_small = target[0,...,0]
+    thins_gen = np.moveaxis(target_small, 0,-1)
+
+    return thins_gen
+     
+def attention_coeff(target, Z):
+    index  = np.zeros((target.shape[0],5))
+    if Z < 2:
+        index[:,0] =1
+    elif Z < 3:
+        t = Z-2
+        index[:,0] = 1-t
+        index[:,1] = t
+    elif Z < 4:
+        t = Z-3
+        index[:,1] = 1-t
+        index[:,2] = t
+    elif Z < 5:
+        t = Z-4
+        index[:,2] = 1-t
+        index[:,3] = t
+    elif Z < 6:
+        t = Z-5
+        index[:,3] = 1-t
+        index[:,4] = t
+    else:
+        index[:,4] = 1
+    return index
+
+MAX_VAL = 10000
+
+def rescale_img(image, max_val=MAX_VAL):
+    image = image - np.min(image)
+    image = (np.maximum(image, 0) / image.max()) * max_val
+    return (image)
+
