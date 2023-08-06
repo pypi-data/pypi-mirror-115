0fbd7300bb5af24c246117f22f2d8a115e295aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:25 +0000

    Add package crownstone-cloud/1.4.6/crownstone-cloud-1.4.6.tar.gz

---
+++ packages/crownstone-cloud/crownstone-cloud-1.4.6.tar.gz/crownstone-cloud-1.4.6/crownstone_cloud/helpers/conversion.py
@@ -0,0 +1,19 @@
+"""Contains functions that convert a parameter into something else."""
+import hashlib
+import json
+from typing import Any
+from urllib.parse import quote
+
+
+def password_to_hash(password: str) -> str:
+    """Generate a sha1 password from string."""
+    if password is None:
+        return None
+    pw_hash = hashlib.sha1(password.encode("utf-8"))
+    return pw_hash.hexdigest()
+
+
+def quote_json(_json: dict[str, Any]) -> str:
+    """Convert JSON to HTML quote."""
+    stringified = json.dumps(_json)
+    return quote(stringified)
