67a4aa32e33c6c99c46f17b2f1b864c0a0855b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:33 +0000

    Add package patchymcpatchface/0.1.11/patchymcpatchface-0.1.11-py3-none-any.whl

---
+++ packages/patchymcpatchface/patchymcpatchface-0.1.11-py3-none-any.whl/patchymcpatchface/__init__.py
@@ -0,0 +1,18 @@
+"""patchymcpatchface inner guts"""
+import sys
+import os
+import logging
+from src.patchymcpatchface.hooks import invoke_patch_hooks
+from src.patchymcpatchface.apply_patch import patch_apply
+
+__all__ = [invoke_patch_hooks, patch_apply]
+
+sys.path.append(os.getcwd())
+logger = logging.getLogger(__name__)
+
+try:
+    from patch_manifest import PATCH_MODULES
+
+    invoke_patch_hooks(PATCH_MODULES)
+except ModuleNotFoundError:
+    pass
