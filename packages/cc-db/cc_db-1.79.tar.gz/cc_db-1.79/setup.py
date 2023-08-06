7592e4241fd0a1dfa628bf84f148adaa1581b3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:10 +0000

    Add package cc-db/1.79/cc_db-1.79.tar.gz

---
+++ packages/cc-db/cc_db-1.79.tar.gz/cc_db-1.79/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='cc_db',
+      version='1.79',
+      description='CargoCast database interface',
+      packages=find_packages(),
+      author_email='pascal.dietirch@cargocast.com',
+      zip_safe=False)
