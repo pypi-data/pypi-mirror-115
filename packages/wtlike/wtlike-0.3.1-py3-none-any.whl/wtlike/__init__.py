6b0077ca9f0924042daa003bc279c59707b89ef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:03 +0000

    Add package wtlike/0.3.1/wtlike-0.3.1-py3-none-any.whl

---
+++ packages/wtlike/wtlike-0.3.1-py3-none-any.whl/wtlike/__init__.py
@@ -0,0 +1,15 @@
+"""
+# wtlike--make FermiLAT gamma-ray Light curves
+
+see https://tburnett.github.io/wtlike/tutorial
+"""
+
+__version__ = "0.3.1"
+
+
+from .config import Config, UTC, MJD
+from .data_man import check_data
+from .sources import PointSource
+from .loglike import PoissonRep
+from .simulation import Simulation
+from .main import WtLike
