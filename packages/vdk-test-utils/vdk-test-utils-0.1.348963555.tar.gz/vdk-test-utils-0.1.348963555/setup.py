e9e9aeb7f6dc3214d9988eee75be352bbb261465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package vdk-test-utils/0.1.348963555/vdk-test-utils-0.1.348963555.tar.gz

---
+++ packages/vdk-test-utils/vdk-test-utils-0.1.348963555.tar.gz/vdk-test-utils-0.1.348963555/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+
+__version__ = "0.1.348963555"
+
+
+setuptools.setup(
+    name="vdk-test-utils",
+    version=__version__,
+    description="Provides utilities for testing Versatile Data Kit SDK plugins.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+)
