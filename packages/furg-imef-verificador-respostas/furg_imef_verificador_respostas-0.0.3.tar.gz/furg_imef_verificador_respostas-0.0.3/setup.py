ce0523ed1efd3827c326d1f14b190365042051c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:56 +0000

    Add package furg-imef-verificador-respostas/0.0.3/furg_imef_verificador_respostas-0.0.3.tar.gz

---
+++ packages/furg-imef-verificador-respostas/furg_imef_verificador_respostas-0.0.3.tar.gz/furg_imef_verificador_respostas-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="furg_imef_verificador_respostas",
+    version='0.0.3',
+    description="Verificador de respostas numéricas para problemas de laboratórios do IMEF.",
+    long_description=long_description,
+    url="https://github.com/LaboratorioIPythonFURG/verificador_respostas",
+    author='FURG - IMEF',
+    license='Unlicense',
+    install_requires=['numpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent"
+    ],
+    packages=['furg_imef_verificador_respostas'],
+    python_requires=">=3.6"
+)
