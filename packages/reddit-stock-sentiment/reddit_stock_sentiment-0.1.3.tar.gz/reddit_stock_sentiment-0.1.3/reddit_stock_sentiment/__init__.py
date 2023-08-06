0e40b491be349dde44f2d6c413b8def3f93ef660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:20 +0000

    Add package reddit-stock-sentiment/0.1.3/reddit_stock_sentiment-0.1.3.tar.gz

---
+++ packages/reddit-stock-sentiment/reddit_stock_sentiment-0.1.3.tar.gz/reddit_stock_sentiment-0.1.3/reddit_stock_sentiment/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Reddit Stock Sentiment."""
+
+__author__ = """Paul Armstrong"""
+__email__ = 'paul.armstrong211@gmail.com'
+__version__ = '0.1.3'
+
+from . import reddit_stock_sentiment
