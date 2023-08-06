fa0e23c96e4a38b929c6dde67101fd0a668202c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:26 +0000

    Add package make_pdf/0.1.0.post1/make_pdf-0.1.0.post1-py3-none-any.whl

---
+++ packages/make_pdf/make_pdf-0.1.0.post1-py3-none-any.whl/make_pdf/__init__.py
@@ -0,0 +1,7 @@
+from .base import cli
+from .commands import letter
+from .commands import newsletter
+from .commands import plain
+from .commands import presentation
+
+__all__ = ["cli", "plain", "letter", "newsletter", "presentation"]
