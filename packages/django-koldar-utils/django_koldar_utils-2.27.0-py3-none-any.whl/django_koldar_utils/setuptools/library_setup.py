6add27137640aa34c091ebb345ad56639a8dd077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:25 +0000

    Add package django-koldar-utils/2.27.0/django_koldar_utils-2.27.0-py3-none-any.whl

---
+++ packages/django-koldar-utils/django_koldar_utils-2.27.0-py3-none-any.whl/django_koldar_utils/setuptools/library_setup.py
@@ -0,0 +1,8 @@
+from django_koldar_utils.setuptools.commons import AbstractScriptSetup
+
+
+class LibraryScriptSetup(AbstractScriptSetup):
+    """
+    A package that is simply a library tha tcan be used in another projects
+    """
+    pass
