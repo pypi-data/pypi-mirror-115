8c407964db073657906a9dd073a2b555a45e73f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:50 +0000

    Add package optic-django-middleware/0.1.0/optic-django-middleware-0.1.0.tar.gz

---
+++ packages/optic-django-middleware/optic-django-middleware-0.1.0.tar.gz/optic-django-middleware-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['optic_django_middleware']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=3.2.4,<4.0.0',
+ 'kubi-ecs-logger>=0.1.0,<0.2.0',
+ 'optic-sdk>=0.1.0,<0.2.0']
+
+setup_kwargs = {
+    'name': 'optic-django-middleware',
+    'version': '0.1.0',
+    'description': 'Django Middleware for optic',
+    'long_description': None,
+    'author': 'Mukesh',
+    'author_email': 'mmukesh95@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
