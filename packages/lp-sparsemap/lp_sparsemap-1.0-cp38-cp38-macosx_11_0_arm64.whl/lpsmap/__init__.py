4d88d7b6845690e839ed2ff59c9bdfbde956d1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:02 +0000

    Add package lp-sparsemap/1.0/lp_sparsemap-1.0-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/lp-sparsemap/lp_sparsemap-1.0-cp38-cp38-macosx_11_0_arm64.whl/lpsmap/__init__.py
@@ -0,0 +1,11 @@
+from .version import __version__
+from .api.factors import (Xor, Or, AtMostOne, Imply, XorOut, OrOut,
+                         AndOut, Budget, Pair)
+from .api.factors_extension import Sequence, DepTree
+from .api.api import FactorGraph
+
+try:
+    from .api.autograd import TorchFactorGraph
+except ImportError:
+    pass
+
