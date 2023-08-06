7f26290be4d6c1557d3b5e88b995c5c280a46db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:01 +0000

    Add package zanshinsdk/0.3.0/zanshinsdk-0.3.0.tar.gz

---
+++ packages/zanshinsdk/zanshinsdk-0.3.0.tar.gz/zanshinsdk-0.3.0/zanshinsdk/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.3.0"
+
+# set up package logger
+import logging
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# import Connection class into namespace, but handle failure during setup if
+# dependencies are not present yet
+try:
+    from zanshinsdk.client import Client, AlertState, AlertSeverity
+except ImportError:
+    pass
