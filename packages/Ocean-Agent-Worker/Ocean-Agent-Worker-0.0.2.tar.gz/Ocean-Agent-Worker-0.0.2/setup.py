8c8ee5439ca3992f720b26d13463a2002cd92bce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:16 +0000

    Add package Ocean-Agent-Worker/0.0.2/Ocean-Agent-Worker-0.0.2.tar.gz

---
+++ packages/Ocean-Agent-Worker/Ocean-Agent-Worker-0.0.2.tar.gz/Ocean-Agent-Worker-0.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+from os import environ
+
+setup_requires = [
+]
+
+install_requires = [
+    'websockets',
+    'click',
+    'requests'
+]
+
+dependency_links = [
+]
+
+setup(
+    name='Ocean-Agent-Worker',
+    version=environ["AGENT_VERSION"],
+    description='Ocean-Agent for Worker nodes',
+    author='kairos03',
+    author_email='kairos9603@gmail.com',
+    packages=find_packages(),
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    dependency_links=dependency_links,
+    scripts=[],
+    entry_points={
+        'console_scripts': [
+            'agent-worker = worker.main:main'
+            ],
+        },
+    )
