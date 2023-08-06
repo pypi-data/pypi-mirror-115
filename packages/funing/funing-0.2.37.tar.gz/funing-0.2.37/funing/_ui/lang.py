456f01c506d2cb8be3d175ff3d1d2fb0bd462938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:04 +0000

    Add package funing/0.2.37/funing-0.2.37.tar.gz

---
+++ packages/funing/funing-0.2.37.tar.gz/funing-0.2.37/funing/_ui/lang.py
@@ -0,0 +1,36 @@
+
+import os
+import sys
+from langcodes import Language
+import gettext
+import locale
+from funing import settings
+import yaml
+
+import os
+from funing import settings
+
+f_mo_path  = os.path.join(settings.locale_path, settings.locale_langcodes[0] ,\
+'LC_MESSAGES', 'funing.mo' )
+if not os.path.exists( f_mo_path ):
+    from funing._ui import Enjoy
+    Enjoy().msgfmt()
+    
+sys_lang_code = locale.getdefaultlocale()[0]
+
+if settings.lang_code == 'en_US' and \
+    sys_lang_code != settings.lang_code and \
+    sys_lang_code in settings.locale_langcodes:
+    settings.config_yml['lang_code'] = settings.lang_code = sys_lang_code
+    yaml.dump( settings.config_yml, open( settings.config_path, 'w') )
+
+
+if settings.debug:
+    print( settings.lang_code, settings.locale_path)
+
+lang = gettext.translation(
+    'funing',
+    localedir = settings.locale_path,
+    languages = [ settings.lang_code.replace('-','_') ])
+lang.install()
+_ = lang.gettext
