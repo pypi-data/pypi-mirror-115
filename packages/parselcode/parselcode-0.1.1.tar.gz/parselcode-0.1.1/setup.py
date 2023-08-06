c3fd3d523788da14fa910bba9d6064438818b8c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:46 +0000

    Add package parselcode/0.1.1/parselcode-0.1.1.tar.gz

---
+++ packages/parselcode/parselcode-0.1.1.tar.gz/parselcode-0.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+
+setup(
+    name="parselcode",
+    version="0.1.1",
+    description="Substitution based Cipher to Encrypt/Decrypt by mapping word structure to hindu birth chart houses",
+    url="https://github.com/Puru-Malhotra/parselcode",
+    author="Puru Malhotra",
+    author_email="purumalhotra99@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["parselcode"],
+    include_package_data=True,
+    install_requires=["pandas"],
+)
+
