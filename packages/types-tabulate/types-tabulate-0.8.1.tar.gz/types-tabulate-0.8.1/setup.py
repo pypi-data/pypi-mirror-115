8f175309320af9f56f2b648c38c60d472690bc8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:14 +0000

    Add package types-tabulate/0.8.1/types-tabulate-0.8.1.tar.gz

---
+++ packages/types-tabulate/types-tabulate-0.8.1.tar.gz/types-tabulate-0.8.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-tabulate"
+description = "Typing stubs for tabulate"
+long_description = '''
+## Typing stubs for tabulate
+
+This is a PEP 561 type stub package for the `tabulate` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `tabulate`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/tabulate. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `ef5b4b6820ba7638d4749e9642a1d4bc2bc95707`.
+'''.lstrip()
+
+setup(name=name,
+      version="0.8.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['tabulate-stubs'],
+      package_data={'tabulate-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
