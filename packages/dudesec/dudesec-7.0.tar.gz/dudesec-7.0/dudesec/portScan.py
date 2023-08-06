de8f8dffc32584c14b4e9b67055fdc871fe950a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:14 +0000

    Add package dudesec/7.0/dudesec-7.0.tar.gz

---
+++ packages/dudesec/dudesec-7.0.tar.gz/dudesec-7.0/dudesec/portScan.py
@@ -0,0 +1,14 @@
+## Importando librarys
+
+import socket
+import os
+import random
+from urllib.request import urlopen
+from ping3 import ping, verbose_ping
+from bs4 import BeautifulSoup
+from itertools import permutations
+import requests 
+import base64
+from random import choice
+import string
+
