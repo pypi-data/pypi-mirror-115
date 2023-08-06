9d01162e904d5131c57bbc5cf0ef9ea4e8093f17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:50 +0000

    Add package MyDateTimeLib/0.1.1/MyDateTimeLib-0.1.1.tar.gz

---
+++ packages/MyDateTimeLib/MyDateTimeLib-0.1.1.tar.gz/MyDateTimeLib-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='MyDateTimeLib',
+    packages=find_packages(include=['MyDateTimeLib']),
+    version='0.1.1',
+    description='Date time from string library',
+    author='Praveen Kumar Srivas',
+    license='MIT',
+    install_requires=['datefinder'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==5.3.1'],
+    test_suite='tests',
+)
