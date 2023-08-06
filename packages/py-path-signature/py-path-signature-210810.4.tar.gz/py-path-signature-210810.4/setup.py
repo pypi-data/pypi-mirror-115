6ae4feb63aed3fd9409408b6ace3a4f647712d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:48 +0000

    Add package py-path-signature/210810.4/py-path-signature-210810.4.tar.gz

---
+++ packages/py-path-signature/py-path-signature-210810.4.tar.gz/py-path-signature-210810.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['py_path_signature']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'py-path-signature',
+    'version': '210810.4',
+    'description': '',
+    'long_description': None,
+    'author': 'TBD',
+    'author_email': 'tbd@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
