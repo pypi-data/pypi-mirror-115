dbff92d44ddc4b16051351616fa8e41729338a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package intasend-python/1.0.1/intasend_python-1.0.1-py3-none-any.whl

---
+++ packages/intasend-python/intasend_python-1.0.1-py3-none-any.whl/intasend/transfers.py
@@ -0,0 +1,42 @@
+from .client import APIBase
+from .utils import sign_message
+
+
+class Transfer(APIBase):
+    def send_money(self, device_id, provider, currency, transactions, callback_url=None):
+        payload = {
+            "device_id": device_id,
+            "provider": provider,
+            "currency": currency,
+            "transactions": transactions,
+            "callback_url": callback_url
+        }
+        return self.send_request("POST", "send-money/initiate/", payload)
+
+    def approve(self, payload):
+        nonce = payload["nonce"]
+        signed_nonce = sign_message(self.private_key.encode("utf-8"), nonce)
+        payload["nonce"] = signed_nonce
+        return self.send_request("POST", "send-money/approve/", payload)
+
+    def status(self, tracking_id):
+        payload = {
+            "tracking_id": tracking_id
+        }
+        return self.send_request("POST", "send-money/status/", payload)
+
+    def mpesa(self,  device_id, currency, transactions, callback_url=None):
+        provider = "MPESA-B2C"
+        return self.send_money(device_id, provider, currency, transactions, callback_url)
+
+    def mpesa_b2b(self,  device_id, currency, transactions, callback_url=None):
+        provider = "MPESA-B2B"
+        return self.send_money(device_id, provider, currency, transactions, callback_url)
+
+    def intasend(self, device_id, currency, transactions, callback_url=None):
+        provider = "INTASEND"
+        return self.send_money(device_id, provider, currency, transactions, callback_url)
+
+    def bank(self, device_id, currency, transactions, callback_url=None):
+        provider = "PESALINK"
+        return self.send_money(device_id, provider, currency, transactions, callback_url)
