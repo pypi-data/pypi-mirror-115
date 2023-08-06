5ca0b0f0d51841ac46b88a2c94905b62e077bb9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:48 +0000

    Add package omnilearn/0.5.14/omnilearn-0.5.14.tar.gz

---
+++ packages/omnilearn/omnilearn-0.5.14.tar.gz/omnilearn-0.5.14/omnilearn/op/__init__.py
@@ -0,0 +1,20 @@
+
+from .datasets import MNIST
+
+from .model import load_model
+# from .data import load_data
+# from .data import load_data, Dataset
+from .evaluation import evaluate
+from .training import iterative_training
+from .report import get_report
+from .runs import Run, get_save_dir, Testable, Inline
+from .loading import Torch_Run
+from . import extensions
+from .report import get_report
+from .analysis import Run_Manager
+from . import records
+from . import download
+try:
+	from .pretrained import Pretrained
+except ImportError:
+	pass
