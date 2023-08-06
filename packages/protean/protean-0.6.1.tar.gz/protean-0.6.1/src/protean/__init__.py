e400df03c68b01c89bb84735a965ac8e7d06d0b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:40 +0000

    Add package protean/0.6.1/protean-0.6.1.tar.gz

---
+++ packages/protean/protean-0.6.1.tar.gz/protean-0.6.1/src/protean/__init__.py
@@ -0,0 +1,9 @@
+"""Primary Module to define version and expose packages"""
+
+__version__ = "0.6.1"
+
+from protean.utils import get_version
+
+from .domain import Domain
+
+__all__ = ("Domain", "get_version")
