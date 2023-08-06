363cdac0fc5c15cf27f68abf7f06f8a445203483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:25 +0000

    Add package javscraper/1.0.3/javscraper-1.0.3.tar.gz

---
+++ packages/javscraper/javscraper-1.0.3.tar.gz/javscraper-1.0.3/README.md
@@ -0,0 +1,36 @@
+# javscraper
+Scraper library for JAVs.
+
+## Supported Sites
+- R18
+- JAVLibrary
+- MGStage
+- S1S1S1
+- SOD Prime
+- 10Musume
+
+## Requirements
+- python3.4+
+- geckodriver
+
+## Installation
+Install geckodriver or your custom driver of choice from
+your package manager or add it to your PATH then install
+the library.
+```commandline
+python -m pip install -U javscraper
+```
+
+## Usage
+```python
+>>> from javscraper import *
+>>> javlibrary = JAVLibrary()
+
+# Searching for videos
+>>> print(javlibrary.search("SSIS-001"))
+[...]
+
+# Getting video data
+>>> print(javlibrary.get_video("SSIS-001"))
+JAVResult(name=..., code=..., studio=..., ...)
+```
---
+++ packages/javscraper/javscraper-1.0.3.tar.gz/javscraper-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+# Requirements
+requirements = [
+    "requests>=2.26.0",
+    "beautifulsoup4>=4.9.3",
+    "lxml>=4.6.3",
+    "cloudscraper>=1.2.58",
+    "selenium>=3.141.0"
+]
+
+
+setup(
+    name="javscraper",
+    version="1.0.3",
+    description="Python library used to help scrape JAV sites.",
+    url="https://github.com/hibikidesu/javscraper",
+    author="Hibiki",
+    license_files=("COPYING",),
+    packages=["javscraper"],
+    python_requires=">=3.4",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities"
+    ]
+)
