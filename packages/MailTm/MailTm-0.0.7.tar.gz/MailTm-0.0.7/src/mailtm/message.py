8e23897dd6374f4278874ef413e13a7c911f2cb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:59 +0000

    Add package MailTm/0.0.7/MailTm-0.0.7.tar.gz

---
+++ packages/MailTm/MailTm-0.0.7.tar.gz/MailTm-0.0.7/src/mailtm/message.py
@@ -0,0 +1,53 @@
+import json
+import time
+import requests
+from threading import Thread
+
+class Listen:
+    listen = False
+    message_ids = []
+
+    def message_list(self):
+        url = "https://api.mail.tm/messages"
+        headers = { 'Authorization': 'Bearer ' + self.token }
+        response = requests.request("GET", url, headers=headers)
+        
+        try:
+            data = json.loads(response.text)
+            return  [
+                        msg for i, msg in enumerate(data['hydra:member']) 
+                            if data['hydra:member'][i]['id'] not in self.message_ids
+                    ]
+        except Exception as e:
+            return []
+
+    def message(self, idx):
+        url = "https://api.mail.tm/messages/" + idx
+        headers = { 'Authorization': 'Bearer ' + self.token }
+        response = requests.request("GET", url, headers=headers)
+        return json.loads(response.text)
+
+    def run(self):
+        while self.listen:
+            for message in self.message_list():
+                self.message_ids.append(message['id'])
+                message = self.message(message['id'])
+                self.listener(message)
+
+            time.sleep(self.interval)
+
+    def start(self, listener, interval=3):
+        if self.listen:
+            self.stop()
+
+        self.listener = listener
+        self.interval = interval
+        self.listen = True
+
+        # Start listening thread
+        self.thread = Thread(target=self.run)
+        self.thread.start()
+    
+    def stop(self):
+        self.listen = False
+        self.thread.join()
