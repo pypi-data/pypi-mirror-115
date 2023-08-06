93c409ff11e94bcdb9029f38dc0e6592b18b17d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:09 +0000

    Add package PyTangtv/0.2.4/PyTangtv-0.2.4.tar.gz

---
+++ packages/PyTangtv/PyTangtv-0.2.4.tar.gz/PyTangtv-0.2.4/setup.py
@@ -0,0 +1,34 @@
+
+
+import sys
+import setuptools
+from distutils.core import setup
+from glob import glob
+
+
+install_requires = [
+     'requests',
+]
+if sys.version_info[0] == 2:
+    install_requires.append('tk')
+    install_requires.append('pathlib2')
+elif sys.version_info[0] == 3:
+    install_requires.append('tk')
+    install_requires.append('pathlib')
+
+setup(
+    name='PyTangtv',
+    version='0.2.4',
+    packages=['pytangtv','pytangtv.pymorph','pytangtv.pyalign','pytangtv.pymask','pytangtv.picker'],
+    maintainer='Bill Meyer',
+    maintainer_email='meyer8@llnl.gov',
+    description="LLNL DIII-D diagnostic image tools",
+    package_data={ "pytangtv":["bitmaps/*.xbm","bitmaps/*.png"],},
+    scripts=['pytangtv/pyalign/pyalign','pytangtv/pymorph/pymorph','pytangtv/picker/picker','pytangtv/pymask/pymask'],
+    license='LLNL',
+    long_description=open('README.txt').read(),
+    classifiers = ['Programming Language :: Python',
+                   'Programming Language :: Python :: 3'],
+    install_requires = install_requires,
+)
+
