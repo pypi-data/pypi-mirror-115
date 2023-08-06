dcfd70b9ae336499bdf7f0ed4a919b01c543ff7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:35 +0000

    Add package youtil/1.0.2/youtil-1.0.2.tar.gz

---
+++ packages/youtil/youtil-1.0.2.tar.gz/youtil-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(name='youtil', # 패키지 명
+
+version='1.0.2',
+
+description='Module to get youtube details',
+
+author='smartwe',
+
+author_email='sparklingwee@naver.com',
+
+url='https://github.com/smartwe/youtil',
+
+license='MIT', # MIT에서 정한 표준 라이센스 따른다
+
+py_modules=['main'], # 패키지에 포함되는 모듈
+
+python_requires='>=3',
+
+install_requires=['bs4'], # 패키지 사용을 위해 필요한 추가 설치 패키지
+
+packages=['youtil'], # 패키지가 들어있는 폴더들
+
+)
