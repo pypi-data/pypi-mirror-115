d770d7976718b511c77ce6342d2e371b83c8c523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:58 +0000

    Add package distributionslib/0.1/distributionslib-0.1.tar.gz

---
+++ packages/distributionslib/distributionslib-0.1.tar.gz/distributionslib-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributionslib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionslib'],
+      author='Sanjiv Gupta',
+      zip_safe=False)
