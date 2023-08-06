2b4a1ceaccf7784964619ff0f8cd4e08d7f26d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:37 +0000

    Add package smidge/0.0.2/smidge-0.0.2.tar.gz

---
+++ packages/smidge/smidge-0.0.2.tar.gz/smidge-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name='smidge',
+    version='0.0.2',
+    descrption='Small python tools',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/xloem/pysmidge',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7'
+)
