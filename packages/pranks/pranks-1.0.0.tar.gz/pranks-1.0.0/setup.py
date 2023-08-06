dbae5c2818db3582672458ee7995d1b110f7f9b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:56 +0000

    Add package pranks/1.0.0/pranks-1.0.0.tar.gz

---
+++ packages/pranks/pranks-1.0.0.tar.gz/pranks-1.0.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+ 
+setuptools.setup(
+    name="pranks",
+    version="1.0.0",
+    author="TabsOrSpace",
+    description="Pranks",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+)
