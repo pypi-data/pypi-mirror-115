9b4939e06a2f8e7ae252a4bab0b42fe0d2144ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:52 +0000

    Add package scanimate/0.1.1/scanimate-0.1.1.tar.gz

---
+++ packages/scanimate/scanimate-0.1.1.tar.gz/scanimate-0.1.1/scanimate/__init__.py
@@ -0,0 +1,11 @@
+# @Email: rah3156@rit.edu
+# @Website: https://ryanhartzell.github.io/scanimate
+# @License: https://github.com/RyanHartzell/scanimate/blob/master/LICENSE
+# @github: https://github.com/RyanHartzell/scanimate
+#
+# Copyright (c) 2020 Ryan Hartzell
+#
+from .moire import *
+from .display import *
+from .io import *
+from .version_info import *
