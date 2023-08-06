a68a08d982dc0b9869d4fe8a4b59378c75c7805d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package mlstatpy/0.3.593/mlstatpy-0.3.593.tar.gz

---
+++ packages/mlstatpy/mlstatpy-0.3.593.tar.gz/mlstatpy-0.3.593/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sys
+import os
+from setuptools import find_packages, setup
+from pyquicksetup import read_version, read_readme, default_cmdclass
+
+#########
+# settings
+#########
+
+project_var_name = "mlstatpy"
+versionPython = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
+path = "Lib/site-packages/" + project_var_name
+readme = 'README.rst'
+history = 'HISTORY.rst'
+requirements = None
+
+KEYWORDS = project_var_name + ', Xavier Dupré'
+DESCRIPTION = """Lectures about machine learning, mathematics, statistics, programming."""
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Development Status :: 5 - Production/Stable'
+]
+
+#######
+# data
+#######
+
+packages = find_packages('src', exclude='src')
+package_dir = {k: "src/" + k.replace(".", "/") for k in packages}
+package_data = {}
+
+
+setup(
+    name=project_var_name,
+    version=read_version(__file__, project_var_name, subfolder='src'),
+    author='Xavier Dupré',
+    author_email='xavier.dupre@gmail.com',
+    license="MIT",
+    url="http://www.xavierdupre.fr/app/mlstatpy/helpsphinx/index.html",
+    download_url="https://github.com/sdpython/mlstatpy/",
+    description=DESCRIPTION,
+    long_description=read_readme(__file__),
+    cmdclass=default_cmdclass(),
+    keywords=KEYWORDS,
+    classifiers=CLASSIFIERS,
+    packages=packages,
+    package_dir=package_dir,
+    package_data=package_data,
+    setup_requires=["pyquicksetup"],
+    install_requires=['numpy>=1.16', 'scipy>=1.4',
+                      'mlinsights>=0.2', 'cpyquickhelper>=0.2'],
+)
