a7541b1af20feec47aef40062eb41706086c41ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:49 +0000

    Add package SlappPy-Slate/1.3.9/SlappPy-Slate-1.3.9.tar.gz

---
+++ packages/SlappPy-Slate/SlappPy-Slate-1.3.9.tar.gz/SlappPy-Slate-1.3.9/src/slapp_py/misc/models/battlefy_player.py
@@ -0,0 +1,20 @@
+from dataclasses import dataclass
+from typing import Dict
+
+
+@dataclass
+class BattlefyPlayer:
+    in_game_name: str
+    user_slug: str
+
+    @staticmethod
+    def from_dict(obj: dict) -> 'BattlefyPlayer':
+        assert isinstance(obj, dict)
+        return BattlefyPlayer(
+            in_game_name=obj.get("inGameName", None),
+            user_slug=obj.get("userSlug", None)
+        )
+
+    def to_dict(self) -> dict:
+        result = {"inGameName": self.in_game_name, "userSlug": self.user_slug}
+        return result
