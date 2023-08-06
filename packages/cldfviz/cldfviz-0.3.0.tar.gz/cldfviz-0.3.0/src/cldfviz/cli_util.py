9829909c027ac24daa35dd059ab5fe9f309244fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:10 +0000

    Add package cldfviz/0.3.0/cldfviz-0.3.0.tar.gz

---
+++ packages/cldfviz/cldfviz-0.3.0.tar.gz/cldfviz-0.3.0/src/cldfviz/cli_util.py
@@ -0,0 +1,11 @@
+import argparse
+
+
+def add_testable(parser):
+    parser.add_argument('--test', action='store_true', default=False, help=argparse.SUPPRESS)
+
+
+def add_listvalued(parser, *args, **kw):
+    kw.setdefault('default', [])
+    kw.setdefault('type', lambda s: s.split(','))
+    parser.add_argument(*args, **kw)
