8902b1814afe994308479b9b65a023276f34aaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:05 +0000

    Add package pywapor-test/2.1.10/pywapor_test-2.1.10.tar.gz

---
+++ packages/pywapor-test/pywapor_test-2.1.10.tar.gz/pywapor_test-2.1.10/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pywapor_test',
+    version = '2.1.10',
+    url = 'https://bitbucket.org/cioapps/wapor-et-look/src/master/',
+    author = "FAO",
+    author_email = "bert.coerver@fao.org",
+    license = "Apache v2.0",
+    packages = find_packages(include = ['pywapor', 'pywapor.*']),
+    install_requires = [
+        'gdal<=3.1.4',
+        'watertools<=0.0.28',
+        'numpy',
+        'pandas',
+        'requests',
+        'matplotlib',
+        # These should be requirements for watertools, not pywapor:
+        'netcdf4',
+        'pyproj',
+        'scipy',
+        'fiona',
+        'pycurl',
+        'pyshp',
+        'joblib',
+        'bs4',
+        'paramiko',
+    ]
+)
