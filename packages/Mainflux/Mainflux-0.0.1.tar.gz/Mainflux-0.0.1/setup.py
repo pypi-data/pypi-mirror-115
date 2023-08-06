b1663933fc7925064b1e75e080e5a33928591cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:24 +0000

    Add package Mainflux/0.0.1/Mainflux-0.0.1.tar.gz

---
+++ packages/Mainflux/Mainflux-0.0.1.tar.gz/Mainflux-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Mainflux",
+    version="0.0.1",
+    author="Filip Bugarski",
+    author_email="filipbugarski@gmail.com",
+    description="Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mainflux/sdk-py",
+    project_urls={
+        "Bug Tracker": "https://github.com/mainflux/sdk-py",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"sdk-py": "lib"},
+    packages=setuptools.find_packages(where="sdk-py/lib"),
+    python_requires=">=3.6",
+)
