4240742d2204b679be26d3f8660778eec74ef176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:01 +0000

    Add package ailever/0.2.471/ailever-0.2.471-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.471-py3-none-any.whl/ailever/investment/__init__.py
@@ -0,0 +1,16 @@
+from ._fmlops_policy import local_initialization_policy
+from ._fmlops_policy import fmlops_bs as __fmlops_bs__
+local_initialization_policy()
+
+from .INVESTMENTInterfaces import initialize
+from .INVESTMENTInterfaces import parallelize
+from .INVESTMENTInterfaces import portfolio_optimize
+from .INVESTMENTInterfaces import Forecaster
+from .INVESTMENTInterfaces import sectors
+from .INVESTMENTInterfaces import Loader
+from .INVESTMENTInterfaces import Preprocessor
+from .INVESTMENTInterfaces import screener
+
+
+
+
