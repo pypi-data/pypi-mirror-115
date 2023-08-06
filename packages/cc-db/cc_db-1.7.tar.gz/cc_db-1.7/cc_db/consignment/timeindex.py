992dd2ce9035a4b50ab46e646bf30d80af3b275b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:56 +0000

    Add package cc-db/1.7/cc_db-1.7.tar.gz

---
+++ packages/cc-db/cc_db-1.7.tar.gz/cc_db-1.7/cc_db/consignment/timeindex.py
@@ -0,0 +1,17 @@
+from cc_db.engines import *
+from cc_db.session_handler import SessionHandler
+from sqlalchemy import Column, String, Boolean
+
+class TimeIndex(Base,SessionHandler):
+  __tablename__ = 'timeindex'
+  Id = Column(String, primary_key=True)
+  Name = Column(String)
+  Description = Column(String)
+
+  def create_session(self):
+    Session = sessionmaker(forecast_engine)
+    return Session()
+  
+  
+  def __repr__(self): 
+    return "<TimeIndex(Id='%s', Name='%s', Description='%s')>" %(self.Id, self.Name, self.Description)
