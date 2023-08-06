b656b426da83e8d95eb2a8879f50a0567a5c9d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:50 +0000

    Add package pfun/0.12.3/pfun-0.12.3-cp37-cp37m-macosx_10_15_x86_64.whl

---
+++ packages/pfun/pfun-0.12.3-cp37-cp37m-macosx_10_15_x86_64.whl/pfun/__init__.py
@@ -0,0 +1,14 @@
+from . import console, files, logging, ref, subprocess  # noqa
+from .dict import Dict  # noqa
+from .effect import *  # noqa
+from .either import Either, Left, Right  # noqa
+from .functions import *  # noqa
+from .immutable import Immutable  # noqa
+from .lens import *  # noqa
+from .list import List  # noqa
+from .maybe import Just, Maybe, Nothing  # noqa
+
+try:
+    from . import http, sql, hypothesis_strategies  # noqa
+except ImportError:
+    pass
