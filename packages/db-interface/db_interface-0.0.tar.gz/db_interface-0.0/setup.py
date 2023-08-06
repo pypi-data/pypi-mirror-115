beea6c9d851c855f82adf48b7687608a624e14b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:33 +0000

    Add package db-interface/0.0/db_interface-0.0.tar.gz

---
+++ packages/db-interface/db_interface-0.0.tar.gz/db_interface-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='db_interface',
+      version='0.0',
+      description='CargoCast database interface',
+      packages=find_packages(),
+      author_email='pascal.dietrich@cargocast.de',
+      zip_safe=False)
