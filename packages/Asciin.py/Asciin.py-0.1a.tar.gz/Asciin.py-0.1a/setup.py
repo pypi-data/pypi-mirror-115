409fbbe933323fa25246a447f69a412bf70a5f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:10 +0000

    Add package Asciin.py/0.1a/Asciin.py-0.1a.tar.gz

---
+++ packages/Asciin.py/Asciin.py-0.1a.tar.gz/Asciin.py-0.1a/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from re import search, MULTILINE
+
+pkg_name = "Asciinpy"
+prj_name = "Asciin.py"
+with open(f"{pkg_name}/__init__.py") as fp:
+    version = search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fp.read(), MULTILINE
+    ).group(1)
+
+packages = [pkg_name, "Asciinpy.methods"]
+setup(
+    name=prj_name,
+    version=version,
+    description="Featherweight 3D / 2D ascii game engine for Python 2.7+ with no external dependencies and written in Python and optimized in C.",
+    author="Rickaym",
+    url=f"https://github.com/Rickaym/{prj_name}",
+    project_urls={
+        "Documentation": "https://asciipy.readthedocs.io/en/latest/",
+        "Issue tracker": f"https://github.com/Rickaym/{prj_name}/issues",
+    },
+    license="MIT",
+    python_requires=">=2.7",
+    packages=packages,
+)
