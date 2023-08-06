84c04a9b6ea9d39bb5d25112b1a0b32ca8e04eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:20 +0000

    Add package wuml/0.012/wuml-0.012.tar.gz

---
+++ packages/wuml/wuml-0.012.tar.gz/wuml-0.012/wuml/type_check.py
@@ -0,0 +1,20 @@
+
+import numpy as np
+
+def ensure_numpy(data):
+	if type(data).__name__ == 'ndarray': 
+		len(data.shape) == 1
+		X = np.atleast_2d(data).T
+	elif type(data).__name__ == 'wData': 
+		X = data.df.values
+	elif type(data).__name__ == 'DataFrame': 
+		X = data.values
+	elif np.isscalar(data):
+		X = np.array([[data]])
+
+	return X
+
+
+#def correct_variable_type(variable, intended_type):
+#	if type(variable).__name__ == intended_type: return True
+#	return False
