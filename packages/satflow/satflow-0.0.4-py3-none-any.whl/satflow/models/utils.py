51aee626818799fb6c5eb217fd35d582ebf610a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:53 +0000

    Add package satflow/0.0.4/satflow-0.0.4-py3-none-any.whl

---
+++ packages/satflow/satflow-0.0.4-py3-none-any.whl/satflow/models/utils.py
@@ -0,0 +1,17 @@
+import torch
+from satflow.models.layers import CoordConv
+
+
+def get_conv_layer(conv_type: str = "standard") -> torch.nn.Module:
+    if conv_type == "standard":
+        conv_layer = torch.nn.Conv2d
+    elif conv_type == "coord":
+        conv_layer = CoordConv
+    elif conv_type == "antialiased":
+        # TODO Add anti-aliased coordconv here
+        conv_layer = torch.nn.Conv2d
+    elif conv_type == "3d":
+        conv_layer = torch.nn.Conv3d
+    else:
+        raise ValueError(f"{conv_type} is not a recognized Conv method")
+    return conv_layer
