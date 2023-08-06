325ee8ed483301ae6a6ce00880f6555e9d031c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:54 +0000

    Add package ontic/1.0.0/ontic-1.0.0-py3-none-any.whl

---
+++ packages/ontic/ontic-1.0.0-py3-none-any.whl/ontic/__init__.py
@@ -0,0 +1,17 @@
+from ontic import (core,
+                   property,
+                   schema,
+                   type)
+from ontic.property import OnticProperty
+from ontic.schema import Schema
+from ontic.type import OnticType
+
+__all__ = [
+    'core',
+    'property',
+    'schema',
+    'type',
+    'OnticProperty',
+    'Schema',
+    'OnticType',
+]
