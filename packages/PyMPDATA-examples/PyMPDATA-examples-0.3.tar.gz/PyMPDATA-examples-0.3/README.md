7356ec0e2c4f69b6ec5c9a2cf12fbf09fc78958c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:53 +0000

    Add package PyMPDATA-examples/0.3/PyMPDATA-examples-0.3.tar.gz

---
+++ packages/PyMPDATA-examples/PyMPDATA-examples-0.3.tar.gz/PyMPDATA-examples-0.3/README.md
@@ -0,0 +1,2 @@
+This repository stores example files for PyMPDATA.
+See [PyMPDATA README.md](https://github.com/atmos-cloud-sim-uj/PyMPDATA/blob/master/README.md) file for details.
---
+++ packages/PyMPDATA-examples/PyMPDATA-examples-0.3.tar.gz/PyMPDATA-examples-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMPDATA-examples',
+    version="0.3",
+    setup_requires=['setuptools_scm'],
+    install_requires=['PyMPDATA',
+                      'pystrict',
+                      'matplotlib',
+                      'ipywidgets',
+                      'scipy',
+                      'pint',
+                      'joblib',
+                      'sympy',
+                      'ghapi'],
+    author='https://github.com/orgs/atmos-cloud-sim-uj/people',
+    license="GPL-3.0",
+    packages=find_packages(include=['PyMPDATA_examples', 'PyMPDATA_examples.*']),
+    package_data={'': ['*/*/*.txt']},
+    include_package_data=True
+)
