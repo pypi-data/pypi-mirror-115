67605d7a57b5bedda122cfabbcec0b20d1e78ca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:17 +0000

    Add package techlib-nr-datasets-metadata/1.0.0a3/techlib-nr-datasets-metadata-1.0.0a3.tar.gz

---
+++ packages/techlib-nr-datasets-metadata/techlib-nr-datasets-metadata-1.0.0a3.tar.gz/techlib-nr-datasets-metadata-1.0.0a3/nr_datasets_metadata/marshmallow/json.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2019 CERN.
+#
+# My site is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
+
+"""JSON Schemas."""
+
+from oarepo_rdm_records.marshmallow.dataset import DataSetMetadataSchemaV2
+
+
+class DatasetMetadataSchemaV1(DataSetMetadataSchemaV2):
+    pass
