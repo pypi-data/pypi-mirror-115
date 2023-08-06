d162d12e643df070ee09b259cbed143b699ce09f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:47 +0000

    Add package platypy/3.1.2/platypy-3.1.2.tar.gz

---
+++ packages/platypy/platypy-3.1.2.tar.gz/platypy-3.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '3.1.2'
+DESCRIPTION = 'This platypy which have many submodules to help you.'
+LONG_DESCRIPTION = long_description
+
+# Setting up
+setup(
+    name="platypy",
+    version=VERSION,
+    author="Python Rohan",
+    author_email="rohan9878787@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'calculator', 'calci', 'calculate', 'calculation', 'Arithmetic'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
