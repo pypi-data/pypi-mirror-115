dd8d97b7d6ad5d49fc9347ff361230fc593632bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:30 +0000

    Add package mutation-load/1.0.1/mutation_load-1.0.1.tar.gz

---
+++ packages/mutation-load/mutation_load-1.0.1.tar.gz/mutation_load-1.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+        name="mutation_load",
+        version="1.0.1",
+        author="Timo_Järvinen",
+        author_email="neville160@gmail.com",
+        description="VCF permutation tool",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/jarvint12/mutation_load",
+        packages=setuptools.find_packages(),
+        classifiers=(
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: BSD License",
+            "Operating System :: POSIX :: Linux",
+        ),
+)
+
