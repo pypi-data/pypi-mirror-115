e391e19d895be27da0c8ef4a630734e4bb122fa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:14 +0000

    Add package pyargos/0.0.2/pyargos-0.0.2.tar.gz

---
+++ packages/pyargos/pyargos-0.0.2.tar.gz/pyargos-0.0.2/pyargos/plugin.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from pyargos.element import ArgosElement
+from pyargos.attributed_element import ArgosButton
+from pyargos.separator import ArgosSeparator
+
+
+class ArgosPlugin:
+    def __init__(self, *, buttons: List[ArgosButton] = None, elements: List[ArgosElement] = None) -> None:
+        super().__init__()
+
+        self.buttons: List[ArgosButton] = buttons if buttons is not None else []
+        self.lines: List[ArgosElement] = elements if elements is not None else []
+
+    def to_argos(self) -> str:
+        output = []
+
+        # Add the buttons
+        for button in self.buttons:
+            output.append(button.to_argos())
+
+        # Add the button separator
+        output.append(ArgosSeparator().to_argos())
+
+        # Add the lines
+        for line in self.lines:
+            output.append(line.to_argos())
+
+        # Return everything on separate lines
+        return '\n'.join(output)
