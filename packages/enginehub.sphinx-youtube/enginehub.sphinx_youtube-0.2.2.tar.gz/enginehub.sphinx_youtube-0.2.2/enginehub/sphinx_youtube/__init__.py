a83a3dbddc284cb4b204df77ad78ce2f53de80fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:50 +0000

    Add package enginehub.sphinx-youtube/0.2.2/enginehub.sphinx_youtube-0.2.2.tar.gz

---
+++ packages/enginehub.sphinx-youtube/enginehub.sphinx_youtube-0.2.2.tar.gz/enginehub.sphinx_youtube-0.2.2/enginehub/sphinx_youtube/__init__.py
@@ -0,0 +1,46 @@
+#-*- coding:utf-8 -*-
+u'''
+embedding youtube video to sphinx
+
+usage:
+
+First of all, add `enginehub.youtube` to sphinx extension list in conf.py
+
+.. code-block:: python
+
+   extensions = ['enginehub.sphinx_youtube']
+
+
+then use `youtube` directive.
+
+You can specify video by video url or video id.
+
+.. code-block:: rst
+
+   .. youtube:: http://www.youtube.com/watch?v=Ql9sn3aLLlI
+
+   .. youtube:: Ql9sn3aLLlI
+
+
+finally, build your sphinx project.
+
+.. code-block:: sh
+
+   $ make html
+
+'''
+
+__version__ = '0.2.2'
+__author__ = '@shomah4a'
+__license__ = 'LGPLv3'
+
+
+
+def setup(app):
+
+    from . import youtube
+
+    app.add_node(youtube.youtube,
+                 html=(youtube.visit, youtube.depart))
+    app.add_directive('youtube', youtube.YoutubeDirective)
+    return {'parallel_read_safe': True}
