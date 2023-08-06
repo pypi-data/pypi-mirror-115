547800d403a3566dc2d88740b1cd8f1073c41992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:33 +0000

    Add package potluck-eval/0.9.2/potluck_eval-0.9.2-py3-none-any.whl

---
+++ packages/potluck-eval/potluck_eval-0.9.2-py3-none-any.whl/potluck/testarea/test_course/fall2021/examples/perfect/functionsTest/functions.py
@@ -0,0 +1,47 @@
+"""
+functions.py
+
+Your name: Peter Mawhorter
+Your username: pmwh
+Submission date: 2021-6-22
+
+A few functions to practice with.
+"""
+
+
+def indentMessage(message, targetLength):
+    """
+    Returns a string that's at least the given target length. If given a
+    longer string, it returns it as-is, but if given a shorter string, it
+    adds spaces to the front of the string to make it the required
+    length.
+    """
+    indent = max(0, targetLength - len(message))
+    return ' ' * indent + message
+
+def printMessage(message, width):
+    """
+    Prints a message, taking up at least the required width (will be
+    wider if the message itself is wider). Uses indentMessage.
+    """
+    print(indentMessage(message, width))
+
+import math
+
+def ellipseArea(radius1, radius2):
+    """
+    Computes the area of an ellipse with the given radii (you may specify
+    the major and minor radii in either order). Returns the result as a
+    floating-point number.
+    """
+    return radius1 * radius2 * math.pi
+
+from turtle import *
+
+def polygon(sideLength, nSides):
+    """
+    Draws a polygon with the given side length and number of sides.
+    """
+    for _ in range(nSides):
+        fd(sideLength)
+        lt(360 / nSides)
