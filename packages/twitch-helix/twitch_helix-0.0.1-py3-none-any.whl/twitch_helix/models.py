0c1b0d30e505f2ce57173a1745e4fe13f780c23c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:04 +0000

    Add package twitch-helix/0.0.1/twitch_helix-0.0.1-py3-none-any.whl

---
+++ packages/twitch-helix/twitch_helix-0.0.1-py3-none-any.whl/twitch_helix/models.py
@@ -0,0 +1,73 @@
+from dataclasses import dataclass
+
+
+def decode(data, cls):
+    if isinstance(data, cls):
+        return data
+    kwargs = {}
+    for key, value in data.items():
+        member_type = cls.__annotations__[key]
+        member = decode(value, member_type)
+        kwargs[key] = member
+    return cls(**kwargs)
+
+
+class Model:
+    @classmethod
+    def from_json(cls, data: dict):
+        return decode(data, cls)
+
+    def to_json(self, encode_none: bool = False):
+        data = {}
+        for key, value in self.__dict__.items():
+            if isinstance(value, Model):
+                data[key] = value.to_json(encode_none)
+            elif value is None:
+                if encode_none:
+                    data[key] = None
+            # TODO handle all primitive types
+            elif isinstance(value, int) or isinstance(value, str):
+                data[key] = value
+            else:
+                raise ValueError('bad type')
+        return data
+
+
+@dataclass
+class ChannelInfo(Model):
+    broadcaster_id: str = None
+    broadcaster_name: str = None
+    game_name: str = None
+    game_id: str = None
+    broadcaster_language: str = None
+    title: str = None
+    delay: int = None
+
+
+@dataclass
+class Condition(Model):
+    pass
+
+
+@dataclass
+class BroadcasterCondition(Condition):
+    broadcaster_user_id: str
+
+
+@dataclass
+class FromToCondition(Condition):
+    from_broadcaster_user_id: str = None
+    to_broadcaster_user_id: str = None
+
+
+@dataclass
+class RewardCondition(Condition):
+    broadcaster_user_id: str
+    reward_id: str = None
+
+
+@dataclass
+class Transport(Model):
+    method: str
+    callback: str
+    secret: str
