8fd0b62e4ce84600aa162ed5191eeea67a21b0e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:02 +0000

    Add package mehdi/0.23/mehdi-0.23.tar.gz

---
+++ packages/mehdi/mehdi-0.23.tar.gz/mehdi-0.23/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+metadata = dict(
+    name="mehdi",  # Replace with your own username
+    version="0.23",
+    author="Stacrypt",
+    author_email="Stacrypt@info.com",
+    description="Stacrypt utility package",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url=None,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7')
+setup(**metadata)
+
