5b3ed4729bde54a8d91450fe2e30ff99e0e137b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:03 +0000

    Add package windowsapps/0.0.9/windowsapps-0.0.9-py3-none-any.whl

---
+++ packages/windowsapps/windowsapps-0.0.9-py3-none-any.whl/windowsapps/__init__.py
@@ -0,0 +1,29 @@
+from json import loads
+from subprocess import getoutput
+from os import startfile
+
+def get_apps():
+	cmd = 'powershell -ExecutionPolicy Bypass "Get-StartApps|convertto-json"'
+	apps=loads(getoutput(cmd))
+	names = {}
+	for each in apps:
+		names.update({each['Name']:each['AppID']})
+	return names
+
+def find_app(app_name):
+	apps = get_apps()
+	for each in sorted(apps,key=len):
+		if app_name.upper() in each.upper():
+			return each,apps[each]
+	else:
+		return "Application not found!"
+
+def open_app(app_name):
+	app = find_app(app_name)
+	if app == None:
+		raise ValueError('Application not found!')
+	else:
+		startfile('shell:AppsFolder\%s'%app[1])
+
+if __name__=='__main__':
+        find_app('stealth pc monitor')
