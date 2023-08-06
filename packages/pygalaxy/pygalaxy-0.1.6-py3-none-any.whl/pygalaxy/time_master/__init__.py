242b2071854e9bc3012eba3502a1ed42697c393c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:44 +0000

    Add package pygalaxy/0.1.6/pygalaxy-0.1.6-py3-none-any.whl

---
+++ packages/pygalaxy/pygalaxy-0.1.6-py3-none-any.whl/pygalaxy/time_master/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import datetime
+import time
+
+
+def get_current_date_str():
+    """
+    格式化获取当前的日期
+    """
+    now = datetime.datetime.now()
+    time_str = datetime.datetime.strftime(now, '%Y-%m-%d')
+    return time_str
+
+
+def get_current_time_str():
+    """
+    格式化获取当前的日期时间
+    """
+    now = datetime.datetime.now()
+    time_str = datetime.datetime.strftime(now, '%Y-%m-%d %H:%M:%S')
+    return time_str
+
+
+def get_current_unix_time():
+    """
+    获取当前10位unix时间戳
+    """
+    now = datetime.datetime.now()
+    unix_time = time.mktime(now.timetuple())
+    return int(unix_time)
+
+
+if __name__ == '__main__':
+    print(get_current_time_str())
+    print(get_current_unix_time())
