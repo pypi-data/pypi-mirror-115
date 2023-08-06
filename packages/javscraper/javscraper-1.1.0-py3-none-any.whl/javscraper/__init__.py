0e3a8073c8f27fcc62f09a47fb22ee1d1a0e7b02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:24 +0000

    Add package javscraper/1.1.0/javscraper-1.1.0-py3-none-any.whl

---
+++ packages/javscraper/javscraper-1.1.0-py3-none-any.whl/javscraper/__init__.py
@@ -0,0 +1,10 @@
+from .javlibrary import JAVLibrary
+from .mgstage import MGStage
+from .r18 import R18
+from .s1 import S1
+from .sod import SOD
+from .tenmusume import TenMusume
+from .ideapocket import IdeaPocket
+from .caribbeancom import Caribbeancom
+
+__version__ = "1.1.0"
