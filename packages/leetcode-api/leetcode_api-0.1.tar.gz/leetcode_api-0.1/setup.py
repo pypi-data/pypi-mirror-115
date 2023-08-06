b03d6b62d70d6c450972f39b80420b78cc04c046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:37 +0000

    Add package leetcode-api/0.1/leetcode_api-0.1.tar.gz

---
+++ packages/leetcode-api/leetcode_api-0.1.tar.gz/leetcode_api-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='leetcode_api',# 需要打包的名字,即本模块要发布的名字
+    version='v0.1',#版本
+    description='leetcode api', # 简要描述
+    py_modules=['leetcode_api'],   #  需要打包的模块
+    author='vansin', # 作者名
+    author_email='msnode@163.com',   # 作者邮件
+    url='https://github.com/vansin/leetcode_stat', # 项目地址,一般是代码托管的网站
+    # requires=['requests','urllib3'], # 依赖包,如果没有,可以不要
+    license='MIT'
+)
