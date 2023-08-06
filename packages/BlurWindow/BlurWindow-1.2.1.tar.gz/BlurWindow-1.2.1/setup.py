7094f62a5b11b80a8c2083f9e4b97c611c464f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:52 +0000

    Add package BlurWindow/1.2.1/BlurWindow-1.2.1.tar.gz

---
+++ packages/BlurWindow/BlurWindow-1.2.1.tar.gz/BlurWindow-1.2.1/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+import pathlib, platform
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.rst").read_text()
+
+setup(
+    name="BlurWindow",
+    version="1.2.1",
+    description="Blur PySide, Tkinter, etc windows.",
+    url="https://github.com/Peticali/PythonBlurBehind",
+    author="Peticali",
+    long_description_content_type="text/x-rst",
+    long_description=README,   
+    author_email="pedropalmeira68@gmail.com",
+    license="MIT",
+    packages=['BlurWindow']
+)
