196cfc5ff165570eb6050adbee86652dbee91903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package simplebot-howdoi/1.1.0/simplebot_howdoi-1.1.0.tar.gz

---
+++ packages/simplebot-howdoi/simplebot_howdoi-1.1.0.tar.gz/simplebot_howdoi-1.1.0/CHANGELOG.rst
@@ -0,0 +1,16 @@
+Changelog
+=========
+
+`1.1.0`_
+--------
+
+- added filter to allow search directly in private 1:1 chats.
+
+1.0.0
+-----
+
+- initial release
+
+
+.. _Unreleased: https://github.com/adbenitez/simplebot_howdoi/compare/v1.1.0...HEAD
+.. _Unreleased: https://github.com/adbenitez/simplebot_howdoi/compare/v1.0.0...v1.1.0
---
+++ packages/simplebot-howdoi/simplebot_howdoi-1.1.0.tar.gz/simplebot_howdoi-1.1.0/simplebot_howdoi.py
@@ -0,0 +1,61 @@
+"""Plugin's filters and commands definitions."""
+
+import json
+
+import simplebot
+from deltachat import Message
+from howdoi.howdoi import howdoi
+from pkg_resources import DistributionNotFound, get_distribution
+from simplebot.bot import Replies
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = "0.0.0.dev0-unknown"
+
+
+@simplebot.filter
+def query_filter(message: Message, replies: Replies) -> None:
+    """Send me a question in private to get an answer."""
+    if not message.chat.is_group():
+        _search(message.text, replies)
+
+
+@simplebot.command(name="/howdoi")
+def cmd_howdoi(payload: str, replies: Replies) -> None:
+    """Instant coding answers.
+
+    Example:
+    /howdoi format date bash
+
+    You can also send me the question direclty in private.
+    """
+    _search(payload, replies)
+
+
+def _search(query: str, replies: Replies) -> None:
+    try:
+        res = json.loads(howdoi("{} -j".format(query)))[0]
+        replies.add(text="{}\n\n↗️ {}".format(res["answer"], res["link"]))
+    except (Exception, SystemExit):  # noqa
+        replies.add(text="❌ Something went wrong.")
+
+
+class TestPlugin:
+    """Online tests"""
+
+    def test_howdoi(self, mocker):
+        msg = mocker.get_one_reply("/howdoi format date bash")
+        assert "↗" in msg.text
+
+        msg = mocker.get_one_reply("/howdoi -h")
+        assert "❌" in msg.text
+
+    def test_filter(self, mocker):
+        msg = mocker.get_one_reply("format date bash")
+        assert "↗" in msg.text
+
+        # filter should work only in private/direct chat
+        msgs = mocker.get_replies("format date bash", group="group1")
+        assert not msgs
