03c70468e4cd5cc54b2eeb7831a0c3246707e6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:51 +0000

    Add package business-python/2.0.3/business-python-2.0.3.tar.gz

---
+++ packages/business-python/business-python-2.0.3.tar.gz/business-python-2.0.3/business/__init__.py
@@ -0,0 +1,19 @@
+"""Init."""
+# mypy: ignore-errors
+
+
+def get_version() -> str:
+    """Get package version."""
+    try:
+        from importlib.metadata import version
+    except ImportError:
+        from importlib_metadata import version  # type: ignore
+    return version("business")
+
+
+# noinspection PyBroadException
+try:
+    __version__ = get_version()
+except Exception:
+    # development mode: set dev version if package is not installed
+    __version__ = "dev"
