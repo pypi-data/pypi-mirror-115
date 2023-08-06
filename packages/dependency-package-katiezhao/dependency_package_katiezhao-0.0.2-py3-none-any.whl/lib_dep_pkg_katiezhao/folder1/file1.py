712851ab7f02f9d92ff57684be10100aa61d6ca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package dependency-package-katiezhao/0.0.2/dependency_package_katiezhao-0.0.2-py3-none-any.whl

---
+++ packages/dependency-package-katiezhao/dependency_package_katiezhao-0.0.2-py3-none-any.whl/lib_dep_pkg_katiezhao/folder1/file1.py
@@ -0,0 +1,13 @@
+class katie_psw_cls_1:
+    def __init__(self):
+        import numpy
+        a = numpy.array([0,1,3,4])
+        self.val = 'xiaogege:' + str(520 + a[0]) + ' from katie_psw_cls_1'
+    def get_val(self):
+        return self.val
+
+def katie_psw_func_1():
+    import numpy as np
+    a = np.array([i for i in range(10)])
+    ret = 'xiaogege:' + str(519 + a[1]) + ' from katie_psw_func_1'
+    return ret
