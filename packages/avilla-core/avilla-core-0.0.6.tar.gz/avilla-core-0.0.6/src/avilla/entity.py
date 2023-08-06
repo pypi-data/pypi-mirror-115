fa7da1ec3a3b10293a95fc4f9d34a3510cdbac59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:19 +0000

    Add package avilla-core/0.0.6/avilla-core-0.0.6.tar.gz

---
+++ packages/avilla-core/avilla-core-0.0.6.tar.gz/avilla-core-0.0.6/src/avilla/entity.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from pydantic import BaseModel  # pylint: ignore
+
+P = TypeVar("P")
+
+
+class Entity(BaseModel, Generic[P]):
+    def __init__(self, id: str, profile: P):
+        super().__init__(id=id, profile=profile)
+
+    id: str
+    profile: P
+
+
+@dataclass
+class EntityPtr:
+    id: str
