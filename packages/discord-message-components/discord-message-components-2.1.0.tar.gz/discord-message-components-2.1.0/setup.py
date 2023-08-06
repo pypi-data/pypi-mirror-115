d13bb2f86292a53282c39bb0e76951fa2966d77e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:07 +0000

    Add package discord-message-components/2.1.0/discord-message-components-2.1.0.tar.gz

---
+++ packages/discord-message-components/discord-message-components-2.1.0.tar.gz/discord-message-components-2.1.0/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+
+def getVersion():
+    with open("./discord_message_components/__init__.py", "r", encoding="utf-8") as f:
+        return [f for f in f.readlines() if f.startswith("__version__")][0].split('"')[1].split('"')[0]
+def getReadme():
+    with open("./README.md", "r", encoding="utf-8") as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="discord-message-components",
+    version=getVersion(),
+    author="404kuso, RedstoneZockt",
+    author_email="bellou9022@gmail.com, redstoneprofihd@gmail.com",
+    description="A discord message component handler for discord.py",
+    long_description=getReadme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KusoRedsto/discord_py_buttons",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9' if 'READTHEDOCS' not in os.environ else None,
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
