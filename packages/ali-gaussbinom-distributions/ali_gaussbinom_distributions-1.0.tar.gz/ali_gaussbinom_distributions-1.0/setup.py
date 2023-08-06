966da68f75feea3c628ca8e160f39f217f35deaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:04 +0000

    Add package ali-gaussbinom-distributions/1.0/ali_gaussbinom_distributions-1.0.tar.gz

---
+++ packages/ali-gaussbinom-distributions/ali_gaussbinom_distributions-1.0.tar.gz/ali_gaussbinom_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ali_gaussbinom_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['ali_gaussbinom_distributions'],
+      author='Ali Zeynalli',
+      zip_safe=False)
