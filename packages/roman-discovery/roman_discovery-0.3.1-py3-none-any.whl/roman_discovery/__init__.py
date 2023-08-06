112389802f28712f3adb5b9f6426538b54ea37d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:27 +0000

    Add package roman-discovery/0.3.1/roman_discovery-0.3.1-py3-none-any.whl

---
+++ packages/roman-discovery/roman_discovery-0.3.1-py3-none-any.whl/roman_discovery/__init__.py
@@ -0,0 +1,15 @@
+"""Top-level package for Roman Discovery."""
+
+__author__ = """Roman Imankulov"""
+__email__ = "roman.imankulov@gmail.com"
+__version__ = "0.3.1"
+
+
+from roman_discovery.discovery import IRule, ModuleRule, ObjectRule, discover
+
+__all__ = (
+    "discover",
+    "IRule",
+    "ModuleRule",
+    "ObjectRule",
+)
