9cf78ddfa53cbf793184468c98d9cc952f1ca0f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:12 +0000

    Add package types-setuptools/57.0.1/types-setuptools-57.0.1.tar.gz

---
+++ packages/types-setuptools/types-setuptools-57.0.1.tar.gz/types-setuptools-57.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-setuptools"
+description = "Typing stubs for setuptools"
+long_description = '''
+## Typing stubs for setuptools
+
+This is a PEP 561 type stub package for the `setuptools` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `setuptools`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/setuptools. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `ef5b4b6820ba7638d4749e9642a1d4bc2bc95707`.
+'''.lstrip()
+
+setup(name=name,
+      version="57.0.1",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['pkg_resources-stubs'],
+      package_data={'pkg_resources-stubs': ['__init__.pyi', 'py31compat.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
