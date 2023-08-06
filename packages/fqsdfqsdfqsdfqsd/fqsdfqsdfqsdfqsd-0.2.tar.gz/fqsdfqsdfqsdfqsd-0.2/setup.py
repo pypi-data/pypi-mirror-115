201ab2d7ae8e3ca46cf1f774d7d152a9c1a006b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:03 +0000

    Add package fqsdfqsdfqsdfqsd/0.2/fqsdfqsdfqsdfqsd-0.2.tar.gz

---
+++ packages/fqsdfqsdfqsdfqsd/fqsdfqsdfqsdfqsd-0.2.tar.gz/fqsdfqsdfqsdfqsd-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='fqsdfqsdfqsdfqsd',
+      version='0.2',
+      description='fqsdfqsdfqsdfqsd',
+      author='fqsdfqsdfqsdfqsd',
+      author_email='fqsdfqsdfqsdfqsdn@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
