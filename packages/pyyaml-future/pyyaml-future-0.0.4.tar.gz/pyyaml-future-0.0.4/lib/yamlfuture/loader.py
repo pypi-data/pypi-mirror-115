0f9a2fe3e4314f3c9e5ee3fb5d0a98263c812467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:49 +0000

    Add package pyyaml-future/0.0.4/pyyaml-future-0.0.4.tar.gz

---
+++ packages/pyyaml-future/pyyaml-future-0.0.4.tar.gz/pyyaml-future-0.0.4/lib/yamlfuture/loader.py
@@ -0,0 +1,35 @@
+import os
+from .xxx import *
+__all__ = ['Loader']
+
+from yaml import SafeLoader, composer, representer
+
+from .stdlib import Library
+
+class Loader(SafeLoader):
+    anchors = None
+    filepath = None
+
+    def __init__(self, stream):
+        SafeLoader.__init__(self, stream)
+        self.library = Library(self)
+        self.represent = representer.Representer().represent_data
+        if Loader.anchors is not None:
+            self.set_anchors(Loader.anchors)
+        if Loader.filepath is None:
+            self.importpath = None
+        else:
+            self.importpath = os.path.abspath(
+                os.path.dirname(Loader.filepath))
+
+    def set_anchors(self, anchors):
+        self.anchors = {}
+
+        for key in anchors:
+            self.anchors[key] = self.represent(anchors[key])
+
+    def compose_document(self):
+        anchors = self.anchors
+        node = composer.Composer.compose_document(self)
+        self.anchors = anchors
+        return node
