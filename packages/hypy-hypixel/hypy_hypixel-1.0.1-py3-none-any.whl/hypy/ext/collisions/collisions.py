1871c2d533f5cc1231344936ed98d34f5f701cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:54 +0000

    Add package hypy-hypixel/1.0.1/hypy_hypixel-1.0.1-py3-none-any.whl

---
+++ packages/hypy-hypixel/hypy_hypixel-1.0.1-py3-none-any.whl/hypy/ext/collisions/collisions.py
@@ -0,0 +1,9 @@
+from ...abc import HypyIterable
+
+
+def player_collisions(party1: HypyIterable, party2: HypyIterable) -> set[str]:
+    """Returns collisions of people between 2 Objects"""
+    parties = []
+    for i in [party1, party2]:
+        parties.append([x.no_dashes for x in i.iter_uuids()])
+    return set(set(parties[0]).intersection(parties[1]))
