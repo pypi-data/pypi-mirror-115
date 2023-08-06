2cb1b7eeebbeb100cca824a9dde26158940efa28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:19 +0000

    Add package syntheticdb/0.0.15/syntheticdb-0.0.15.tar.gz

---
+++ packages/syntheticdb/syntheticdb-0.0.15.tar.gz/syntheticdb-0.0.15/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['syntheticdb']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.4.2,<4.0.0',
+ 'pandas>=1.2.1,<2.0.0',
+ 'scipy>=1',
+ 'sqlparse>=0.4.1,<0.5.0']
+
+setup_kwargs = {
+    'name': 'syntheticdb',
+    'version': '0.0.15',
+    'description': '',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
