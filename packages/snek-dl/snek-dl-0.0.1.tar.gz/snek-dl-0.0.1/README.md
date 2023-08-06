63ab9a7091923bf244fbaf7bcb1a7a905bf461cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:31 +0000

    Add package snek-dl/0.0.1/snek-dl-0.0.1.tar.gz

---
+++ packages/snek-dl/snek-dl-0.0.1.tar.gz/snek-dl-0.0.1/README.md
@@ -0,0 +1,3 @@
+# Snek
+
+An intuitive wrapper for youtube-dl, for in-line use.
---
+++ packages/snek-dl/snek-dl-0.0.1.tar.gz/snek-dl-0.0.1/snek/util.py
@@ -0,0 +1,29 @@
+from pprint import pformat
+from subprocess import Popen, PIPE
+
+
+class PrettyDict(dict):
+    def __str__(self):
+        return pformat(self, sort_dicts=False)
+
+
+class PrettyList(list):
+    def __str__(self):
+        return pformat(self, sort_dicts=False)
+
+
+class Pretty:
+    def __new__(cls, item):
+        if isinstance(item, list):
+            item = PrettyList(item)
+        elif isinstance(item, dict):
+            item = PrettyDict(item)
+        return item
+
+
+def prettify(function):
+    def wrapper(*args, **kwargs):
+        item = function(*args, **kwargs)
+        return Pretty(item)
+
+    return wrapper
