0aeaf425d56245c05decad3d1e73b306b0b08fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:08 +0000

    Add package osmium/3.2.0/osmium-3.2.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/osmium/osmium-3.2.0-cp37-cp37m-macosx_10_9_x86_64.whl/osmium/version.py
@@ -0,0 +1,15 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '3.2'
+# current release (Pip version)
+pyosmium_release = '3.2.0'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.17.0'
+# protozero version shipped with the Pip release
+protozero_version = '1.7.0'
+# pybind11 version shipped with the Pip release
+pybind11_version = '2.7.1'
