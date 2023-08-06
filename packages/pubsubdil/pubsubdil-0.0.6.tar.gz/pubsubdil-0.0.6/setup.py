9c2cf83c5049b43cd2b84326733e09dc2b950fd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:13 +0000

    Add package pubsubdil/0.0.6/pubsubdil-0.0.6.tar.gz

---
+++ packages/pubsubdil/pubsubdil-0.0.6.tar.gz/pubsubdil-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# # The directory containing this file
+# HERE = pathlib.Path(__file__).parent
+
+# # The text of the README file
+# README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="pubsubdil",
+    version="0.0.6",
+    description="Package for pubsub channel",
+    author="DeepInnovations",
+    author_email="lidor@deepinnovations.co.uk",
+    packages=find_packages(exclude=["tests"]),
+    install_requires = ['redis'],
+    py_modules = ['pubsub', 'config', '__init__'],
+    include_package_data = True
+)
