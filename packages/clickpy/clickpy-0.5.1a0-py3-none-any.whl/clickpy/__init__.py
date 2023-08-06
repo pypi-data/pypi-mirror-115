c56ca716f1f9a949b250dadda828c3f5c1347a83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:22 +0000

    Add package clickpy/0.5.1a0/clickpy-0.5.1a0-py3-none-any.whl

---
+++ packages/clickpy/clickpy-0.5.1a0-py3-none-any.whl/clickpy/__init__.py
@@ -0,0 +1,2 @@
+from .cli import run
+from .clickpy import auto_click
---
+++ packages/clickpy/clickpy-0.5.1a0-py3-none-any.whl/clickpy/__main__.py
@@ -0,0 +1,6 @@
+"""Clicker module file. This will be ran when called via 'python -m clicker'."""
+
+if __name__ == "__main__":
+    from clickpy import run
+
+    run()
