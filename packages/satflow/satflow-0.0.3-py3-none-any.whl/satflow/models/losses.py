c16267f9f03f4ac4993dac8e3f1c443ab9e93698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:19 +0000

    Add package satflow/0.0.3/satflow-0.0.3-py3-none-any.whl

---
+++ packages/satflow/satflow-0.0.3-py3-none-any.whl/satflow/models/losses.py
@@ -0,0 +1,186 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from pytorch_msssim import SSIM, MS_SSIM
+from torch.nn import functional as F
+
+
+class SSIMLoss(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.ssim_module = SSIM(**kwargs)
+
+    def forward(self, X, Y):
+        return 1.0 - self.ssim_module(X, Y)
+
+
+class MS_SSIMLoss(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.ssim_module = MS_SSIM(**kwargs)
+
+    def forward(self, X, Y):
+        return 1.0 - self.ssim_module(X, Y)
+
+
+class GridCellLoss(nn.Module):
+    """
+    Grid Cell Regularizer loss from Skillful Nowcasting, see https://arxiv.org/pdf/2104.00954.pdf
+
+    """
+
+    def __init__(self, weight_fn=None):
+        super().__init__()
+        self.weight_fn = weight_fn  # In Paper, weight_fn is max(y+1,24)
+
+    def forward(self, generated_images, targets):
+        """
+        Calculates the grid cell regularizer value, assumes generated images are the mean predictions from
+        6 calls to the generater (Monte Carlo estimation of the expectations for the latent variable)
+        Args:
+            generated_images: Mean generated images from the generator
+            targets: Ground truth future frames
+
+        Returns:
+            Grid Cell Regularizer term
+        """
+        difference = generated_images - targets
+        if self.weight_fn is not None:
+            difference *= self.weight_fn(targets)
+        difference /= targets.size(1) * targets.size(3) * targets.size(4)  # 1/HWN
+        return difference.mean()
+
+
+class NowcastingLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x, real_flag):
+        if real_flag is True:
+            x = -x
+        return F.relu(1.0 + x).mean()
+
+
+class FocalLoss(nn.Module):
+    """
+    copy from: https://github.com/Hsuxu/Loss_ToolBox-PyTorch/blob/master/FocalLoss/FocalLoss.py
+    This is a implementation of Focal Loss with smooth label cross entropy supported which is proposed in
+    'Focal Loss for Dense Object Detection. (https://arxiv.org/abs/1708.02002)'
+        Focal_Loss= -1*alpha*(1-pt)*log(pt)
+    :param num_class:
+    :param alpha: (tensor) 3D or 4D the scalar factor for this criterion
+    :param gamma: (float,double) gamma > 0 reduces the relative loss for well-classified examples (p>0.5) putting more
+                    focus on hard misclassified example
+    :param smooth: (float,double) smooth value when cross entropy
+    :param balance_index: (int) balance class index, should be specific when alpha is float
+    :param size_average: (bool, optional) By default, the losses are averaged over each loss element in the batch.
+    """
+
+    def __init__(
+        self,
+        apply_nonlin=None,
+        alpha=None,
+        gamma=2,
+        balance_index=0,
+        smooth=1e-5,
+        size_average=True,
+    ):
+        super(FocalLoss, self).__init__()
+        self.apply_nonlin = apply_nonlin
+        self.alpha = alpha
+        self.gamma = gamma
+        self.balance_index = balance_index
+        self.smooth = smooth
+        self.size_average = size_average
+
+        if self.smooth is not None:
+            if self.smooth < 0 or self.smooth > 1.0:
+                raise ValueError("smooth value should be in [0,1]")
+
+    def forward(self, logit, target):
+        if self.apply_nonlin is not None:
+            logit = self.apply_nonlin(logit)
+        num_class = logit.shape[1]
+
+        if logit.dim() > 2:
+            # N,C,d1,d2 -> N,C,m (m=d1*d2*...)
+            logit = logit.view(logit.size(0), logit.size(1), -1)
+            logit = logit.permute(0, 2, 1).contiguous()
+            logit = logit.view(-1, logit.size(-1))
+        target = torch.squeeze(target, 1)
+        target = target.view(-1, 1)
+        #
+        alpha = self.alpha
+
+        if alpha is None:
+            alpha = torch.ones(num_class, 1)
+        elif isinstance(alpha, (list, np.ndarray)):
+            assert len(alpha) == num_class
+            alpha = torch.FloatTensor(alpha).view(num_class, 1)
+            alpha = alpha / alpha.sum()
+        elif isinstance(alpha, float):
+            alpha = torch.ones(num_class, 1)
+            alpha = alpha * (1 - self.alpha)
+            alpha[self.balance_index] = self.alpha
+
+        else:
+            raise TypeError("Not support alpha type")
+
+        if alpha.device != logit.device:
+            alpha = alpha.to(logit.device)
+
+        idx = target.cpu().long()
+
+        one_hot_key = torch.FloatTensor(target.size(0), num_class).zero_()
+        one_hot_key = one_hot_key.scatter_(1, idx, 1)
+        if one_hot_key.device != logit.device:
+            one_hot_key = one_hot_key.to(logit.device)
+
+        if self.smooth:
+            one_hot_key = torch.clamp(
+                one_hot_key, self.smooth / (num_class - 1), 1.0 - self.smooth
+            )
+        pt = (one_hot_key * logit).sum(1) + self.smooth
+        logpt = pt.log()
+
+        gamma = self.gamma
+
+        alpha = alpha[idx]
+        alpha = torch.squeeze(alpha)
+        loss = -1 * alpha * torch.pow((1 - pt), gamma) * logpt
+
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+        return loss
+
+
+def get_loss(loss: str = "mse", **kwargs) -> torch.nn.Module:
+    if isinstance(loss, torch.nn.Module):
+        return loss
+    assert loss in [
+        "mse",
+        "bce",
+        "binary_crossentropy",
+        "crossentropy",
+        "focal",
+        "ssim",
+        "ms_ssim",
+        "l1",
+    ]
+    if loss == "mse":
+        criterion = F.mse_loss
+    elif loss in ["bce", "binary_crossentropy", "crossentropy"]:
+        criterion = F.nll_loss
+    elif loss in ["focal"]:
+        criterion = FocalLoss()
+    elif loss in ["ssim"]:
+        criterion = SSIMLoss(data_range=1.0, size_average=True, **kwargs)
+    elif loss in ["ms_ssim"]:
+        criterion = MS_SSIMLoss(data_range=1.0, size_average=True, **kwargs)
+    elif loss in ["l1"]:
+        criterion = torch.nn.L1Loss()
+    else:
+        raise ValueError(f"loss {loss} not recognized")
+    return criterion
---
+++ packages/satflow/satflow-0.0.3-py3-none-any.whl/satflow/models/metnet.py
@@ -0,0 +1,266 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from satflow.models.base import register_model
+from satflow.models.utils import get_conv_layer
+from satflow.models.losses import get_loss
+from satflow.models.layers import ConvGRU2, TimeDistributed, ConditionTime
+from axial_attention import AxialAttention
+from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+import numpy as np
+import torchvision
+import antialiased_cnns
+
+
+class DownSampler(nn.Module):
+    def __init__(self, in_channels, output_channels: int = 256, conv_type: str = "standard"):
+        super().__init__()
+        conv2d = get_conv_layer(conv_type=conv_type)
+        if conv_type == "antialiased":
+            antialiased = True
+        else:
+            antialiased = False
+
+        self.module = nn.Sequential(
+            conv2d(in_channels, 160, 3, padding=1),
+            nn.MaxPool2d((2, 2), stride=1 if antialiased else 2),
+            antialiased_cnns.BlurPool(160, stride=2) if antialiased else nn.Identity(),
+            nn.BatchNorm2d(160),
+            conv2d(160, output_channels, 3, padding=1),
+            nn.BatchNorm2d(output_channels),
+            conv2d(output_channels, output_channels, 3, padding=1),
+            nn.BatchNorm2d(output_channels),
+            conv2d(output_channels, output_channels, 3, padding=1),
+            nn.MaxPool2d((2, 2), stride=1 if antialiased else 2),
+            antialiased_cnns.BlurPool(output_channels, stride=2) if antialiased else nn.Identity(),
+        )
+
+    def forward(self, x):
+        return self.module.forward(x)
+
+
+class MetNetPreprocessor(nn.Module):
+    def __init__(self, sat_channels: int = 12, crop_size: int = 256, use_space2depth: bool = True):
+        """
+        Performs the MetNet preprocessing of mean pooling Sat channels, followed by
+        concatenating the center crop and mean pool
+
+        In the paper, the radar data is space2depth'd, while satellite channel is mean pooled, but for this different
+        task, we choose to do either option for satellites
+        Args:
+            sat_channels: Number of satellite channels
+            crop_size: Center crop size
+            use_space2depth: Whether to use space2depth on satellite channels, or mean pooling, like in paper
+
+        """
+        super().__init__()
+        self.sat_channels = sat_channels
+
+        # Split off sat + mask channels into own image, and the rest, which we just take a center crop
+        # For this,
+        self.sat_downsample = (
+            torch.nn.PixelUnshuffle(downscale_factor=2)
+            if use_space2depth
+            else torch.nn.AvgPool3d(kernel_size=(1, 2, 2))
+        )
+        self.center_crop = torchvision.transforms.CenterCrop(size=crop_size)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        sat_channels = x[:, :, : self.sat_channels, :, :]
+        other_channels = x[:, :, self.sat_channels :, :, :]
+        sat_channels = self.sat_downsample(sat_channels)
+        other_channels = torchvision.transforms.CenterCrop(size=other_channels.size()[-1] // 2)(
+            other_channels
+        )  # center crop to same as downsample
+        # In paper, satellite and radar data is concatenated here
+        # We are just going to skip that bit
+
+        sat_center = self.center_crop(sat_channels)
+        sat_mean = F.avg_pool3d(sat_channels, (1, 2, 2))
+        other_channels = self.center_crop(other_channels)
+        # All the same size now, so concatenate together, already have time, lat/long, and elevation image
+        x = torch.cat([sat_center, sat_mean, other_channels], dim=2)
+        return x
+
+
+@register_model
+class MetNet(pl.LightningModule):
+    def __init__(
+        self,
+        image_encoder: str = "downsampler",
+        input_channels: int = 12,
+        sat_channels: int = 12,
+        input_size: int = 256,
+        out_channels: int = 12,
+        hidden_dim: int = 64,
+        kernel_size: int = 3,
+        num_layers: int = 1,
+        num_att_layers: int = 1,
+        head: nn.Module = nn.Identity(),
+        forecast_steps: int = 48,
+        temporal_dropout: float = 0.2,
+        lr: float = 0.001,
+        pretrained: bool = False,
+        visualize: bool = False,
+        loss: str = "mse",
+    ):
+        super().__init__()
+
+        self.forecast_steps = forecast_steps
+        self.criterion = get_loss(loss)
+        self.lr = lr
+        self.visualize = visualize
+        self.preprocessor = MetNetPreprocessor(
+            sat_channels=sat_channels, crop_size=input_size, use_space2depth=True
+        )
+        # Update number of input_channels with output from MetNetPreprocessor
+        new_channels = sat_channels * 4  # Space2Depth
+        new_channels *= 2  # Concatenate two of them together
+        input_channels = input_channels - sat_channels + new_channels
+        self.drop = nn.Dropout(temporal_dropout)
+        if image_encoder in ["downsampler", "default"]:
+            image_encoder = DownSampler(input_channels + forecast_steps)
+        else:
+            raise ValueError(f"Image_encoder {image_encoder} is not recognized")
+        nf = 256  # from the simple image encoder
+        self.image_encoder = TimeDistributed(image_encoder)
+        self.ct = ConditionTime(forecast_steps)
+        self.temporal_enc = TemporalEncoder(nf, hidden_dim, ks=kernel_size, n_layers=num_layers)
+        self.temporal_agg = nn.Sequential(
+            *[
+                AxialAttention(dim=hidden_dim, dim_index=1, heads=8, num_dimensions=2)
+                for _ in range(num_att_layers)
+            ]
+        )
+
+        self.head = head
+        self.head = nn.Conv2d(hidden_dim, out_channels, kernel_size=(1, 1))  # Reduces to mask
+        # self.head = nn.Sequential(nn.AdaptiveAvgPool2d(1), )
+
+    def encode_timestep(self, x, fstep=1):
+
+        # Preprocess Tensor
+        x = self.preprocessor(x)
+
+        # Condition Time
+        x = self.ct(x, fstep)
+
+        ##CNN
+        x = self.image_encoder(x)
+
+        # Temporal Encoder
+        _, state = self.temporal_enc(self.drop(x))
+        return self.temporal_agg(state)
+
+    def forward(self, imgs):
+        """It takes a rank 5 tensor
+        - imgs [bs, seq_len, channels, h, w]
+        """
+
+        # Compute all timesteps, probably can be parallelized
+        res = []
+        for i in range(self.forecast_steps):
+            x_i = self.encode_timestep(imgs, i)
+            out = self.head(x_i)
+            res.append(out)
+        res = torch.stack(res, dim=1).squeeze()
+        return res
+
+    def configure_optimizers(self):
+        # DeepSpeedCPUAdam provides 5x to 7x speedup over torch.optim.adam(w)
+        # optimizer = torch.optim.adam()
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=10, max_epochs=100)
+        lr_dict = {
+            # REQUIRED: The scheduler instance
+            "scheduler": scheduler,
+            # The unit of the scheduler's step size, could also be 'step'.
+            # 'epoch' updates the scheduler on epoch end whereas 'step'
+            # updates it after a optimizer update.
+            "interval": "step",
+            # How many epochs/steps should pass between calls to
+            # `scheduler.step()`. 1 corresponds to updating the learning
+            # rate after every epoch/step.
+            "frequency": 1,
+            # If using the `LearningRateMonitor` callback to monitor the
+            # learning rate progress, this keyword can be used to specify
+            # a custom logged name
+            "name": None,
+        }
+        return {"optimizer": optimizer, "lr_scheduler": lr_dict}
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.float()
+        y_hat = self(x)
+        y = torch.squeeze(y)
+
+        if self.visualize:
+            if np.random.random() < 0.01:
+                self.visualize_step(x, y, y_hat, batch_idx)
+        # Generally only care about the center x crop, so the model can take into account the clouds in the area without
+        # being penalized for that, but for now, just do general MSE loss, also only care about first 12 channels
+        loss = self.criterion(y_hat, y)
+        self.log("train/loss", loss)
+        frame_loss_dict = {}
+        for f in range(self.forecast_steps):
+            frame_loss = self.criterion(y_hat[:, f, :, :], y[:, f, :, :]).item()
+            frame_loss_dict[f"train/frame_{f}_loss"] = frame_loss
+        self.log_dict(frame_loss_dict)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        x = x.float()
+        y_hat = self(x)
+        y = torch.squeeze(y)
+        val_loss = self.criterion(y_hat, y)
+        self.log("val/loss", val_loss)
+        # Save out loss per frame as well
+        frame_loss_dict = {}
+        for f in range(self.forecast_steps):
+            frame_loss = self.criterion(y_hat[:, f, :, :], y[:, f, :, :]).item()
+            frame_loss_dict[f"val/frame_{f}_loss"] = frame_loss
+        self.log_dict(frame_loss_dict)
+        return val_loss
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        y = torch.squeeze(y)
+        loss = F.mse_loss(y_hat, y)
+        return loss
+
+    def visualize_step(self, x, y, y_hat, batch_idx, step="train"):
+        tensorboard = self.logger.experiment[0]
+        # Add all the different timesteps for a single prediction, 0.1% of the time
+        images = x[0].cpu().detach()
+        images = [img for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=13)
+        tensorboard.add_image(f"{step}/Input_Image_Stack", image_grid, global_step=batch_idx)
+        images = y[0].cpu().detach()
+        images = [img for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=12)
+        tensorboard.add_image(f"{step}/Target_Image_Stack", image_grid, global_step=batch_idx)
+        images = y_hat[0].cpu().detach()
+        images = [img for img in images]
+        image_grid = torchvision.utils.make_grid(images, nrow=12)
+        tensorboard.add_image(f"{step}/Generated_Image_Stack", image_grid, global_step=batch_idx)
+
+
+class TemporalEncoder(nn.Module):
+    def __init__(self, in_channels, out_channels=384, ks=3, n_layers=1):
+        super().__init__()
+        self.rnn = ConvGRU2(in_channels, out_channels, (ks, ks), n_layers, batch_first=True)
+
+    def forward(self, x):
+        x, h = self.rnn(x)
+        return (x, h[-1])
+
+
+def feat2image(x, target_size=(128, 128)):
+    "This idea comes from MetNet"
+    x = x.transpose(1, 2)
+    return x.unsqueeze(-1).unsqueeze(-1) * x.new_ones(1, 1, 1, *target_size)
