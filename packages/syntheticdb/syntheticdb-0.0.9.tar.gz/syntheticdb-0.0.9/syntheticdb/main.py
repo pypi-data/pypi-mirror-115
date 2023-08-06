448532e8f15c2f2ea40bcc43793bbff58496f9f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:58 +0000

    Add package syntheticdb/0.0.9/syntheticdb-0.0.9.tar.gz

---
+++ packages/syntheticdb/syntheticdb-0.0.9.tar.gz/syntheticdb-0.0.9/syntheticdb/main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from syntheticdb.db_core import Table, DataBase
+from syntheticdb.distributions import uniform_float_column, standard_normal_float_column
+
+
+if __name__ == "__main__":
+    user_table = Table(
+        columns={
+            "age": uniform_float_column(0, 100),
+            "height": standard_normal_float_column(5, 1),
+        },
+        row_count=1000,
+    )
+    db = DataBase(tables={"user": user_table})
+
+    rows = db.select("select age from `user` where height > 2 and age < 20")
+    df = pd.DataFrame.from_dict(rows)
+    plot = pd.DataFrame(df)["age"].hist(bins=50)
+    fig = plot.get_figure()
+    fig.savefig("test2.png")
+    print(df)
