6cc2ed178a77e1db5c85f52549c66501eb4244dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:16 +0000

    Add package cuwais-common/1.3.0/cuwais-common-1.3.0.tar.gz

---
+++ packages/cuwais-common/cuwais-common-1.3.0.tar.gz/cuwais-common-1.3.0/README.md
@@ -0,0 +1,4 @@
+# common
+The common Python code shared by the other modules
+
+`DATABASE_CONNECTION` env var gives the url for the database
---
+++ packages/cuwais-common/cuwais-common-1.3.0.tar.gz/cuwais-common-1.3.0/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cuwais-common",
+    version="1.3.0",
+    author="Joe O'Connor",
+    author_email="jo429@cam.ac.uk",
+    description="The shared libraries for CUWAIS competitions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AI-Wars-Soc/common",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'sqlalchemy~=1.4.1',
+        'pyyaml~=5.4.1',
+        'psycopg2~=2.7.7',
+        'chess~=1.3.3'
+    ],
+    python_requires='>=3.6',
+)
