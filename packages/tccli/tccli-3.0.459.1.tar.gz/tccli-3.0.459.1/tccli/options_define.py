3c146ced96f6c0abcbc2ee3ade0f0b19ccdadc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:24 +0000

    Add package tccli/3.0.459.1/tccli-3.0.459.1.tar.gz

---
+++ packages/tccli/tccli-3.0.459.1.tar.gz/tccli-3.0.459.1/tccli/options_define.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+SecretId = "secretId"
+SecretKey = "secretKey"
+Token = "token"
+RoleArn = "roleArn"
+RoleSessionName = "roleSessionName"
+UseCVMRole = "useCVMRole"
+Region = "region"
+Endpoint = "endpoint"
+Version = "version"
+ServiceVersion = "service_version"
+Filter = "filter"
+Profile = "profile"
+Timeout = "timeout"
+Output = "output"
+ArrayCount = "arrayCount"
+Warnings = "warning"
+HttpsProxy = "https_proxy"
+GenerateCliSkeleton = "generate-cli-skeleton"
+CliInputJson = "cli-input-json"
+CliUnfoldArgument = "cli-unfold-argument"
+
+ENV_SECRET_ID = "TENCENTCLOUD_SECRET_ID"
+ENV_SECRET_KEY = "TENCENTCLOUD_SECRET_KEY"
+ENV_ROLE_ARN = "TENCENTCLOUD_ROLE_ENV"
+ENV_ROLE_SESSION_NAME = "TENCENTCLOUD_ROLE_SESSION_NAME"
+ENV_REGION = "TENCENTCLOUD_REGION"
+ENV_TOKEN = "TENCENTCLOUD_TOKEN"
+
+Help = "help"
+CliVersion = "version"
+Detail = "detail"
+
+
+ACTION_GLOBAL_OPT = [SecretId, SecretKey, RoleArn, RoleSessionName, UseCVMRole, Token, Region, Endpoint, Version,
+                     Filter, Profile, Timeout, Output, GenerateCliSkeleton, CliInputJson, CliUnfoldArgument]
+
+
+
