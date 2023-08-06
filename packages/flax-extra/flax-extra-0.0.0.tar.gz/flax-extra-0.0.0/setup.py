a9bc91cb1149a895a954bb8ecf2bbec177238c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:39 +0000

    Add package flax-extra/0.0.0/flax-extra-0.0.0.tar.gz

---
+++ packages/flax-extra/flax-extra-0.0.0.tar.gz/flax-extra-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+# Setup keywords.
+# https://setuptools.readthedocs.io/en/latest/references/keywords.html
+setup(
+    name="flax-extra",
+    version="0.0.0",
+    author="Andrei Nesterov",
+    author_email="ae.nesterov@gmail.com",
+    description="",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+)
