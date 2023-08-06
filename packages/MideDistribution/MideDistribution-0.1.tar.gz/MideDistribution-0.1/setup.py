ef4706752ec6afd8691fb720cd751280d776e24d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:34 +0000

    Add package MideDistribution/0.1/MideDistribution-0.1.tar.gz

---
+++ packages/MideDistribution/MideDistribution-0.1.tar.gz/MideDistribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='MideDistribution',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['MideDistribution'],
+      author= 'Midecreative',
+      author_email = "adesoba5@gmail.com",
+      zip_safe=False)
