4800bae3fcca08e82c1ba267565cdb57852ba72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:31 +0000

    Add package xlsxreader/0.1.5/xlsxreader-0.1.5.tar.gz

---
+++ packages/xlsxreader/xlsxreader-0.1.5.tar.gz/xlsxreader-0.1.5/test/test_readxlsx.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+import os
+import sys
+
+if sys.version_info[0] == 3:
+    from pathlib import Path
+else:
+    from pathlib2 import Path
+    ModuleNotFoundError = ImportError
+
+try:
+
+    from xlsxreader import readxlsx
+    print('what the fuck?!')
+
+except ModuleNotFoundError:
+    sys.path.append('..')
+    import xlsxreader
+    print('I am lost')
+
+if 'test' in os.listdir('.'):
+    os.chdir('./test')
+print('whats')
+test_file = xlsxreader.readxlsx('testboo1.xlsx')
+print(str(test_file.read(), encoding="utf-8"))
