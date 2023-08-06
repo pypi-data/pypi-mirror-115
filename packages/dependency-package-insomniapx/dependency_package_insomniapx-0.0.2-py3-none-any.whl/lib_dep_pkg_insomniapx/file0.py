5aecb9a717a7fe4e852efedc3486b05c122f9655
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:03 +0000

    Add package dependency-package-insomniapx/0.0.2/dependency_package_insomniapx-0.0.2-py3-none-any.whl

---
+++ packages/dependency-package-insomniapx/dependency_package_insomniapx-0.0.2-py3-none-any.whl/lib_dep_pkg_insomniapx/file0.py
@@ -0,0 +1,13 @@
+class px_psw_cls_0:
+    def __init__(self):
+        import numpy
+        a = numpy.array([0,1,3,4])
+        self.val = 'qiqi:' + str(520 + a[0]) + ' from px_psw_cls_0'
+    def get_val(self):
+        return self.val
+
+def px_psw_func_0():
+    import numpy as np
+    a = np.array([i for i in range(10)])
+    ret = 'qiqi:' + str(519 + a[1]) + ' from px_psw_func_0'
+    return ret
