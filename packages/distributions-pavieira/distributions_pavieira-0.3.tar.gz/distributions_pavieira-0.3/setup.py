82bc93667c746d9e5a32823db518462f0c5586cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:34 +0000

    Add package distributions-pavieira/0.3/distributions_pavieira-0.3.tar.gz

---
+++ packages/distributions-pavieira/distributions_pavieira-0.3.tar.gz/distributions_pavieira-0.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_pavieira',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions-pavieira'],
+      zip_safe=False)
