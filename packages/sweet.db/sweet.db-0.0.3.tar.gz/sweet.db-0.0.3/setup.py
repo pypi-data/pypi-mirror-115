d5de43aa043c3759fced839e246fb47e0a089780
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:06 +0000

    Add package sweet.db/0.0.3/sweet.db-0.0.3.tar.gz

---
+++ packages/sweet.db/sweet.db-0.0.3.tar.gz/sweet.db-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="sweet.db",
+    version="0.0.3",
+    author="tonglei",
+    author_email="tonglei@qq.com",
+    description="Sweet's db autotest module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sweeterio/db",
+    packages=['sweet.modules'],
+    package_data={'.': ['*.py']},
+    install_requires=[
+        'injson'
+    ],            
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
