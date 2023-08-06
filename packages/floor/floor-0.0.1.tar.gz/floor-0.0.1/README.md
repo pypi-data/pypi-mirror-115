65a69f2eec4bcc521a5b0535c397cb2c7aab613e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:52 +0000

    Add package floor/0.0.1/floor-0.0.1.tar.gz

---
+++ packages/floor/floor-0.0.1.tar.gz/floor-0.0.1/README.md
@@ -0,0 +1,8 @@
+# Motivate
+
+A rich and useful library to floor strings of text
+
+import using: from floor import floor
+
+commands:
+- floor("string")
---
+++ packages/floor/floor-0.0.1.tar.gz/floor-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+__project__ = "floor"
+__version__ = "0.0.1"
+__description__ = "a python module to floor strings or variables"
+__packages__ = ["floor"]
+__author__ = "Shayan B."
+__author_email__ = "sebebse9554@gmail.com"
+
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    author_email = __author_email__,
+)
