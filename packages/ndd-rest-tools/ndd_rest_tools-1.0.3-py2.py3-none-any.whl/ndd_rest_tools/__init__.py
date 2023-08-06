7a2422cc3d56eb95e388e7262627f2bf8738bf05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:50 +0000

    Add package ndd-rest-tools/1.0.3/ndd_rest_tools-1.0.3-py2.py3-none-any.whl

---
+++ packages/ndd-rest-tools/ndd_rest_tools-1.0.3-py2.py3-none-any.whl/ndd_rest_tools/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from .welcome import welcome
+from .models import ProxyModel, RequestResponse
+from .main import ApiClient
+
+__version__ = "1.0.3"
+__all__ = [
+    "welcome",
+    "ProxyModel",
+    'RequestResponse',
+    'ApiClient'
+]
