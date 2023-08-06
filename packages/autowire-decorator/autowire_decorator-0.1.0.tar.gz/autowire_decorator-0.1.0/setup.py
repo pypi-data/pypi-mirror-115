bf3bf43bc92d3bd4beacebfa5ebf1cadbbe0bb0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:49 +0000

    Add package autowire-decorator/0.1.0/autowire_decorator-0.1.0.tar.gz

---
+++ packages/autowire-decorator/autowire_decorator-0.1.0.tar.gz/autowire_decorator-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['autowire_decorator']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['flask-restx>=0.5.0,<0.6.0']
+
+setup_kwargs = {
+    'name': 'autowire-decorator',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'nadaabdelmaboud',
+    'author_email': 'nada5aled52@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
