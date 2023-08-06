bfe7e1d759c1551d41403fcdd0b738e3a10f07a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:09 +0000

    Add package pl-minio-callback/1.0.4/pl-minio-callback-1.0.4.tar.gz

---
+++ packages/pl-minio-callback/pl-minio-callback-1.0.4.tar.gz/pl-minio-callback-1.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pl-minio-callback",
+    version="1.0.4",
+    author="Daniel Garcia Pulpeiro",
+    author_email="danielgarciapulpeiro@gmail.com",
+    description="Callback prepared to log checkpoints to minio server.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dpulpeiro/pl-minio-callback",
+    project_urls={
+        "Bug Tracker": "https://github.com/dpulpeiro/pl-minio-callback/issues",
+
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: System :: Logging",
+    ],
+    install_requires=[
+        "pytorch-lightning",
+        "minio"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
