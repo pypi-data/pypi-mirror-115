a77ea6f664fa6184802d364497cf217a2785d5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:30 +0000

    Add package comeagainquestionmark/0.1.0/comeagainquestionmark-0.1.0.tar.gz

---
+++ packages/comeagainquestionmark/comeagainquestionmark-0.1.0.tar.gz/comeagainquestionmark-0.1.0/comeagainquestionmark/__main__.py
@@ -0,0 +1,4 @@
+import sys
+from comeAgainQuestionMark import main
+
+main.app(sys.argv)
---
+++ packages/comeagainquestionmark/comeagainquestionmark-0.1.0.tar.gz/comeagainquestionmark-0.1.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['comeagainquestionmark']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SpeechRecognition>=3.8.1,<4.0.0',
+ 'alive-progress>=1.6.2,<2.0.0',
+ 'moviepy>=1.0.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'comeagainquestionmark',
+    'version': '0.1.0',
+    'description': 'Transcription tool for video to text',
+    'long_description': None,
+    'author': 'Henry Bergström',
+    'author_email': 'henrybergstrom@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
