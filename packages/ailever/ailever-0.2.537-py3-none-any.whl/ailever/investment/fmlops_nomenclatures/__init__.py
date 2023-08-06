80e675baa692c08f13ce788eb79811d56c908a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:55 +0000

    Add package ailever/0.2.537/ailever-0.2.537-py3-none-any.whl

---
+++ packages/ailever/ailever-0.2.537-py3-none-any.whl/ailever/investment/fmlops_nomenclatures/__init__.py
@@ -0,0 +1,6 @@
+from .model_registry import ModelRegistryNomenclature as Base_MRN
+from .feature_store import FeatureStoreNomenclature as Base_FSN
+from .metadata_store import MetadataStoreNomenclature as Base_MSN
+from .rawdata_store import RawdataStoreNomenclature as Base_RSN
+from .source_repository import SourceRepositoryNomenclature as Base_SRN
+
