a77168ac75f5fcd77d1bb853d0cf636f3a3b6281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:21 +0000

    Add package test-xlsxreader-test/0.0.2/test_xlsxreader_test-0.0.2.tar.gz

---
+++ packages/test-xlsxreader-test/test_xlsxreader_test-0.0.2.tar.gz/test_xlsxreader_test-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_xlsxreader_test",
+    version="0.0.2",
+    author="Michael Brine",
+    author_email="mbrine0@gmail.com",
+    description="test xlsxreader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/michael-brine/xlsxreader",
+    project_urls={
+        "Bug Tracker": "https://github.com/michael-brine/xlsxreader",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
