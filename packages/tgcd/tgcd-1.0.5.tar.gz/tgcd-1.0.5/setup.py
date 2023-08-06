0be9d24b41b59ea945ff199860c0baa280beed9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:29 +0000

    Add package tgcd/1.0.5/tgcd-1.0.5.tar.gz

---
+++ packages/tgcd/tgcd-1.0.5.tar.gz/tgcd-1.0.5/setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+setuptools.setup(
+    name="tgcd",
+    version="1.0.5",
+    author="reaitten",
+    author_email="wsy0xf2u8@relay.firefox.com",
+    description="a private fork of slam-mirrorbot.",
+    include_package_data=True,
+    python_requires=">=3.8.2",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'aiohttp',
+        'aria2p',
+        'appdirs',
+        'beautifulsoup4',
+        'cloudscrape',
+        'feedparser',
+        'gitpython',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'heroku3',
+        'js2py',
+        'lk21',
+        'lxml',
+        'psutil',
+        'psycopg2-binary',
+        'pybase64',
+        'pyrogram',
+        'python-dotenv',
+        'python-magic',
+        'python-telegram-bot',
+        'requests',
+        'speedtest-cli',
+        'telegraph',
+        'tenacity',
+        'TgCrypto',
+        'youtube_dl'],
+    scripts=['extract'],
+    entry_points={
+        "console_scripts":[
+            "tgcd = bot.__main__:main"
+                ],
+    },
+)
