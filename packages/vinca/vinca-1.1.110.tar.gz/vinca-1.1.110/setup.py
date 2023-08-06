d60a2fe816faf496af438acb2adeb5d204a4a403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:31 +0000

    Add package vinca/1.1.110/vinca-1.1.110.tar.gz

---
+++ packages/vinca/vinca-1.1.110.tar.gz/vinca-1.1.110/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+
+setuptools.setup(
+    name="vinca",
+    version="1.1.110",
+    author="Oscar Laird", 
+    data_files = [('man/man1', ['vinca.1'])],
+    include_package_data = True,
+    packages=setuptools.find_packages(),
+    install_requires=[ 
+        'readchar',
+    ],
+    scripts = ['scripts/vinca','scripts/vinca_debug']
+)
