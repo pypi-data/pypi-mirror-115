88bdfdba56a6d65b261172b946cac1a1ae83ac98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:48 +0000

    Add package statistical-distributions-TK/0.1/statistical_distributions_TK-0.1.tar.gz

---
+++ packages/statistical-distributions-TK/statistical_distributions_TK-0.1.tar.gz/statistical_distributions_TK-0.1/README.md
@@ -0,0 +1,4 @@
+# statistical_distributions_TK
+_Machine Learning Exercise_
+
+Python package containing general, gaussian, and binomial classes.
---
+++ packages/statistical-distributions-TK/statistical_distributions_TK-0.1.tar.gz/statistical_distributions_TK-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='statistical_distributions_TK',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['statistical_distributions_TK'],
+      zip_safe=False)
