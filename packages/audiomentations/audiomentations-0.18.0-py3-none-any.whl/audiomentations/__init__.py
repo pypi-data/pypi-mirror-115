c8cf33c16e0478d15f1e67dce7993aff12a54c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:11 +0000

    Add package audiomentations/0.18.0/audiomentations-0.18.0-py3-none-any.whl

---
+++ packages/audiomentations/audiomentations-0.18.0-py3-none-any.whl/audiomentations/__init__.py
@@ -0,0 +1,30 @@
+from .augmentations.spectrogram_transforms import SpecFrequencyMask, SpecChannelShuffle
+from .augmentations.transforms import (
+    AddBackgroundNoise,
+    AddGaussianNoise,
+    AddGaussianSNR,
+    AddImpulseResponse,
+    ApplyImpulseResponse,
+    AddShortNoises,
+    BandPassFilter,
+    Clip,
+    ClippingDistortion,
+    FrequencyMask,
+    Gain,
+    HighPassFilter,
+    LoudnessNormalization,
+    LowPassFilter,
+    Mp3Compression,
+    Normalize,
+    PitchShift,
+    PolarityInversion,
+    Resample,
+    Reverse,
+    Shift,
+    TimeMask,
+    TimeStretch,
+    Trim,
+)
+from .core.composition import Compose, SpecCompose
+
+__version__ = "0.18.0"
