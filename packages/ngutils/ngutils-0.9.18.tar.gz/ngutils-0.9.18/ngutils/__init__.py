f5c7cb87de12729a80669d8263d2c3e96dac41c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:05 +0000

    Add package ngutils/0.9.18/ngutils-0.9.18.tar.gz

---
+++ packages/ngutils/ngutils-0.9.18.tar.gz/ngutils-0.9.18/ngutils/__init__.py
@@ -0,0 +1,16 @@
+from ngutils.functions import *
+
+__version__ = '0.9.18'
+
+__all__ = [
+    'accel_steps',
+    'flatten',
+    'hash_hd',
+    'json_to_list',
+    'read_files_contents',
+    'read_urls_contents',
+    'reduce_content',
+    'tune_steps',
+    'view_types',
+]
+
