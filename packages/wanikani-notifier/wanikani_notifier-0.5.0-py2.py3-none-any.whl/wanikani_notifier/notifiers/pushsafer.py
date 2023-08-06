073a72bf82a0a6b954c2a99824c8983bef923e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:42 +0000

    Add package wanikani-notifier/0.5.0/wanikani_notifier-0.5.0-py2.py3-none-any.whl

---
+++ packages/wanikani-notifier/wanikani_notifier-0.5.0-py2.py3-none-any.whl/wanikani_notifier/notifiers/pushsafer.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+import pushsafer
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+
+from wanikani_notifier.notifiers.notifier import Notifier, NoMessageProvided, factory
+
+
+class PushSaferNotifier(Notifier):
+    def __init__(self, private_key: str):
+        urllib3.disable_warnings(category=InsecureRequestWarning)
+
+        pushsafer.init(private_key)
+        self._client = pushsafer.Client("")
+
+    @classmethod
+    def key(cls) -> str:
+        return "pushsafer"
+
+    @classmethod
+    def build(cls, private_key: str, **_ignored) -> "PushSaferNotifier":
+        return PushSaferNotifier(private_key)
+
+    def notify(self, title: str, message: str, url: Optional[str] = None, icon: Optional[str] = None):
+        if not message:
+            raise NoMessageProvided
+
+        self._client.send_message(message, title,
+                                  None, None, None, None, url, None, None,
+                                  None, None, None, None, None, None, None)
+
+
+factory.register_builder(PushSaferNotifier.key(), PushSaferNotifier.build)
