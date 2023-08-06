1d6af7524d09ea67931fb1328b94af82c69c7163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package vdk-ingest-http/0.1.348963555/vdk-ingest-http-0.1.348963555.tar.gz

---
+++ packages/vdk-ingest-http/vdk-ingest-http-0.1.348963555.tar.gz/vdk-ingest-http-0.1.348963555/setup.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+__version__ = "0.1.348963555"
+
+setuptools.setup(
+    name="vdk-ingest-http",
+    version=__version__,
+    description="Versatile Data Kit SDK ingestion plugin to ingest data via http requests.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    install_requires=["vdk-core"],
+    package_dir={"": "src"},
+    packages=setuptools.find_namespace_packages(where="src"),
+    entry_points={
+        "vdk.plugin.run": ["vdk-ingest-http = taurus.vdk.ingest_http_plugin"]
+    },
+)
