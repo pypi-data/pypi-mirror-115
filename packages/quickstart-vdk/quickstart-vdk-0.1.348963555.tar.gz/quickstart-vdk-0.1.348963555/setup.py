2d5a98f02ca1b769d15e9064a0a90b09c9fc557a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:50 +0000

    Add package quickstart-vdk/0.1.348963555/quickstart-vdk-0.1.348963555.tar.gz

---
+++ packages/quickstart-vdk/quickstart-vdk-0.1.348963555.tar.gz/quickstart-vdk-0.1.348963555/setup.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import pathlib
+
+import setuptools
+
+__version__ = "0.1.348963555"
+
+setuptools.setup(
+    name="quickstart-vdk",
+    description="Versatile Data Kit SDK packaging containing common plugins to get started quickly using it.",
+    long_description=pathlib.Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    version=__version__,
+    install_requires=[
+        "vdk-core",
+        "vdk-plugin-control-cli",
+        "vdk-trino",
+        "vdk-ingest-http",
+        "vdk-ingest-file",
+    ],
+)
