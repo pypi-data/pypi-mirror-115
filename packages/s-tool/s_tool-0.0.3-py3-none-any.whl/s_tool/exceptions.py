8162b18cd4cac2f7b72bfbd1ccd51e8297c4a3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:23 +0000

    Add package s-tool/0.0.3/s_tool-0.0.3-py3-none-any.whl

---
+++ packages/s-tool/s_tool-0.0.3-py3-none-any.whl/s_tool/exceptions.py
@@ -0,0 +1,20 @@
+class SToolException(Exception):
+    """
+    Base Class for selenium tools Exceptions
+    """
+
+    def __init__(self, message):
+        """[summary]
+
+        Args:
+            message ([str]): Exception message
+        """
+        self.message = message
+
+    def __str__(self):
+        """Return Exception message
+
+        Returns:
+            [str]: Exception
+        """
+        return str(self.message)
