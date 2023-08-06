4a3e35fe6a4698d4ef6677f0f771e69e50b2089d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:04 +0000

    Add package fqsdfqsdfqsdfqsd/0.3/fqsdfqsdfqsdfqsd-0.3.tar.gz

---
+++ packages/fqsdfqsdfqsdfqsd/fqsdfqsdfqsdfqsd-0.3.tar.gz/fqsdfqsdfqsdfqsd-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='fqsdfqsdfqsdfqsd',
+      version='0.3',
+      description='fqsdfqsdfqsdfqsd',
+      author='fqsdfqsdfqsdfqsd',
+      author_email='fqsdfqsdfqsdfqsdn@gmail.com',
+      license='MIT',
+      packages=find_packages(include=['credix', 'credix.*']),
+      zip_safe=False)
