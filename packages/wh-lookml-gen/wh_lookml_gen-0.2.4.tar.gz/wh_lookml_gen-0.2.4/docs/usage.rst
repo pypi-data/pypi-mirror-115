46a94d74013b77e7fd4451a7ffb147a974e4edfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:12 +0000

    Add package wh-lookml-gen/0.2.4/wh_lookml_gen-0.2.4.tar.gz

---
+++ packages/wh-lookml-gen/wh_lookml_gen-0.2.4.tar.gz/wh_lookml_gen-0.2.4/docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use wh_lookml_gen in a project::
+
+    import wh_lookml_gen
---
+++ packages/wh-lookml-gen/wh_lookml_gen-0.2.4.tar.gz/wh_lookml_gen-0.2.4/wh_lookml_gen/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for wh_lookml_gen."""
+
+__author__ = """Lewis Baker"""
+__email__ = 'lewischarlebaker@gmail.com'
+__version__ = '0.1.7'
