3c67ba932476f3c4bb2b61117cacf642d38ee547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package cc-database-interface/0.0/cc-database_interface-0.0.tar.gz

---
+++ packages/cc-database-interface/cc-database_interface-0.0.tar.gz/cc-database_interface-0.0/cc-database_interface/cargocast_forecast/forecast.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy import String, Date, Integer, Numeric
+from engines import *
+from cargoline.partner import Partner
+from .timeindex import TimeIndex
+from .forecastunit import ForecastUnit
+from .model import Model
+from session_handler import SessionHandler
+
+class Forecast(Base,SessionHandler):
+  __tablename__ = 'forecast'
+  TimeIndexId = Column(String, ForeignKey('timeindex.Id'),primary_key=True)
+  ForecastUnitId = Column(String,ForeignKey('forecastunit.Id'), primary_key=True)
+  PartnerId = Column(String, ForeignKey('partner.Id'), primary_key=True)
+  ModelId = Column(Integer, ForeignKey('model.Id'),primary_key=True)
+  Date = Column(Date, primary_key=True)
+  H = Column(Integer, primary_key=True)
+  Forecast = Column(Numeric)
+  
+  
+  def create_session(self):
+    Session = sessionmaker(forecast_engine)
+    return Session()
+
+  def __repr__(self):
+    return "<Forecast(TimeIndexId='%s', ForecastUnitId='%s', PartnerId='%s',ModelId='%s',Date='%s',H='%s',Forecast = '%s')>" % (self.TimeIndexId, self.ForecastUnitId, self.PartnerId, self.ModelId,self.Date,self.H,self.Forecast)
