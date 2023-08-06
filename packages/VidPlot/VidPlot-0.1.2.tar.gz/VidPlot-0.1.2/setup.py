6b1c528ffc1a620994c8957dd9e40ab180610eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:11 +0000

    Add package VidPlot/0.1.2/VidPlot-0.1.2.tar.gz

---
+++ packages/VidPlot/VidPlot-0.1.2.tar.gz/VidPlot-0.1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 2 - Pre-Alpha',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='VidPlot',
+  version='0.1.2',
+  description='Create and Display mp4 file from matplotlib figure',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',
+  author='Silvano Rosenau',
+  author_email='silvano.rosenau@studium.uni-hamburg.de',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='video',
+  packages=find_packages(),
+  install_requires=['numpy', ['imageio'], ['IPython']]
+)
