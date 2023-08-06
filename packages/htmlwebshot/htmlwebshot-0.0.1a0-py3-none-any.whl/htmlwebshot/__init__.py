69a904d3015dec05d1da464c429319e9213ce783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:41 +0000

    Add package htmlwebshot/0.0.1a0/htmlwebshot-0.0.1a0-py3-none-any.whl

---
+++ packages/htmlwebshot/htmlwebshot-0.0.1a0-py3-none-any.whl/htmlwebshot/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+python wrapper to convert any readable file/txt to image using the webkit rendering engine.
+"""
+
+from .config import Config
+from .webshot import WebShot
+
+__version__ = "0.1"
