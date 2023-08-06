4fb24f819a54d9928ab80ab5436b40219c1d512c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:36 +0000

    Add package b3api/0.0.4/b3api-0.0.4-py3-none-any.whl

---
+++ packages/b3api/b3api-0.0.4-py3-none-any.whl/b3api/__main__.py
@@ -0,0 +1,24 @@
+import json
+import optparse
+
+import assets
+
+if __name__ == "__main__":
+
+    parser = optparse.OptionParser()
+
+    parser.add_option(
+        "-o",
+        "--output",
+        action="store",
+        dest="filename",
+        help="JSON file name.",
+        default="output.json",
+    )
+
+    options, tickers = parser.parse_args()
+
+    data = assets.getAll(tickers)
+
+    with open(options.filename, "w") as f:
+        json.dump(data, f)
